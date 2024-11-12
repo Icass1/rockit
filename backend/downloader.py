fc884e7c7365f2438554ce12afdd636943dcebd9
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-12 19:12:37 +0100

    MInor changes

--- backend/downloader.py
+++ backend/downloader.py
@@ -1,5 +1,8 @@
 from spotdl.download.downloader import Downloader as SpotifyDownloader
 from spotdl.types.song import Song
+from spotdl.utils.matching import get_best_matches, order_results
+from spotdl.types.result import Result
+import spotdl.providers.audio.base
 
 from api_types import RawSpotifyApiSong, RawSpotifyApiAlbum, AlbumItem
 
@@ -10,9 +13,103 @@ from constants import DOWNLOADER_OPTIONS
 import requests
 from spotify import Spotify
 import threading
-from typing import Any, Dict, Optional, List
-import time
+from typing import Any, Dict, Optional, List, Tuple
 import json
+import time
+from bs4 import BeautifulSoup
+import logging
+
+# # Set up a handler specifically for spotdl
+# spotdl_logger = logging.getLogger("spotdl")
+# spotdl_logger.setLevel(logging.DEBUG)
+
+# # Create a console handler for the spotdl logger
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.DEBUG)
+
+# # Optional: add formatting to the handler
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# console_handler.setFormatter(formatter)
+
+# # Add the handler to the spotdl logger
+# spotdl_logger.addHandler(console_handler)
+
+# # Remove other handlers to avoid duplicate logs
+# spotdl_logger.propagate = False
+
+
+
+
+
+
+
+
+def get_best_result(self, results: Dict[Result, float]) -> Tuple[Result, float]:
+    """
+    Get the best match from the results
+    using views and average match
+
+    ### Arguments
+    - results: A dictionary of results and their scores
+
+    ### Returns
+    - The best match URL and its score
+    """
+
+    best_results = get_best_matches(results, 8)
+
+    # If we have only one result, return it
+    if len(best_results) == 1:
+        return best_results[0][0], best_results[0][1]
+
+    # Initial best result based on the average match
+    best_result = best_results[0]
+
+    # If the best result has a score higher than 80%
+    # and it's a isrc search, return it
+    if best_result[1] > 80 and best_result[0].isrc_search:
+        return best_result[0], best_result[1]
+
+    # If we have more than one result,
+    # return the one with the highest score
+    # and most views
+    if len(best_results) > 1:
+        views: List[int] = []
+        for best_result in best_results:
+            if best_result[0].views:
+                views.append(best_result[0].views)
+            else:
+                response = requests.get(best_result[0].url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                test = soup.find("meta", itemprop="interactionCount")
+
+                _views = int(test["content"])
+                views.append(_views)
+
+        highest_views = max(views)
+        lowest_views = min(views)
+
+        if highest_views in (0, lowest_views):
+            return best_result[0], best_result[1]
+
+        weighted_results: List[Tuple[Result, float]] = []
+        for index, best_result in enumerate(best_results):
+            result_views = views[index]
+            views_score = (
+                (result_views - lowest_views) / (highest_views - lowest_views)
+            ) * 15
+            score = min(best_result[1] + views_score, 100)
+            weighted_results.append((best_result[0], score))
+
+        # Now we return the result with the highest score
+        return max(weighted_results, key=lambda x: x[1])
+
+    return best_result[0], best_result[1]
+
+
+
+spotdl.providers.audio.base.AudioProvider.get_best_result = get_best_result
+
 
 THREADS = 4
 
@@ -23,9 +120,9 @@ class ListDownloader:
         self.downloader = downloader
 
         self.url = self.downloader.spotify.parse_url(url)
-        self.list: RawSpotifyApiAlbum
-        self.spotdl_songs: List[Song]
-        self.raw_songs: List[AlbumItem]
+        self.list: RawSpotifyApiAlbum = None
+        self.spotdl_songs: List[Song] = None
+        self.raw_songs: List[AlbumItem] = None
 
         if "/album/" in self.url:
             self.type = "album"
@@ -78,7 +175,7 @@ class ListDownloader:
                     continue
 
                 print(f"[LIST DOWNLOADER MANAGER]: Started thread {index}")
-                thread = threading.Thread(target=self.downloader.download_song, name=f"List song downloader {self.spotdl_songs[i].name} - {self.spotdl_songs[i].artist}", args=(self.spotdl_songs[index],))
+                thread = threading.Thread(target=self.downloader.download_song, name=f"List song downloader {self.spotdl_songs[index].name} - {self.spotdl_songs[i].artist}", args=(self.spotdl_songs[index],))
                 thread.start()
                 threads.append(thread)
                 
@@ -120,6 +217,7 @@ class ListDownloader:
                     # k["list"] = self.list.json # Too much data
                     k["list"] = {}
                     k["list"]["name"] = self.list.name
+                    k["list"]["id"] = self.list.id
                     k["list"]["artists"] = [artist.json for artist in self.list.artists]
                     k["list"]["images"] = [image.json for image in self.list.images]
 
@@ -143,7 +241,8 @@ class SongDownloader:
         self.raw_song: RawSpotifyApiSong = None
 
         self.downloader = downloader
-        threading.Thread(target=lambda : self.fetch_song(), name=f"Song downloader {url}").start()
+        self.thread = threading.Thread(target=lambda : self.fetch_song(), name=f"Song downloader {url}")
+        self.thread.start()
 
     def fetch_song(self):
         self.spotdl_song, self.raw_song = self.downloader.spotify.spotdl_song_from_url(self.url)
@@ -174,9 +273,12 @@ class SongDownloader:
 
         last_messages_len = 0
         finish = False
+        last_time_new_message = time.time()
         while not finish:
+
+                       
             for k in self.downloader.downloads_dict[self.spotdl_song.song_id]["messages"][last_messages_len:]:
-                # k["song"] = self.raw_song.json
+                last_time_new_message = time.time()
                 k["song"] = {}
                 k["song"]["name"] = self.raw_song.name
                 k["song"]["artists"] = [artist.json for artist in self.raw_song.artists]
@@ -187,6 +289,20 @@ class SongDownloader:
                 
             last_messages_len = len(self.downloader.downloads_dict[self.spotdl_song.song_id]["messages"])
             time.sleep(0.5)
+
+            # Timeout
+            # if last_time_new_message + 10 < time.time():
+            #     k["song"] = {}
+            #     k["song"]["name"] = self.raw_song.name
+            #     k["song"]["artists"] = [artist.json for artist in self.raw_song.artists]
+            #     k["completed"] = 100
+            #     k["message"] = "Timeout"
+
+            #     print("Stopping", self.thread.name)
+            #     self.thread.terminate()
+
+            #     yield f"data: {json.dumps(k)}\n\n"
+            #     finish = True
 
 class Downloader:
     def __init__(self, spotify: Spotify) -> None:

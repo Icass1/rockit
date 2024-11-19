9c3f9862a3137fa2c74daf9ccc9e16a078469d34
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-19 11:45:18 +0100

    Improved types

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,102 @@
 import sqlite from "better-sqlite3";
+import * as fs from 'fs'
+
+fs.mkdir("database", { recursive: false }, () => { })
 export const db = sqlite("database/database.db");
+
+interface Column {
+    cid: number,
+    name: string,
+    type: string,
+    notnull: number,
+    dflt_value: null | string,
+    pk: number
+}
+
+function getDifference(listA: Column[], listB: Column[]) {
+    let addedColumns: string[] = []
+    let removedColumns: string[] = []
+    let modifiedColumns: { name: string, param: string, previous: string | null | number, next: string | null | number }[] = []
+
+    listA.map(columnA => {
+        let columnB = listB.find(columnB => columnA.name == columnB.name)
+        if (columnB == undefined) {
+            removedColumns.push(columnA.name)
+            return
+        }
+        if (columnA.dflt_value != columnB?.dflt_value) {
+            modifiedColumns.push({ name: columnA.name, param: "dflt_value", previous: columnA.dflt_value, next: columnB?.dflt_value })
+        }
+        if (columnA.pk != columnB?.pk) {
+            modifiedColumns.push({ name: columnA.name, param: "pk", previous: columnA.pk, next: columnB?.pk })
+        }
+        if (columnA.type != columnB?.type) {
+            modifiedColumns.push({ name: columnA.name, param: "type", previous: columnA.type, next: columnB?.type })
+        }
+        if (columnA.notnull != columnB?.notnull) {
+            modifiedColumns.push({ name: columnA.name, param: "notnull", previous: columnA.notnull, next: columnB?.notnull })
+        }
+    })
+
+    listB.map(columnB => {
+        let columnA = listA.find(columnA => columnA.name == columnB.name)
+        if (columnA == undefined) {
+            addedColumns.push(columnB.name)
+            return
+        }
+    })
+
+    return { modifiedColumns, removedColumns, addedColumns }
+
+}
+
+function checkTable(tableName: string, query: string, existingColumns: Column[]) {
+
+    let columns = query.split("(")[1].split(")")[0].replaceAll("\n", "").split(",")
+    columns = columns.map(column => {
+        while (column.startsWith(" ")) {
+            column = column.replace(" ", "")
+        }
+        return column
+    })
+    const newColumns = columns.map((column, index): Column => {
+        const columnSplit = column.split(" ")
+        return {
+            cid: index,
+            name: columnSplit[0],
+            type: columnSplit[1],
+            notnull: column.includes("NOT NULL") ? 1 : 0,
+            dflt_value: columnSplit.indexOf("DEFAULT") != -1 ? columnSplit[columnSplit.indexOf("DEFAULT") + 1] : null,
+            pk: column.includes("PRIMARY KEY") ? 1 : 0
+        }
+
+    })
+
+    const { modifiedColumns, addedColumns, removedColumns } = getDifference(existingColumns, newColumns)
+
+    if (modifiedColumns.length > 0) {
+        console.warn("Detected column change(s).", modifiedColumns)
+    }
+
+    if (removedColumns.length > 0) {
+        console.warn("Detected removed column(s).", removedColumns)
+    }
+
+    if (addedColumns.length > 0) {
+        console.warn("Detected new column(s).", addedColumns)
+        addedColumns.map(column => {
+            const newColumn = newColumns.find(_column => _column.name == column)
+            if (!newColumn) {
+                return console.error("Fatal, new column is not defined")
+            }
+
+            query = `${newColumn.name} ${newColumn.type} ${newColumn.dflt_value ? 'DEFAULT ' + newColumn.dflt_value : ''} ${newColumn.notnull ? 'NOT NULL' : ''}`
+            console.log(column, newColumn)
+            console.log(query)
+            db.exec(`ALTER TABLE ${tableName} ADD COLUMN ${query}`)
+        })
+    }
+}
 
 db.exec(`CREATE TABLE IF NOT EXISTS session (
     id TEXT NOT NULL PRIMARY KEY,
@@ -33,7 +130,7 @@ export interface UserDB {
     createdAt: number
 }
 
-db.exec(`CREATE TABLE IF NOT EXISTS user (
+const userQuery = `CREATE TABLE IF NOT EXISTS user (
     id TEXT NOT NULL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     passwordHash TEXT NOT NULL UNIQUE,
@@ -44,6 +141,7 @@ db.exec(`CREATE TABLE IF NOT EXISTS user (
     currentTime INTEGER,
     queue TEXT DEFAULT "[]" NOT NULL,
     queueIndex INTEGER,
+    pinnedLists TEXT DEFAULT "[]" NOT NULL,
     randomQueue BOOLEAN DEFAULT 0 NOT NULL,
     volume INTEGER DEFAULT 1 NOT NULL,
     admin BOOLEAN DEFAULT 0 NOT NULL,
@@ -52,7 +150,10 @@ db.exec(`CREATE TABLE IF NOT EXISTS user (
     showLyrics BOOLEAN DEFAULT 0 NOT NULL,
     updatedAt INTEGER NOT NULL,
     createdAt INTEGER NOT NULL
-)`);
+)`
+
+checkTable("user", userQuery, db.prepare("PRAGMA table_info(user)").all() as Column[])
+db.exec(userQuery);
 
 // ****************************************
 // ************** Song stuff **************
@@ -83,14 +184,15 @@ export interface RawSongDB {
     dateAdded: string | undefined
 }
 
-export type SongDB = {
+export type SongDB<Keys extends keyof SongDBFull = keyof SongDBFull> = Pick<SongDBFull, Keys>;
+export type SongDBFull = {
     id: string
     name: string
-    artists: DBArtist[]
+    artists: ArtistDB[]
     genres: string
     discNumber: number | undefined
     albumName: string
-    albumArtist: DBArtist[]
+    albumArtist: ArtistDB[]
     albumType: string
     albumId: string
     duration: number
@@ -100,19 +202,19 @@ export type SongDB = {
     tracksCount: number | undefined
     publisher: string | undefined
     path: string | undefined
-    images: DBImage[]
+    images: ImageDB[]
     copyright: string | undefined
     downloadUrl: string | undefined
     lyrics: string | undefined
     popularity: number | undefined
     dateAdded: string | undefined
 }
-export type DBImage = {
+export type ImageDB = {
     url: string
     width: number
     height: number
 }
-export type DBArtist = {
+export type ArtistDB = {
     name: string
     id: string
 }
@@ -126,30 +228,30 @@ export function parseSong(rawSong: RawSongDB | undefined): SongDB | undefined {
     return {
         id: rawSong.id,
         name: rawSong.name,
-        artists: JSON.parse(rawSong.artists),
-        genres: JSON.parse(rawSong.genres),
+        artists: JSON.parse(rawSong.artists || "[]"),
+        genres: JSON.parse(rawSong.genres || "[]"),
         discNumber: rawSong.discNumber,
         albumName: rawSong.albumName,
-        albumArtist: JSON.parse(rawSong.albumArtist),
+        albumArtist: JSON.parse(rawSong.albumArtist || "[]"),
         albumType: rawSong.albumType,
         albumId: rawSong.albumId,
         duration: rawSong.duration,
         year: rawSong.year,
         date: rawSong.date,
-        trackNumber: rawSong.discNumber,
-        tracksCount: rawSong.discNumber,
+        trackNumber: rawSong.trackNumber,
+        tracksCount: rawSong.tracksCount,
         publisher: rawSong.publisher,
         path: rawSong.path,
-        images: JSON.parse(rawSong.images),
+        images: JSON.parse(rawSong.images || "[]"),
         copyright: rawSong.copyright,
         downloadUrl: rawSong.downloadUrl,
         lyrics: rawSong.lyrics,
-        popularity: rawSong.discNumber,
+        popularity: rawSong.popularity,
         dateAdded: rawSong.dateAdded
     }
 }
 
-db.exec(`CREATE TABLE IF NOT EXISTS song (
+const songQuery = `CREATE TABLE IF NOT EXISTS song (
     id TEXT NOT NULL PRIMARY KEY UNIQUE,
     name TEXT NOT NULL,
     artists TEXT NOT NULL,
@@ -172,7 +274,10 @@ db.exec(`CREATE TABLE IF NOT EXISTS song (
     lyrics TEXT,
     popularity INTEGER,
     dateAdded TEXT
-)`);
+)`;
+
+checkTable("song", songQuery, db.prepare("PRAGMA table_info(song)").all() as Column[])
+db.exec(songQuery);
 
 // ********************************************
 // ************** Playlist stuff **************
@@ -189,7 +294,7 @@ export interface RawPlaylistDB {
 
 export interface PlaylistDB {
     id: string
-    images: DBImage[]
+    images: ImageDB[]
     name: string
     description: string
     owner: string
@@ -217,7 +322,7 @@ export function parsePlaylist(playlist: RawPlaylistDB | undefined): PlaylistDB |
     }
 }
 
-db.exec(`CREATE TABLE IF NOT EXISTS playlist (
+const playlistQuery = `CREATE TABLE IF NOT EXISTS playlist (
     id TEXT NOT NULL PRIMARY KEY UNIQUE,
     images TEXT NOT NULL,
     name TEXT NOT NULL,
@@ -225,7 +330,10 @@ db.exec(`CREATE TABLE IF NOT EXISTS playlist (
     owner TEXT NOT NULL,
     followers INTEGER NOT NULL,
     songs TEXT NOT NULL 
-)`);
+)`;
+
+checkTable("playlist", playlistQuery, db.prepare("PRAGMA table_info(playlist)").all() as Column[])
+db.exec(playlistQuery);
 
 // *****************************************
 // ************** Album stuff **************
@@ -248,10 +356,10 @@ export interface RawAlbumDB {
 export interface AlbumDB {
     id: string
     type: string
-    images: DBImage[]
+    images: ImageDB[]
     name: string
     releaseDate: string
-    artists: DBArtist[]
+    artists: ArtistDB[]
     copyrights: AlbumDBCopyright[]
     popularity: number
     genres: string[]
@@ -270,8 +378,6 @@ export function parseAlbum(album: RawAlbumDB | undefined): AlbumDB | undefined {
     if (!album) {
         return undefined
     }
-
-
     return {
         id: album.id,
         type: album.type,
@@ -288,7 +394,7 @@ export function parseAlbum(album: RawAlbumDB | undefined): AlbumDB | undefined {
     }
 }
 
-db.exec(`CREATE TABLE IF NOT EXISTS album (
+const albumQuery = `CREATE TABLE IF NOT EXISTS album (
     id TEXT NOT NULL PRIMARY KEY UNIQUE,
     type TEXT NOT NULL,
     images TEXT NOT NULL,
@@ -301,4 +407,7 @@ db.exec(`CREATE TABLE IF NOT EXISTS album (
     songs TEXT NOT NULL,
     discCount INTEGER NOT NULL,
     dateAdded INTEGER NOT NULL
-)`);
+)`;
+
+checkTable("album", albumQuery, db.prepare("PRAGMA table_info(album)").all() as Column[])
+db.exec(albumQuery);

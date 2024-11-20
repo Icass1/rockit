e51fb49c7eb3ef8b4886962d2cf4acf28c130ed8
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-20 15:59:25 +0100

    Added user api route

---
+++ src/pages/api/user.ts
@@ -0,0 +1,29 @@
+import type { APIContext } from "astro";
+
+import { db, parseUser, type RawUserDB } from "@/lib/db";
+
+export async function GET(context: APIContext): Promise<Response> {
+
+    if (!context.locals.user) {
+        return new Response("User is not logged in", { status: 401 })
+    }
+
+    let user
+    try {
+        user = parseUser(
+            db.prepare(`SELECT ${context.url.searchParams.get("q") || "*"} FROM user WHERE id = ?`).get(context.locals.user.id) as RawUserDB,
+        );
+
+    } catch (err) {
+        return new Response(err?.toString(), { status: 404 })
+    }
+
+    // @ts-ignore
+    delete user?.passwordHash
+
+    return new Response(JSON.stringify(user), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}

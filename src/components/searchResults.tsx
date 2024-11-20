101aab5e20d3f58b0655c78c899cabd764a2cb95
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-20 17:29:58 +0100

    Added best result to results and not in database indicator

--- src/components/searchResults.tsx
+++ src/components/searchResults.tsx
@@ -1,69 +1,122 @@
 
 import { useStore } from '@nanostores/react';
-import { searchResults } from '@/stores/searchResults';
+import { searchQuery, searchResults } from '@/stores/searchResults';
 
-import type { SpotifyAlbum, SpotifyTrack } from "@/types/spotify";
+import { DownloadCloud } from 'lucide-react';
+import { useEffect, useState, type MouseEvent, type ReactNode } from 'react';
+import stringSimilarity from '@/lib/stringSimilarity';
 
-function RenderTrackSearchResults({ tracksInfo, handleDownload }: { tracksInfo: SpotifyTrack[], handleDownload: (url: string) => void }) {
-    const handleClick = (element: SpotifyTrack) => {
-        handleDownload(element.external_urls.spotify)
+function Result({ image, name, artistsOrOwner, inDatabase, url }: { image: string, name: string, artistsOrOwner: string, inDatabase: boolean, url: string }) {
+
+    const handleClick = (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+        console.log("handle click")
+        event.stopPropagation()
+        event.preventDefault()
     }
-
     return (
-        <div
-            className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-2 mt-2 mb-5 h-fit"
+        <a
+            className={"flex flex-row h-full rounded overflow-hidden gap-x-2 bg-zinc-700 hover:bg-zinc-500/60 transition-colors cursor-pointer items-center " + ((artistsOrOwner == "") ? " rounded-l-[70px] rounded-r-lg " : " ")}
+            href={url.replace("https://open.spotify.com", "").replace("track", "song")}
         >
-            {tracksInfo.map((element, index) =>
-                <div
-                    key={'element' + element.uri + index}
-                    onClick={() => { handleClick(element) }}
-                    className="flex flex-row h-12 rounded overflow-hidden gap-x-2 bg-zinc-700 hover:bg-zinc-500/60 transition-colors cursor-pointer"
-                >
-                    <img className="aspect-square w-auto h-full" src={element.album.images[0].url}></img>
-                    <div className="flex flex-col text-white min-w-0 max-w-full">
-                        <label className="text-base font-semibold truncate">{element.name}</label>
-                        <label className="text-sm truncate">{element.artists && element.artists.map(artist => artist.name).join(", ")}</label>
-                    </div>
+            <img className={"aspect-square w-auto h-full " + ((artistsOrOwner == "") ? " rounded-full " : " ")} src={image}></img>
+            <div className="flex flex-col text-white min-w-0 max-w-full w-full">
+                <label className="text-base font-semibold truncate">{name}</label>
+                <label className="text-sm truncate">{artistsOrOwner}</label>
+            </div>
+            {!inDatabase &&
+                <div className='w-6 h-6 mr-2 text-blue-400 hover:scale-105 transition-transform' onClick={handleClick}>
+                    <DownloadCloud />
                 </div>
-            )}
-        </div>
-
+            }
+        </a>
     )
 }
-function RenderAlbumSearchResults({ tracksInfo, handleDownload }: { tracksInfo: SpotifyAlbum[], handleDownload: (url: string) => void }) {
-    const handleClick = (element: SpotifyAlbum) => {
-        handleDownload(element.external_urls.spotify)
-    }
 
+function ResultsWrapper({ children }: { children: ReactNode[] }) {
     return (
         <div
-            className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-2 mt-2 mb-5 h-fit"
+            className="grid xl:grid-cols-2 lg:grid-cols-1 md:grid-cols-1 gap-2 mb-2 h-fit"
         >
-            {tracksInfo.map((element, index) =>
-                <div
-                    key={'element' + element.uri + index}
-                    onClick={() => { handleClick(element) }}
-                    className="flex flex-row h-12 rounded overflow-hidden gap-x-2 bg-zinc-700 hover:bg-zinc-500/60 transition-colors cursor-pointer"
-                >
-                    <img className="aspect-square w-auto h-full" src={element.images[0].url}></img>
-                    <div className="flex flex-col text-white min-w-0 max-w-full">
-                        <label className="text-base font-semibold truncate">{element.name}</label>
-                        <label className="text-sm truncate">{element.artists && element.artists.map(artist => artist.name).join(", ")}</label>
-                    </div>
-                </div>
-            )}
+            {children}
         </div>
-
     )
 }
 
 export default function RenderSearchResults() {
 
     const $searchResults = useStore(searchResults);
+    const [bestResult, setBestResult] = useState<{ name: string, artistsOrOwner: string, url: string, image: string, inDatabase: boolean, type: string } | undefined>();
+    const $searchQuery = useStore(searchQuery)
 
-    const handleDownload = (url: string) => {
 
-    }
+
+    useEffect(() => {
+
+        let highestSimilarity = 0
+        let bestResult = undefined
+        let searchQuery = $searchQuery
+
+        $searchResults.albums?.map(album => {
+            const similarity = stringSimilarity(album.name, searchQuery)
+            if (similarity > highestSimilarity) {
+                highestSimilarity = similarity
+                bestResult = {
+                    name: album.name,
+                    artistsOrOwner: album.artists.map(artist => artist.name).join(","),
+                    url: album.external_urls.spotify,
+                    image: album.images[0].url,
+                    inDatabase: album.inDatabase,
+                    type: "album"
+                }
+            }
+        })
+        $searchResults.playlists?.map(playlist => {
+            const similarity = stringSimilarity(playlist.name, searchQuery)
+            if (similarity > highestSimilarity) {
+                highestSimilarity = similarity
+                bestResult = {
+                    name: playlist.name,
+                    artistsOrOwner: playlist.owner.display_name,
+                    url: playlist.external_urls.spotify,
+                    image: playlist.images[0].url,
+                    inDatabase: playlist.inDatabase,
+                    type: "playlist"
+                }
+            }
+        })
+        $searchResults.songs?.map(song => {
+            const similarity = stringSimilarity(song.name, searchQuery)
+            if (similarity > highestSimilarity) {
+                highestSimilarity = similarity
+                bestResult = {
+                    name: song.name,
+                    artistsOrOwner: song.album.artists.map(artist => artist.name).join(","),
+                    url: song.external_urls.spotify,
+                    image: song.album.images[0].url,
+                    inDatabase: song.inDatabase,
+                    type: "song"
+                }
+            }
+        })
+
+        $searchResults.artists?.map(artist => {
+            const similarity = stringSimilarity(artist.name, searchQuery)
+            if (similarity > highestSimilarity) {
+                highestSimilarity = similarity
+                bestResult = {
+                    name: artist.name,
+                    artistsOrOwner: "",
+                    url: artist.external_urls.spotify,
+                    image: artist.images[0].url,
+                    inDatabase: true,
+                    type: "artist"
+                }
+            }
+        })
+
+        setBestResult(bestResult)
+
+    }, [$searchResults])
 
     return (
         ($searchResults.songs == undefined && $searchResults.albums == undefined) ?
@@ -71,20 +124,85 @@ export default function RenderSearchResults() {
             </div>
             :
             <div
-                className="h-fit bg-neutral-800 absolute w-4/5 left-1/2 -translate-x-1/2 top-1/2 rounded-b-xl flex flex-col pt-7 pb-2 px-2 gap-2 z-50"
+                className="h-fit px-4 w-3/4 max-w-[600px] bg-neutral-800 absolute shadow-lg left-1/2 -translate-x-1/2 top-1/2 rounded-b-xl flex flex-col pt-7 pb-2  gap-2 z-50"
             >
                 {
-                    $searchResults.songs &&
+                    bestResult &&
                     <>
-                        <label className="font-bold text-xl text-white ">Songs</label>
-                        <RenderTrackSearchResults tracksInfo={$searchResults.songs.slice(0, 2)} handleDownload={handleDownload} />
+                        <label className="font-bold text-xl text-white ">Best result {bestResult.type}</label>
+                        <div className='h-32'>
+                            <Result
+                                name={bestResult.name}
+                                artistsOrOwner={bestResult.artistsOrOwner}
+                                inDatabase={bestResult.inDatabase}
+                                image={bestResult.image}
+                                url={bestResult.url}
+                            />
+                        </div>
                     </>
                 }
                 {
                     $searchResults.songs &&
                     <>
+                        <label className="font-bold text-xl text-white ">Songs</label>
+                        <ResultsWrapper>
+                            {$searchResults.songs.slice(0, 2).map((song, index) => <div className='h-12' ><Result
+                                key={"song" + index}
+                                name={song.name}
+                                artistsOrOwner={song.artists && song.artists.map(artist => artist.name).join(", ")}
+                                inDatabase={song.inDatabase}
+                                image={song.album.images[0].url}
+                                url={song.external_urls.spotify}
+                            /></div>)}
+                        </ResultsWrapper>
+                    </>
+                }
+                {
+                    $searchResults.albums &&
+                    <>
                         <label className="font-bold text-xl text-white ">Albums</label>
-                        <RenderAlbumSearchResults tracksInfo={$searchResults.albums.slice(0, 2)} handleDownload={handleDownload} />
+                        <ResultsWrapper>
+                            {$searchResults.albums.slice(0, 2).map((album, index) => <div className='h-12'><Result
+                                key={"album" + index}
+                                name={album.name}
+                                artistsOrOwner={album.artists && album.artists.map(artist => artist.name).join(", ")}
+                                inDatabase={album.inDatabase}
+                                image={album.images[0].url}
+                                url={album.external_urls.spotify}
+                            /></div>)}
+                        </ResultsWrapper>
+                    </>
+                }
+                {
+                    $searchResults.playlists &&
+                    <>
+                        <label className="font-bold text-xl text-white ">Playlists</label>
+                        <ResultsWrapper>
+                            {$searchResults.playlists.slice(0, 2).map((playlist, index) => <div className='h-12'><Result
+                                key={"playlist" + index}
+                                name={playlist.name}
+                                artistsOrOwner={playlist.owner.display_name}
+                                inDatabase={playlist.inDatabase}
+                                image={playlist.images[0].url}
+                                url={playlist.external_urls.spotify}
+                            /></div>)}
+                        </ResultsWrapper>
+                    </>
+                }
+                {
+                    $searchResults.artists &&
+                    <>
+                        <label className="font-bold text-xl text-white ">Playlists</label>
+                        <ResultsWrapper>
+                            {$searchResults.artists.slice(0, 2).map((artist, index) => <div className='h-12'><Result
+                                key={"playlist" + index}
+                                name={artist.name}
+                                artistsOrOwner=""
+                                inDatabase={true}
+                                image={artist.images[0].url}
+                                url={artist.external_urls.spotify}
+                            /></div>)}
+                        </ResultsWrapper>
                     </>
                 }
             </div >

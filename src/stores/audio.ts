235a28baf6446b22fc2fe4f92d73f7aa14d3b996
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-20 15:59:47 +0100

    Implemented web socket in audio store to get info of the song playing, etc

--- src/stores/audio.ts
+++ src/stores/audio.ts
@@ -1,20 +1,68 @@
 import { atom } from 'nanostores';
-import type { SongDB } from "@/lib/db"
+import type { SongDB, UserDB } from "@/lib/db"
 
-export const currentSong = atom<SongDB<"images" | "id" | "name" | "artists" | "albumId" | "albumName"> | undefined>(undefined);
+const websocket = new WebSocket("ws://localhost:4321/ws");
+websocket.onopen = (event) => {
+    console.log("Web socket open");
+};
+websocket.onmessage = (event) => {
+    console.log("Web socket message", event.data);
+};
+websocket.onclose = (event) => {
+    console.log("Web socket close");
+};
+
+type Queue = SongDB<"id" | "name" | "artists" | "images" | "duration">[]
+type CurrentSong = SongDB<"images" | "id" | "name" | "artists" | "albumId" | "albumName"> | undefined
+
+const json = await (await fetch("/api/user?q=currentSong,currentTime,queue,queueIndex")).json() as UserDB<"currentSong" | "currentTime" | "queue" | "queueIndex">
+
+let _currentSong = undefined
+let _queue: Queue = []
+let _queueIndex = json.queueIndex || 0
+
+if (json.currentSong) {
+    _currentSong = await (await fetch(`/api/song/${json.currentSong}?q=images,id,name,artists,albumId,albumName`)).json() as CurrentSong
+}
+
+if (json.queue.length > 0) {
+    _queue = await (await fetch(`/api/songs?songs=${json.queue.join()}&p=id,name,artists,images,duration`)).json() as Queue
+}
+
+const send = (json: any) => {
+    if (websocket.OPEN == websocket.readyState) {
+        websocket.send(JSON.stringify(json))
+    }
+}
+
+export const currentSong = atom<CurrentSong>(_currentSong);
 export const playing = atom<boolean>(false);
 export const currentTime = atom<number | undefined>(undefined);
 export const totalTime = atom<number | undefined>(undefined);
-export const queue = atom<SongDB<"id" | "name" | "artists" | "images" | "duration">[]>([]);
-export const queueIndex = atom<number | undefined>(undefined);
+export const queue = atom<Queue>(_queue);
+export const queueIndex = atom<number | undefined>(_queueIndex);
 
-const audio = new Audio()
+const audio = new Audio(_currentSong?.id ? `/api/song/audio/${_currentSong?.id}` : undefined)
+
+if (json.currentTime) {
+    audio.currentTime = json.currentTime
+}
 
 currentSong.subscribe((value) => {
+    send({ currentSong: currentSong.get()?.id })
     if (value) {
         playing.set(false)
         audio.src = `/api/song/audio/${value.id}`
     }
+})
+
+queue.subscribe((value) => {
+    console.log("queue value", value)
+    send({ queue: value.map(value => value.id) })
+})
+
+queueIndex.subscribe((value) => {
+    send({ queueIndex: value })
 })
 
 export const play = async () => {
@@ -48,6 +96,8 @@ export function setTime(time: number) {
 }
 
 export async function next() {
+    console.log(queueIndex.get())
+
     if (!queueIndex.get() && queueIndex.get() != 0) { return }
     queueIndex.set(queueIndex.get() as number + 1)
     await fetch(`/api/song/${queue.get()[queueIndex.get() as number].id}`).then(response => response.json()).then((data: SongDB) => {
@@ -61,6 +111,7 @@ audio.addEventListener("canplay", () => {
 
 audio.addEventListener("timeupdate", () => {
     currentTime.set(audio.currentTime)
+    send({ currentTime: audio.currentTime })
 })
 
 audio.addEventListener("play", () => {

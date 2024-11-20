2b197d8f1182845bfb2b1e26cbe20fb9939dde99
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-20 17:30:35 +0100

    Added types to search results

--- src/stores/searchResults.ts
+++ src/stores/searchResults.ts
@@ -1,4 +1,18 @@
-import { map } from 'nanostores';
+import type { SpotifyAlbum, SpotifyArtist, SpotifyPlaylist, SpotifyTrack } from '@/types/spotify';
+import { atom, map } from 'nanostores';
 
 
-export const searchResults = map<{ [key: string]: any }>({});
+
+export const searchQuery = atom<string>("supertramp")
+
+export const searchResults = map<{
+    albums: SpotifyAlbum[] | undefined,
+    playlists: SpotifyPlaylist[] | undefined,
+    songs: SpotifyTrack[] | undefined
+    artists: SpotifyArtist[] | undefined
+}>({
+    albums: undefined,
+    playlists: undefined,
+    songs: undefined,
+    artists: undefined
+});

79c2fc5b3d267272f5fd3dbb31764b4005cb6fc1
Nicorebo-18 <nicorebo18@gmail.com>
2024-11-25 22:59:29 +0100

    Add unpin function

---
+++ src/pages/api/unpin/[type]/[id].ts
@@ -0,0 +1,56 @@
+import {
+    db,
+    parseAlbum,
+    parsePlaylist,
+    parseUser,
+    type RawAlbumDB,
+    type RawPlaylistDB,
+    type RawUserDB,
+    type UserDB,
+    type UserDBPinnedLists,
+} from "@/lib/db";
+
+import type { APIContext } from "astro";
+
+export async function ALL(context: APIContext): Promise<Response> {
+    if (!context.locals.user) {
+        return new Response("Unauthenticated", { status: 401 });
+    }
+
+    const user = parseUser(
+        db
+            .prepare("SELECT pinnedLists FROM user WHERE id = ?")
+            .get(context.locals.user.id) as RawUserDB
+    ) as UserDB<"pinnedLists">;
+
+    const type = context.params.type;
+    const id = context.params.id;
+
+    // Comprobar si el elemento que se quiere desanclar existe
+    const listToUnpin = user?.pinnedLists?.find(
+        (list) => list.type === type && list.id === id
+    );
+
+    if (!listToUnpin) {
+        return new Response("Item not pinned", { status: 404 });
+    }
+
+    // Eliminar el elemento de la lista de pines
+    const updatedPinnedLists = user?.pinnedLists.filter(
+        (list) => list.type !== type || list.id !== id
+    );
+
+    // Actualizar la base de datos
+    if (updatedPinnedLists) {
+        db.prepare(`UPDATE user SET pinnedLists = ? WHERE id = ?`).run(
+            JSON.stringify(updatedPinnedLists),
+            context.locals.user.id
+        );
+    }
+
+    return new Response(JSON.stringify({ success: true }), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}

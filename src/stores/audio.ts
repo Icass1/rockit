07c76711fe8473989db2700ef6b03cdd7a646a91
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-27 20:16:44 +0100

    Fixed webSocket closing after some time

--- src/stores/audio.ts
+++ src/stores/audio.ts
@@ -3,21 +3,26 @@ import type { SongDB, UserDB } from "@/lib/db";
 
 let websocket: WebSocket;
 
-if (location.protocol == "https:") {
-    websocket = new WebSocket(`wss://${location.host}/ws`);
-} else {
-    websocket = new WebSocket(`ws://${location.host}/ws`);
+function startSocket() {
+    if (location.protocol == "https:") {
+        websocket = new WebSocket(`wss://${location.host}/ws`);
+    } else {
+        websocket = new WebSocket(`ws://${location.host}/ws`);
+    }
+
+    websocket.onopen = (event) => {
+        console.log("Web socket open");
+    };
+    websocket.onmessage = (event) => {
+        console.log("Web socket message", event.data);
+    };
+    websocket.onclose = (event) => {
+        console.log("Web socket close");
+        startSocket();
+    };
 }
 
-websocket.onopen = (event) => {
-    console.log("Web socket open");
-};
-websocket.onmessage = (event) => {
-    console.log("Web socket message", event.data);
-};
-websocket.onclose = (event) => {
-    console.log("Web socket close");
-};
+startSocket();
 
 type Queue = SongDB<"id" | "name" | "artists" | "images" | "duration">[];
 type CurrentSong =
@@ -92,24 +97,6 @@ export const play = async () => {
 export const pause = () => {
     audio.pause();
 };
-
-export function getTime(seconds: number) {
-    seconds = Math.round(seconds);
-
-    if (typeof seconds !== "number" || isNaN(seconds)) {
-        return "Invalid input";
-    }
-
-    // Calculate minutes and remaining seconds
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.round(seconds % 60);
-
-    // Format the result with leading zeros
-    const formattedMinutes = String(minutes).padStart(1, "0");
-    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
-
-    return `${formattedMinutes}:${formattedSeconds}`;
-}
 
 export function setTime(time: number) {
     audio.currentTime = time;

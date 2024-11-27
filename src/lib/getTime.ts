850f44fc060652ff48d60fdcad648ba976d07fb5
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-11-27 22:22:06 +0100

    Added getDate

--- src/lib/getTime.ts
+++ src/lib/getTime.ts
@@ -16,6 +16,33 @@ export function getTime(seconds: number) {
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 
+export function getDate(timeStamp: number) {
+    const date = new Date(timeStamp);
+
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "long" });
+    const year = date.getFullYear();
+
+    // Add suffix for the day
+    const daySuffix = getDaySuffix(day);
+
+    return `${day}${daySuffix} of ${month} of ${year}`;
+}
+
+function getDaySuffix(day: number) {
+    if (day >= 11 && day <= 13) return "th"; // Special case for 11, 12, 13
+    switch (day % 10) {
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        case 3:
+            return "rd";
+        default:
+            return "th";
+    }
+}
+
 export function getMinutes(seconds: number) {
     seconds = Math.round(seconds);
 

5bc683af6f90185649fb886127bbc38ce9a426c9
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-12-05 19:00:25 +0100

    Moved get album logic to lib

---
+++ src/lib/getAlbum.ts
@@ -0,0 +1,172 @@
+import type { SpotifyAlbum } from "@/types/spotify";
+import {
+    db,
+    parseAlbum,
+    parseSong,
+    type AlbumDB,
+    type RawAlbumDB,
+    type RawSongDB,
+    type SongDB,
+} from "./db";
+
+const BACKEND_URL = process.env.BACKEND_URL;
+
+type Song = SongDB<
+    | "images"
+    | "id"
+    | "name"
+    | "artists"
+    | "albumId"
+    | "albumName"
+    | "path"
+    | "duration"
+    | "discNumber"
+    | "trackNumber"
+>;
+
+interface ExtendedAlbumDB extends Omit<AlbumDB, "songs"> {
+    songs: SongDB[];
+}
+
+export default async function getAlbum(id: string): Promise<
+    | {
+          album: ExtendedAlbumDB;
+          songs: Song[];
+          discs: Song[][];
+          inDatabase: boolean;
+      }
+    | "error connecting to backend"
+    | "not found"
+> {
+    let album: ExtendedAlbumDB | undefined;
+    let inDatabase: boolean;
+    let songs: Song[];
+    let discs: Song[][];
+
+    album = parseAlbum(
+        db.prepare("SELECT * FROM album WHERE id = ?").get(id) as RawAlbumDB
+    );
+
+    if (album) {
+        inDatabase = true;
+
+        songs = album.songs
+            .map(
+                (
+                    songID: string
+                ):
+                    | SongDB<
+                          | "images"
+                          | "id"
+                          | "name"
+                          | "artists"
+                          | "albumId"
+                          | "albumName"
+                          | "path"
+                          | "duration"
+                          | "discNumber"
+                          | "trackNumber"
+                      >
+                    | undefined => {
+                    return parseSong(
+                        db
+                            .prepare(
+                                "SELECT images, id, name, artists, albumId, albumName, path, duration, discNumber, trackNumber FROM song WHERE id = ?"
+                            )
+                            .get(songID) as RawSongDB
+                    );
+                }
+            )
+            .filter((a) => typeof a !== "undefined");
+
+        songs.sort((a, b) => {
+            if (
+                typeof a?.trackNumber == "undefined" ||
+                typeof b?.trackNumber == "undefined"
+            ) {
+                return 0;
+            }
+            return a.trackNumber - b.trackNumber;
+        });
+
+        discs = Array(Math.max(...songs.map((song) => song?.discNumber || 0)))
+            .fill(1)
+            .map((_) => []);
+
+        songs.map((song) => {
+            if (song?.discNumber) {
+                discs[song?.discNumber - 1].push({ ...song });
+            }
+        });
+    } else {
+        inDatabase = false;
+        let response;
+        try {
+            response = await fetch(`${BACKEND_URL}/album/${id}`);
+        } catch (error) {
+            return "error connecting to backend";
+        }
+        if (!response.ok) {
+            return "not found";
+        }
+        const data: SpotifyAlbum = await response.json();
+        album = {
+            artists: data.artists,
+            images: data.images,
+            name: data.name,
+            releaseDate: data.release_date,
+            songs: data.tracks.items,
+        };
+
+        console.log(data.tracks.items);
+
+        songs = data.tracks.items.map((item) => {
+            const songDB = parseSong(
+                db
+                    .prepare(
+                        "SELECT images, id, name, artists, albumId, albumName, path, duration, discNumber, trackNumber FROM song WHERE id = ?"
+                    )
+                    .get(item.id) as RawSongDB
+            );
+
+            if (songDB) {
+                return songDB;
+            }
+
+            return {
+                id: item.id,
+                images: data.images,
+                name: item.name,
+                artists: data.artists,
+                albumId: data.id,
+                albumName: data.name,
+                path: "",
+                duration: item.duration_ms / 1000,
+                discNumber: item.disc_number,
+                trackNumber: item.track_number,
+            };
+        });
+
+        songs.sort((a, b) => {
+            if (
+                typeof a?.trackNumber == "undefined" ||
+                typeof b?.trackNumber == "undefined"
+            ) {
+                return 0;
+            }
+            return a.trackNumber - b.trackNumber;
+        });
+
+        discs = Array(Math.max(...songs.map((song) => song?.discNumber || 0)))
+            .fill(1)
+            .map((_) => []);
+
+        songs.map((song) => {
+            if (song?.discNumber) {
+                discs[song?.discNumber - 1].push({ ...song });
+            }
+        });
+    }
+
+    return { album, inDatabase, songs, discs };
+}

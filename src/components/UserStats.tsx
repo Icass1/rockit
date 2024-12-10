956b1a976fb998e77b3a2fe6a713918d6b345178
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-12-10 13:59:06 +0100

    Added songs listened and minutes listened graph and number to stats

--- src/components/UserStats.tsx
+++ src/components/UserStats.tsx
@@ -1,4 +1,4 @@
-import { getDate } from "@/lib/getTime";
+import { getDate, getMinutes } from "@/lib/getTime";
 import BarGraph from "./BarGraph.tsx";
 import { useEffect, useRef, useState } from "react";
 import type { SongForStats, Stats } from "@/lib/stats";
@@ -11,6 +11,9 @@ export default function UserStats() {
     const endDateInputRef = useRef<HTMLInputElement>(null);
 
     const [songsBarGraph, setSongsBarGraph] = useState<SongForStats[]>([]);
+
+    const start = new Date(startDate).getTime();
+    const end = new Date(endDate).getTime();
 
     const [data, setData] = useState<Stats>({
         songs: [],
@@ -81,6 +84,41 @@ export default function UserStats() {
         return yearNumber + "-" + monthString + "-" + dayString;
     };
 
+    const totalTimesPlayedSong = data.songs.length;
+    const totalMinutesListened = data.songs.reduce(
+        (partialSum, a) => partialSum + a.duration,
+        0
+    );
+
+    const minutesListenedPerDay: { [key: string]: number } = {};
+
+    console.log(Math.floor((end - start) / (3600 * 1000 * 24)) + 1);
+
+    Array(Math.floor((end - start) / (3600 * 1000 * 24)) + 1)
+        .fill(0)
+        .map((_, index) => {
+            const timeStamp = start + index * 3600 * 1000 * 24;
+            const date = new Date(timeStamp);
+            const datePlayed = `${date.getFullYear()}-${String(
+                date.getMonth() + 1
+            ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+            minutesListenedPerDay[datePlayed] = 0;
+        });
+
+    data.songs.map((song) => {
+        const date = new Date(song.timePlayed);
+        const datePlayed = `${date.getFullYear()}-${String(
+            date.getMonth() + 1
+        ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+        minutesListenedPerDay[datePlayed] += song.duration;
+    });
+
+    const maxMinutesListenedInADay = Math.max(
+        ...Object.values(minutesListenedPerDay)
+    );
+
+    console.log(minutesListenedPerDay);
+
     return (
         <>
             <label className="text-2xl font-semibold px-5">
@@ -139,6 +177,60 @@ export default function UserStats() {
                     <div>No registered data.</div>
                 ) : (
                     <>
+                        <div className="w-96 bg-gray-800 rounded-lg p-2 h-fit flex flex-col font-semibold">
+                            <label>{totalTimesPlayedSong} songs listened</label>
+                            <label>
+                                {getMinutes(totalMinutesListened)} minutes
+                                listened
+                            </label>
+                        </div>
+                        <div className="w-96 bg-gray-800 rounded-lg p-2 h-fit flex flex-col font-semibold">
+                            <label className="mb-2">
+                                Minutes listened per day
+                            </label>
+                            <div className="relative h-56">
+                                <div
+                                    className="absolute left-0 right-0 top-0 bottom-0 bg-gradient-to-t from-[#ee1086] to-[#fb6467] rounded"
+                                    style={{
+                                        clipPath: `polygon(${Object.entries(
+                                            minutesListenedPerDay
+                                        ).map(
+                                            (song) =>
+                                                `${
+                                                    ((new Date(
+                                                        song[0]
+                                                    ).getTime() -
+                                                        start) /
+                                                        (end - start)) *
+                                                    100
+                                                }% ${
+                                                    100 -
+                                                    (song[1] /
+                                                        maxMinutesListenedInADay) *
+                                                        100
+                                                }%`
+                                        )}, 100% 100%, 0% 100%)`,
+                                    }}
+                                />
+                                {Array(11)
+                                    .fill(0)
+                                    .map((_, index) => (
+                                        <div
+                                            className="bg-gray-600/50 absolute w-[1px] h-full"
+                                            style={{ left: `${index * 10}%` }}
+                                        />
+                                    ))}
+                                {Array(11)
+                                    .fill(0)
+                                    .map((_, index) => (
+                                        <div
+                                            className="bg-gray-600/50 absolute h-[1px] w-full"
+                                            style={{ top: `${index * 10}%` }}
+                                        />
+                                    ))}
+                            </div>
+                        </div>
+
                         <BarGraph
                             name="Most listened albums"
                             items={data.albums.map((album) => {

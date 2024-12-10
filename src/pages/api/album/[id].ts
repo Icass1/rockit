0c382141be1672a4cca94666ca16fdc0233d2b9f
IgnacioPCWSL <i.cassdolopez@gmail.com>
2024-12-10 14:01:05 +0100

    Search album in spotify api if not found and more info in songs

--- src/pages/api/album/[id].ts
+++ src/pages/api/album/[id].ts
@@ -1,19 +1,124 @@
 import type { APIContext } from "astro";
 
-import { db, parseAlbum, type RawAlbumDB } from "@/lib/db";
+import {
+    db,
+    parseAlbum,
+    parseSong,
+    type AlbumDB,
+    type RawAlbumDB,
+    type RawSongDB,
+    type SongDB,
+} from "@/lib/db";
+import type { SpotifyAlbum, SpotifyTrack } from "@/types/spotify";
+
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export async function GET(context: APIContext): Promise<Response> {
     const id = context.params.id as string;
 
-    const album = parseAlbum(
+    let album: AlbumDB | undefined;
+    album = parseAlbum(
         db.prepare("SELECT * FROM album WHERE id = ?").get(id) as RawAlbumDB
     );
+
+    if (!album) {
+        try {
+            const response = await fetch(`${BACKEND_URL}/album/${id}`);
+            if (response.ok) {
+                let responseAlbum = (await response.json()) as SpotifyAlbum;
+
+                album = {
+                    releaseDate: responseAlbum.release_date,
+                    dateAdded: undefined,
+                    artists: responseAlbum.artists,
+                    type: responseAlbum.type,
+                    discCount: responseAlbum.tracks.items.at(-1)
+                        ?.disc_number as number, // items.at(-1) should always exist
+                    genres: [],
+                    popularity: responseAlbum.popularity,
+                    id: responseAlbum.id,
+                    image: "",
+                    images: responseAlbum.images,
+                    name: responseAlbum.name,
+                    copyrights: responseAlbum.copyrights,
+                    songs: responseAlbum.tracks.items.map((item) => item.id),
+                };
+            } else {
+                console.error(
+                    "Backend fetch failed:",
+                    response.status,
+                    response.statusText
+                );
+            }
+        } catch (fetchError) {
+            console.error("Fetch error:", fetchError);
+        }
+    }
 
     if (!album) {
         return new Response("Album not found", { status: 404 });
     }
 
-    return new Response(JSON.stringify(album), {
+    const songs = await Promise.all(
+        album.songs.map(async (songId): Promise<SongDB | string> => {
+            let song;
+            try {
+                const rawSong = db
+                    .prepare("SELECT * FROM song WHERE id = ?")
+                    .get(songId) as RawSongDB;
+                if (rawSong) {
+                    song = parseSong(rawSong);
+                    if (song) {
+                        return song;
+                    }
+                }
+            } catch (dbError) {
+                console.error("Database error:", dbError);
+            }
+
+            try {
+                const response = await fetch(`${BACKEND_URL}/song/${songId}`);
+                if (response.ok) {
+                    song = (await response.json()) as SpotifyTrack;
+
+                    return {
+                        albumArtist: song.album.artists,
+                        albumId: song.album.id,
+                        albumName: song.album.name,
+                        albumType: song.album.type,
+                        name: song.name,
+                        artists: song.artists,
+                        dateAdded: undefined,
+                        date: song.album.release_date,
+                        discNumber: song.disc_number,
+                        downloadUrl: undefined,
+                        duration: song.duration_ms / 1000,
+                        trackNumber: song.track_number,
+                        id: song.id,
+                        image: undefined,
+                        images: song.album.images,
+                        copyright: "",
+                        genres: [],
+                        lyrics: "",
+                        path: "",
+                        popularity: song.popularity,
+                        publisher: "",
+                    };
+                } else {
+                    console.error(
+                        "Backend fetch failed:",
+                        response.status,
+                        response.statusText
+                    );
+                }
+            } catch (fetchError) {
+                console.error("Fetch error:", fetchError);
+            }
+            return songId;
+        })
+    );
+
+    return new Response(JSON.stringify({ ...album, songs }), {
         headers: {
             "Content-Type": "application/json",
         },

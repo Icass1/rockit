adc07d2c077421be6a3331325ad006bce0c3fdb5
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-01-13 21:27:35 +0100

    Added logout api page

---
+++ src/pages/api/logout.ts
@@ -0,0 +1,23 @@
+import { lucia } from "@/auth";
+import type { APIContext } from "astro";
+
+export async function ALL(context: APIContext): Promise<Response> {
+
+    const sessionCookie = context.cookies.get(lucia.sessionCookieName);
+
+    if (!sessionCookie) {
+        return new Response(JSON.stringify({ error: "No session found" }), {
+            status: 400,
+        });
+    }
+
+    try {
+        await lucia.invalidateSession(sessionCookie.value);
+        context.cookies.delete("session");
+        return new Response("/login", { status: 307 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Failed to logout" }), {
+            status: 500,
+        });
+    }
+}

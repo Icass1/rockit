4f78780142c11e650305d74bb703fb605d26ef2e
icassLaptop <i.cassdolopez@gmail.com>
2025-01-31 19:03:52 +0100

    Added download table

---
+++ src/lib/db/download.ts
@@ -0,0 +1,23 @@
+// ************************************************
+// ************** Download log stuff **************
+// ************************************************
+
+export interface DownloadDB {
+    id: string;
+    userId: string;
+    dateStarted: string;
+    dateEnded: string | undefined;
+    downloadURL: string;
+    status: string;
+}
+
+export const downloadQuery = `CREATE TABLE IF NOT EXISTS download (
+    id TEXT NOT NULL PRIMARY KEY UNIQUE,
+    userId TEXT NOT NULL,
+    dateStarted TEXT NOT NULL,
+    dateEnded TEXT,
+    downloadURL TEXT NOT NULL,
+    status TEXT NOT NULL
+)`;
+
+

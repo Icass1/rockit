7278f4d1464c922aef23a4ed4591306b73396238
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-02-25 16:03:20 +0100

    Added dragging to queue songs in mobile

--- src/components/Mobile/MobilePlayerUIQueue.tsx
+++ src/components/Mobile/MobilePlayerUIQueue.tsx
@@ -7,7 +7,7 @@ import {
 import { useStore } from "@nanostores/react";
 import { QueueSong } from "@/components/PlayerUI/QueueSong";
 import useWindowSize from "@/hooks/useWindowSize";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ContextMenu from "../ContextMenu/ContextMenu";
 import ContextMenuTrigger from "../ContextMenu/Trigger";
 import ContextMenuContent from "../ContextMenu/Content";
@@ -32,6 +32,15 @@ export default function MobilePlayerUIQueue({
     const { height } = useWindowSize();
     const scrollRef = useRef<HTMLDivElement>(null);
     const [queueScroll, setQueueScroll] = useState(0);
+    const [draggingSong, setDraggingSong] = useState<
+        | {
+              song: QueueElement;
+              index: number;
+          }
+        | undefined
+    >();
+    const [draggingPosY, setDraggingPosY] = useState(0);
+    const draggingPosYRef = useRef(0);
 
     useEffect(() => {
         if (!scrollRef.current) return;
@@ -63,6 +72,68 @@ export default function MobilePlayerUIQueue({
         // console.log(queue.get(), song, queueIndex.get());
     };
 
+    const touchStart = (
+        event: React.TouchEvent,
+        song: QueueElement,
+        index: number
+    ) => {
+        setDraggingSong({
+            song: song,
+            index: index,
+        });
+        setDraggingPosY(event.touches[0].clientY);
+        draggingPosYRef.current =
+            Math.round(event.touches[0].clientY * 100) / 100;
+    };
+
+    useEffect(() => {
+        if (!draggingSong) return;
+        const handleTouchMove = (event: TouchEvent) => {
+            setDraggingPosY(Math.round(event.touches[0].clientY * 100) / 100);
+            draggingPosYRef.current =
+                Math.round(event.touches[0].clientY * 100) / 100;
+        };
+
+        const handleTouchEnd = (event: TouchEvent) => {
+            setDraggingSong(undefined);
+
+            if (typeof scrollRef.current?.scrollTop != "number") return;
+            const indexInQueue = Math.floor(
+                (draggingPosYRef.current -
+                    185 +
+                    32 +
+                    scrollRef.current?.scrollTop) /
+                    64
+            );
+
+            if (draggingSong.index == indexInQueue) return;
+            else if (draggingSong.index < indexInQueue) {
+                queue.set([
+                    ...queue.get().slice(0, draggingSong.index),
+                    ...queue
+                        .get()
+                        .slice(draggingSong.index + 1, indexInQueue + 1),
+                    draggingSong.song,
+                    ...queue.get().slice(indexInQueue + 1),
+                ]);
+            } else {
+                queue.set([
+                    ...queue.get().slice(0, indexInQueue),
+                    draggingSong.song,
+                    ...queue.get().slice(indexInQueue, draggingSong.index),
+                    ...queue.get().slice(draggingSong.index + 1),
+                ]);
+            }
+        };
+
+        document.addEventListener("touchmove", handleTouchMove);
+        document.addEventListener("touchend", handleTouchEnd);
+        return () => {
+            document.removeEventListener("touchmove", handleTouchMove);
+            document.removeEventListener("touchend", handleTouchEnd);
+        };
+    }, [draggingSong]);
+
     const $lang = useStore(langData);
     if (!$lang) return;
 
@@ -70,12 +141,12 @@ export default function MobilePlayerUIQueue({
         <div
             id="MobilePlayerUIQueue"
             className={
-                "absolute w-full top-[80px] h-[calc(100%_-_5rem)] grid grid-rows-[40px_1fr] bg-gray-700 rounded-t-lg z-50 pt-4 px-2 transition-[top] duration-300 md:select-text select-none"
+                "absolute w-full top-[80px] h-[calc(100%_-_5rem)] grid grid-rows-[40px_1fr] bg-gray-700 rounded-t-lg z-50 pt-4 pl-2 transition-[top] duration-300 md:select-text select-none"
             }
             style={{ top: open ? "80px" : height + "px" }}
         >
             <label
-                className="h-full max-h-full min-h-0 font-semibold min-w-0 max-w-full w-full text-center text-xl "
+                className="h-full max-h-full min-h-0 font-semibold min-w-0 max-w-full w-full text-center text-xl text-nowrap "
                 onClick={() => {
                     setOpen(false);
                 }}
@@ -86,21 +157,65 @@ export default function MobilePlayerUIQueue({
                 <div
                     ref={scrollRef}
                     onScroll={(e) => setQueueScroll(e.currentTarget.scrollTop)}
-                    className="overflow-y-auto h-full max-h-full min-h-0 relative scroll-smooth"
+                    className={
+                        "h-full max-h-full min-h-0 relative " +
+                        (draggingSong == undefined
+                            ? " overflow-y-auto "
+                            : " overflow-y-hidden ")
+                    }
                 >
                     <div className="min-h-5"></div>
                     <div style={{ height: $queue.length * 64 }}></div>
                     {$queue.map((song, index) => {
                         if (!scrollRef.current) return;
 
-                        const top = index * 64;
+                        let top: number;
+
+                        let draggingTop: number | undefined;
+
+                        if (draggingSong)
+                            draggingTop = Math.max(
+                                draggingPosY -
+                                    185 +
+                                    scrollRef.current.scrollTop,
+                                0
+                            );
+
+                        if (
+                            draggingSong?.song.song.id == song.song.id &&
+                            typeof draggingTop == "number"
+                        ) {
+                            top = draggingTop;
+                        } else {
+                            top = index * 64;
+                        }
+
+                        if (
+                            typeof draggingTop == "number" &&
+                            typeof draggingSong?.index == "number" &&
+                            draggingSong?.index != index &&
+                            draggingTop - 32 < top &&
+                            draggingSong?.index * 64 > top
+                        ) {
+                            top += 64;
+                        }
+
+                        if (
+                            typeof draggingTop == "number" &&
+                            typeof draggingSong?.index == "number" &&
+                            draggingSong?.index != index &&
+                            draggingTop + 32 > top &&
+                            draggingSong?.index * 64 < top
+                        ) {
+                            top -= 64;
+                        }
 
                         if (
                             (scrollRef.current?.offsetHeight &&
                                 top >
                                     scrollRef.current?.offsetHeight +
                                         queueScroll) ||
-                            top < queueScroll - 64
+                            top < queueScroll - 74
                         ) {
                             return;
                         }
@@ -109,23 +224,33 @@ export default function MobilePlayerUIQueue({
                             <div
                                 key={song.song.id + song.index}
                                 id={song.song.id + song.index}
-                                className="absolute w-full transition-[top]"
+                                className={
+                                    "absolute w-full " +
+                                    (draggingSong?.song.song.id == song.song.id
+                                        ? " z-10 "
+                                        : " transition-[top] duration-500 ")
+                                }
                                 style={{
                                     top: `${top + 20}px`,
+                                    transitionTimingFunction:
+                                        "cubic-bezier(1,-0.53, 0.09, 1.58)",
                                 }}
                             >
                                 <ContextMenu>
                                     <ContextMenuTrigger>
-                                        <div className="grid grid-cols-[1fr_20px] items-center">
+                                        <div className="grid grid-cols-[1fr_45px] items-center">
                                             <div className="min-w-0 max-w-full w-full">
                                                 <QueueSong
                                                     key={song.index}
                                                     song={song}
                                                 />
                                             </div>
-                                            <div>
-                                                <GripVertical />
-                                            </div>
+                                            <GripVertical
+                                                className="h-full w-full p-1 pr-3"
+                                                onTouchStart={(e) =>
+                                                    touchStart(e, song, index)
+                                                }
+                                            />
                                         </div>
                                     </ContextMenuTrigger>
                                     <ContextMenuContent
@@ -157,7 +282,10 @@ export default function MobilePlayerUIQueue({
                                         </ContextMenuOption>
                                         <ContextMenuOption
                                             onClick={() => {
-                                                saveSongToIndexedDB(song.song, true);
+                                                saveSongToIndexedDB(
+                                                    song.song,
+                                                    true
+                                                );
                                             }}
                                         >
                                             <HardDriveDownload className="h-5 w-5" />

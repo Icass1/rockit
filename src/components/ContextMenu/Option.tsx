83367a9fc262e36da8c5948974d7ee74d648b426
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-03-18 14:45:41 +0100

    Minor fixes

--- src/components/ContextMenu/Option.tsx
+++ src/components/ContextMenu/Option.tsx
@@ -15,20 +15,20 @@ export default function ContextMenuOption({
     className?: string;
     disable?: boolean;
 }) {
-    // console.log(onClick);
 
     return (
         <div
             onClick={(e) => {
+                if (disable) return
                 e.stopPropagation();
                 onClick && onClick(e);
                 closeOnClick && setContextMenuOpen && setContextMenuOpen(false);
             }}
             className={
-                className +
+                (className ?? "") +
                 " context-menu-option md:hover:bg-neutral-700 rounded-sm p-2 cursor-pointer font-semibold text-sm flex flex-row items-center gap-2 " +
-                className +
-                (disable || !onClick ? " pointer-events-none text-neutral-500 " : "")
+                (className ?? "") +
+                (disable || !onClick ? " pointer-events-none opacity-50 " : "")
             }
         >
             {children}

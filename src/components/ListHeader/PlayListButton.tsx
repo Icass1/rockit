0ded8d7e388a1af42f78c3ced8529f5ebd1ee240
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-03-21 13:24:04 +0100

    Added download icon if list is not downloaded

--- src/components/ListHeader/PlayListButton.tsx
+++ src/components/ListHeader/PlayListButton.tsx
@@ -1,21 +1,27 @@
 import { playListHandleClick } from "../PlayList";
 import { pause, play, playing, queue, queueIndex } from "@/stores/audio";
 import { currentList } from "@/stores/currentList";
+import { downloadedLists, downloads } from "@/stores/downloads";
 import { useStore } from "@nanostores/react";
 
-import { Pause, Play } from "lucide-react";
+import { Download, Pause, Play } from "lucide-react";
 
 export default function PlayListButton({
     id,
     type,
+    inDatabase,
+    url,
 }: {
     id: string;
     type: string;
+    inDatabase: boolean;
+    url: string;
 }) {
     const $queue = useStore(queue);
     const $queueIndex = useStore(queueIndex);
     const $currentList = useStore(currentList);
     const $playing = useStore(playing);
+    const $downloadedLists = useStore(downloadedLists);
 
     const playingList =
         $queue &&
@@ -24,10 +30,41 @@ export default function PlayListButton({
         $queue.find((song) => song.index == $queueIndex)?.list?.type ==
             $currentList?.type;
 
+    let icon;
+
+    if (!inDatabase && !$downloadedLists.includes(id)) {
+        icon = (
+            <Download className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2" />
+        );
+    } else if (playingList && $playing) {
+        icon = (
+            <Pause
+                className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2"
+                fill="white"
+            />
+        );
+    } else {
+        icon = (
+            <Play
+                className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2"
+                fill="white"
+            />
+        );
+    }
+
     return (
         <div
             onClick={() => {
-                if (playingList && $playing) {
+                if (!inDatabase && !$downloadedLists.includes(id)) {
+                    fetch(`/api/start-download?url=${url}`).then((response) => {
+                        response.json().then((data) => {
+                            downloads.set([
+                                data.download_id,
+                                ...downloads.get(),
+                            ]);
+                        });
+                    });
+                } else if (playingList && $playing) {
                     pause();
                 } else if (playingList) {
                     play();
@@ -38,19 +75,9 @@ export default function PlayListButton({
                     });
                 }
             }}
-            className="absolute bottom-3 right-3 w-16 h-16 bg-gradient-to-r hover:scale-105 cursor-pointer transition-transform from-[#ee1086] to-[#fb6467] rounded-full shadow-[0px_0px_20px_3px_#0e0e0e]"
+            className="absolute bottom-3 right-3 md:w-20 md:h-20 h-16 w-16 bg-gradient-to-r md:hover:scale-105 cursor-pointer transition-transform from-[#ee1086] to-[#fb6467] rounded-full shadow-[0px_0px_20px_3px_#0e0e0e]"
         >
-            {playingList && $playing ? (
-                <Pause
-                    className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-8"
-                    fill="white"
-                />
-            ) : (
-                <Play
-                    className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-8"
-                    fill="white"
-                />
-            )}
+            {icon}
         </div>
     );
 }

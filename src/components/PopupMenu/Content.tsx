23e0ba07ab4668b1a7774a757d9e0f6c9957e3b3
Nicorebo-18 <Nicorebo18@gmail.com>
2025-03-28 17:52:35 +0100

    Increased z-index of popup

--- src/components/PopupMenu/Content.tsx
+++ src/components/PopupMenu/Content.tsx
@@ -111,7 +111,7 @@ export default function PopupMenuContent({
                     : undefined
             }
             onClick={() => _setPopupMenuOpen && _setPopupMenuOpen(false)}
-            className="fixed bg-neutral-800/90 top-0 left-0 w-full h-[calc(100%_-_4rem)] px-10 md:w-max md:h-auto rounded-md md:p-1 overflow-auto md:shadow-[0px_0px_20px_3px_#0e0e0e] z-20"
+            className="fixed bg-neutral-800/90 top-0 left-0 w-full h-[calc(100%_-_4rem)] px-10 md:w-max md:h-auto rounded-md md:p-1 overflow-auto md:shadow-[0px_0px_20px_3px_#0e0e0e] z-50"
             style={{
                 display: _popupMenuOpen ? "block" : "none",
             }}

361c700749b23cab328d878a0a54559c3f3b0160
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-04 15:46:20 +0200

    Added get image url from image id

---
+++ src/lib/getImageUrl.ts
@@ -0,0 +1,41 @@
+export function getImageUrl({
+    imageId,
+    width,
+    height,
+    fallback,
+    placeHolder,
+    blur = false,
+}: {
+    imageId: string | undefined;
+    width?: number;
+    height?: number;
+    fallback?: string;
+    placeHolder?: string;
+    blur?: boolean;
+}) {
+    if (!imageId) {
+        if (fallback) {
+            return fallback;
+        } else {
+            return placeHolder ?? "";
+        }
+    }
+
+    if (blur == true) {
+        return `/api/image/blur/${imageId}`;
+    }
+
+    if (
+        !width ||
+        !height ||
+        (typeof window != "undefined" && !window.navigator.onLine)
+    ) {
+        return `/api/image/${imageId}`;
+    }
+
+    if (typeof window == "undefined") {
+        return `/api/image/${imageId}_${width}x${height}`;
+    } else {
+        return `/api/image/${imageId}_${Math.round(width * window.devicePixelRatio)}x${Math.round(height * window.devicePixelRatio)}`;
+    }
+}

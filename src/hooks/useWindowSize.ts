10e5b57a15113416e16775833755a9d8c5039ff2
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-06 11:09:52 +0200

    Added window size hook

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 
 export default function useWindowSize() {
-    const [width, setWidth] = useState<number>(window.innerWidth);
-    const [height, setHeight] = useState<number>(window.innerHeight);
+    const [width, setWidth] = useState<number>(
+        typeof window === "undefined" ? 0 : window.innerWidth
+    );
+    const [height, setHeight] = useState<number>(
+        typeof window === "undefined" ? 0 : window.innerHeight
+    );
 
     useEffect(() => {
+        if (typeof window === "undefined") return;
         const handleResize = () => {
             setWidth(window.innerWidth);
             setHeight(window.innerHeight);

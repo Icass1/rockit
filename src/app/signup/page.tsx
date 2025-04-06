f64cd21198f5d4c4595a4c4d4f3e919466fb889c
icassCodeServer <i.cassdolopez@gmail.com>
2025-04-06 21:29:55 +0200

    Added signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,26 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import SignupModal from "@/components/Auth/SignupModal";
+
+export default async function SignupPage() {
+    const session = await getServerSession();
+    console.log("SignupPage session", { session });
+
+    if (session) {
+        redirect("/");
+    }
+
+    return (
+        <div
+            className="h-full w-full relative"
+            style={{
+                backgroundImage: "url(/background.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPositionY: "bottom",
+            }}
+        >
+            <SignupModal />
+        </div>
+    );
+}

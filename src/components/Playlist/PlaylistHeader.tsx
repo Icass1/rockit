94e53cca2f21fbc0e561a7e1ccf3407b1c032216
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-06 11:06:29 +0200

    Added playlist components

---
+++ src/components/Playlist/PlaylistHeader.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import ListOptions from "@/components/ListHeader/ListOptions";
+import type { PlaylistDB, PlaylistDBSong } from "@/lib/db/playlist";
+import type { SongDB } from "@/lib/db/song";
+import { getImageUrl } from "@/lib/getImageUrl";
+import { langData } from "@/stores/lang";
+import { useStore } from "@nanostores/react";
+import { Disc3, Heart, History } from "lucide-react";
+import PlayListButton from "../ListHeader/PlayListButton";
+import Image from "next/image";
+import { CSSProperties } from "react";
+
+function getMinutes(seconds: number) {
+    seconds = Math.round(seconds);
+
+    if (typeof seconds !== "number" || isNaN(seconds)) {
+        return "Invalid input";
+    }
+
+    // Calculate minutes and remaining seconds
+    const minutes = Math.floor(seconds / 60);
+
+    // Format the result with leading zeros
+    const formattedMinutes = String(minutes).padStart(2, "0");
+
+    return `${formattedMinutes}`;
+}
+
+export default function PlaylistHeader({
+    inDatabase,
+    id,
+    songs,
+    className,
+    playlist,
+    style,
+}: {
+    inDatabase: boolean;
+    id: string | "liked" | "most-listened" | "recent-mix";
+    songs: SongDB<
+        | "id"
+        | "images"
+        | "image"
+        | "name"
+        | "albumId"
+        | "duration"
+        | "artists"
+        | "path"
+        | "albumName"
+    >[];
+    className: string;
+    style?: CSSProperties;
+    playlist:
+        | PlaylistDB
+        | {
+              name: string;
+              songs: PlaylistDBSong[];
+              image: string;
+              images:
+                  | {
+                        url: string;
+                    }[]
+                  | undefined;
+              owner: string;
+          };
+}) {
+    const $lang = useStore(langData);
+    if (!$lang) return;
+
+    let coverIcon;
+
+    if (id == "liked") {
+        coverIcon = (
+            <Heart
+                className="w-1/2 h-1/2 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                fill="white"
+            />
+        );
+    } else if (id == "most-listened") {
+        coverIcon = (
+            <Disc3 className="w-1/2 h-1/2 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+        );
+    } else if (id == "recent-mix") {
+        coverIcon = (
+            <History className="w-1/2 h-1/2 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+        );
+    }
+    const specialPlaylist = ["liked", "most-listened", "recent-mix"].includes(
+        id
+    );
+
+    return (
+        <div
+            className={
+                "md:top-1/2 md:-translate-y-1/2 md:w-full md:max-w-96 px-10 md:px-0 flex flex-col gap-1 relative h-[26rem] md:max-h-none md:h-fit " +
+                className
+            }
+            style={style}
+        >
+            {/* Imagen de la playlist */}
+            <div className="relative overflow-hidden aspect-square rounded-xl md:rounded-md w-full h-auto md:bg-none bg-[rgb(15,15,15)]">
+                {specialPlaylist ? (
+                    <div
+                        className="relative rounded-md w-full h-full object-cover"
+                        style={{
+                            backgroundImage: "url(/rockit-background.png)",
+                            backgroundSize: "cover",
+                        }}
+                    >
+                        {coverIcon}
+                    </div>
+                ) : (
+                    <div className="w-full h-full">
+                        <Image
+                            width={370}
+                            height={370}
+                            alt={playlist.name}
+                            src={getImageUrl({
+                                imageId: playlist.image,
+                                fallback:
+                                    playlist?.images?.[0]?.url ??
+                                    "/rockit-background.png",
+                                height: 370,
+                                width: 370,
+                            })}
+                            className="absolute w-full h-full"
+                        />
+                        <PlayListButton
+                            id={id}
+                            type="playlist"
+                            inDatabase={inDatabase}
+                            url={`https://open.spotify.com/playlist/${id}`}
+                        />
+                    </div>
+                )}
+            </div>
+
+            {/* Nombre de la playlist */}
+            <div className="flex flex-row w-fit mx-auto items-center gap-3">
+                <label className="text-2xl font-semibold text-balance">
+                    {playlist.name}
+                </label>
+                <ListOptions
+                    url={`https://open.spotify.com/playlist/${id}`}
+                    type="playlist"
+                    id={id}
+                    image={playlist.image}
+                    inDatabase={inDatabase}
+                />
+            </div>
+
+            {/* Propietario */}
+            <label className="text-xl font-semibold text-stone-400 flex flex-wrap justify-center">
+                {playlist.owner}
+            </label>
+
+            {/* Información adicional */}
+            <label className="text-sm text-stone-400 text-center">
+                {playlist.songs.length} {$lang.songs} |{" "}
+                {getMinutes(
+                    songs.reduce((accumulator: number, song) => {
+                        return accumulator + (song?.duration || 0);
+                    }, 0)
+                )}{" "}
+                {$lang.minutes}
+            </label>
+        </div>
+    );
+}
---
+++ src/components/Playlist/PlaylistSongsView.tsx
@@ -0,0 +1,305 @@
+"use client";
+
+import type {
+    PlaylistDB,
+    PlaylistDBSong,
+    PlaylistDBSongWithAddedAt,
+} from "@/db/playlist";
+import PlaylistSong from "@/components/ListSongs/PlaylistSong";
+import { useEffect, useRef, useState } from "react";
+import { ArrowUp } from "lucide-react";
+import { currentList, currentListSongs } from "@/stores/currentList";
+import PlaylistHeader from "./PlaylistHeader";
+import { langData } from "@/stores/lang";
+import { useStore } from "@nanostores/react";
+import useWindowSize from "@/hooks/useWindowSize";
+
+type columnsType = "name" | "album" | "artist" | "addedAt" | "duration";
+
+export default function PlaylistSongsView({
+    songs,
+    id,
+    playlist,
+    inDatabase,
+}: {
+    id: string;
+    inDatabase: boolean;
+    songs: PlaylistDBSongWithAddedAt<
+        | "id"
+        | "images"
+        | "image"
+        | "name"
+        | "albumId"
+        | "duration"
+        | "artists"
+        | "path"
+        | "albumName"
+    >[];
+    playlist:
+        | PlaylistDB
+        | {
+              name: string;
+              songs: PlaylistDBSong[];
+              image: string;
+              images:
+                  | {
+                        url: string;
+                    }[]
+                  | undefined;
+              owner: string;
+          };
+}) {
+    useEffect(() => {
+        currentList.set({ id, type: "playlist" });
+    }, [id]);
+
+    const [filter, setFilter] = useState<{
+        column: columnsType;
+        ascending: boolean;
+    }>({ column: "addedAt", ascending: false });
+
+    const [songsToRender, setSongsToRender] = useState<
+        PlaylistDBSongWithAddedAt<
+            | "id"
+            | "images"
+            | "image"
+            | "name"
+            | "albumId"
+            | "duration"
+            | "artists"
+            | "path"
+            | "albumName"
+        >[]
+    >([]);
+
+    const divRef = useRef<HTMLDivElement>(null);
+    const [scroll, setScroll] = useState(0);
+    const innerWidth = useWindowSize().width;
+
+    const toggleFilter = (column: columnsType) => {
+        setFilter((value) => {
+            if (value.column == column) {
+                return { column: column, ascending: !value.ascending };
+            } else {
+                return { column: column, ascending: false };
+            }
+        });
+    };
+
+    useEffect(() => {
+        // console.warn("songsToRender", songsToRender);
+        currentListSongs.set(songsToRender);
+    }, [songsToRender]);
+
+    useEffect(() => {
+        switch (filter.column) {
+            case "name":
+                setSongsToRender(
+                    songs.toSorted((a, b) => {
+                        const nameA = a.name.toLowerCase();
+                        const nameB = b.name.toLowerCase();
+                        if (nameA < nameB) {
+                            return filter.ascending ? 1 : -1;
+                        }
+                        if (nameA > nameB) {
+                            return filter.ascending ? -1 : 1;
+                        }
+                        return 0;
+                    })
+                );
+                return;
+            case "addedAt":
+                setSongsToRender(
+                    songs.toSorted((a, b) => {
+                        if (!a.added_at || !b.added_at) {
+                            return 0;
+                        }
+                        return (
+                            (new Date(a?.added_at).getTime() -
+                                new Date(b?.added_at).getTime()) *
+                            (filter.ascending ? 1 : -1)
+                        );
+                    })
+                );
+                return;
+            case "album":
+                setSongsToRender(
+                    songs.toSorted((a, b) => {
+                        const albumNameA = a.albumName.toLowerCase();
+                        const albumNameB = b.albumName.toLowerCase();
+                        if (albumNameA < albumNameB) {
+                            return filter.ascending ? 1 : -1;
+                        }
+                        if (albumNameA > albumNameB) {
+                            return filter.ascending ? -1 : 1;
+                        }
+                        return 0;
+                    })
+                );
+                return;
+            case "artist":
+                setSongsToRender(
+                    songs.toSorted((a, b) => {
+                        const artistsA = a.artists
+                            .map((artist) => artist.name)
+                            .join("")
+                            .toLowerCase();
+                        const artistsB = b.artists
+                            .map((artist) => artist.name)
+                            .join("")
+                            .toLowerCase();
+                        if (artistsA < artistsB) {
+                            return filter.ascending ? 1 : -1;
+                        }
+                        if (artistsA > artistsB) {
+                            return filter.ascending ? -1 : 1;
+                        }
+                        return 0;
+                    })
+                );
+                return;
+            case "duration":
+                setSongsToRender(
+                    songs.toSorted((a, b) => {
+                        if (a.duration < b.duration) {
+                            return filter.ascending ? 1 : -1;
+                        }
+                        if (a.duration > b.duration) {
+                            return filter.ascending ? -1 : 1;
+                        }
+                        return 0;
+                    })
+                );
+
+                return;
+        }
+    }, [filter, songs]);
+
+    const renderColumn = (column: columnsType) => {
+        if (!$lang) return;
+        const columnNames = {
+            name: $lang.name,
+            album: $lang.album,
+            artist: $lang.artist,
+            addedAt: $lang.date_added,
+            duration: $lang.duration,
+        };
+
+        return (
+            <label
+                className={
+                    "hover:underline cursor-pointer flex flex-row items-center font-semibold select-none " +
+                    (filter.column == column ? "text-[#ec5588]" : "")
+                }
+                onClick={() => {
+                    toggleFilter(column);
+                }}
+            >
+                {columnNames[column]}
+                {renderArrow(column)}
+            </label>
+        );
+    };
+
+    const renderArrow = (column: columnsType) => {
+        if (filter.column == column) {
+            return (
+                <ArrowUp
+                    className={
+                        "w-5 h-5 transition-transform " +
+                        (filter.ascending ? "" : "rotate-180")
+                    }
+                ></ArrowUp>
+            );
+        }
+    };
+
+    const marginTop = 96;
+
+    const $lang = useStore(langData);
+    if (!$lang) return;
+
+    return (
+        <div
+            ref={divRef}
+            onScroll={(event) => {
+                setScroll(event.currentTarget.scrollTop);
+            }}
+            className="md:w-full min-h-0 max-h-full h-full overflow-auto relative md:pr-6"
+        >
+            <PlaylistHeader
+                id={id}
+                playlist={playlist}
+                songs={songs}
+                inDatabase={inDatabase}
+                className="md:hidden flex"
+            />
+            <div
+                className="hidden md:flex flex-row items-center gap-4 px-2 rounded text-stone-400 text-sm"
+                style={{ marginTop: `${marginTop}px` }}
+            >
+                <div className="w-10"></div>
+                <div className="flex flex-row w-full items-center justify-between">
+                    <div className="w-1/3 ">{renderColumn("name")}</div>
+                    <div className="flex-1 gap-x-1 flex flex-row w-1/2">
+                        {renderColumn("artist")}
+                        <label>•</label>
+                        {renderColumn("album")}
+                        <label>•</label>
+                        {renderColumn("addedAt")}
+                    </div>
+                    <div className="">{renderColumn("duration")}</div>
+                </div>
+            </div>
+            {/* 4px between songs */}
+            {/* 56px height of songs */}
+            {songsToRender.length == 0 && (
+                <label className="md:text-3xl text-xl font-bold block mt-10 relative md:top-1/2 md:-translate-y-1/2 w-full text-center text-neutral-400">
+                    There is nothing to show here
+                </label>
+            )}
+            {songsToRender.map((song, index) => {
+                if (song) {
+                    let top =
+                        index * (56 + 4) +
+                        (innerWidth < 768 ? 100 : 25) +
+                        marginTop;
+                    if (innerWidth < 768) top += 370;
+
+                    if (divRef.current) {
+                        const boundaries =
+                            divRef.current.getBoundingClientRect();
+
+                        if (
+                            top > boundaries.height + scroll ||
+                            top < scroll - (56 + 4)
+                        )
+                            return;
+                    }
+
+                    return (
+                        <div
+                            key={song.id + index}
+                            className="absolute h-[56px] left-0 right-0"
+                            style={{ top: `${top}px` }}
+                        >
+                            <PlaylistSong song={song} />
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div key={index} className="text-red-400">
+                            Song is undefined
+                        </div>
+                    );
+                }
+            })}
+            {/* 84px at the bottom*/}
+            <div
+                style={{
+                    minHeight: `${songsToRender.length * (4 + 56) + 100}px`,
+                }}
+            />
+        </div>
+    );
+}

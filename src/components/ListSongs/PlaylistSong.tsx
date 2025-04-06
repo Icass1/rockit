644741738704328d9cdbf787f3f8bbf5b1a38ed7
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-06 11:06:21 +0200

    Added list songs components

---
+++ src/components/ListSongs/PlaylistSong.tsx
@@ -0,0 +1,201 @@
+import { queue, queueIndex, songsInIndexedDB } from "@/stores/audio";
+import type { PlaylistDBSongWithAddedAt } from "@/db/playlist";
+import { getTime } from "@/lib/getTime";
+import LikeButton from "../LikeButton";
+import { EllipsisVertical, CheckCircle2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { currentList, currentListSongs } from "@/stores/currentList";
+import { useStore } from "@nanostores/react";
+import { songHandleClick } from "./HandleClick";
+import SongContextMenu from "@/components/ListSongs/SongContextMenu";
+import { downloadedSongs } from "@/stores/downloads";
+import { getImageUrl } from "@/lib/getImageUrl";
+import { networkStatus } from "@/stores/networkStatus";
+import {
+    PopupMenu,
+    PopupMenuContent,
+    PopupMenuTrigger,
+} from "../PopupMenu/PopupMenu";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function PlaylistSong({
+    song,
+}: {
+    song: PlaylistDBSongWithAddedAt<
+        | "name"
+        | "albumId"
+        | "duration"
+        | "artists"
+        | "path"
+        | "albumName"
+        | "image"
+        | "id"
+        | "images"
+    >;
+}) {
+    const [hovered, setHovered] = useState(false);
+
+    const $queue = useStore(queue);
+    const $queueIndex = useStore(queueIndex);
+    const $currentList = useStore(currentList);
+    const $networkStatus = useStore(networkStatus);
+    const $songsInIndexedDB = useStore(songsInIndexedDB);
+    const $currentListSongs = useStore(currentListSongs);
+    const $downloadedSongs = useStore(downloadedSongs);
+
+    const router = useRouter();
+
+    const [_song, setSong] =
+        useState<
+            PlaylistDBSongWithAddedAt<
+                | "name"
+                | "albumId"
+                | "duration"
+                | "artists"
+                | "path"
+                | "albumName"
+                | "image"
+                | "id"
+                | "images"
+            >
+        >(song);
+
+    useEffect(() => {
+        if ($downloadedSongs.includes(_song.id)) {
+            fetch(`/api/song/${_song.id}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setSong(data);
+
+                    currentListSongs.set(
+                        currentListSongs.get().map((song) => {
+                            if (song.id == data.id) {
+                                return data;
+                            } else {
+                                return song;
+                            }
+                        })
+                    );
+                });
+        }
+    }, [$downloadedSongs, _song.id]);
+
+    if (!$queue) return <div>!queue</div>;
+
+    return (
+        <SongContextMenu song={_song}>
+            <div
+                className={
+                    "flex flex-row items-center gap-2 md:gap-4 transition-colors px-2 py-[0.5rem] rounded select-none md:select-text " +
+                    // If offline and the song is not saved to indexedDB or the song is not in the server database, disable that song
+                    ((($networkStatus == "offline" &&
+                        !songsInIndexedDB.get()?.includes(_song.id)) ||
+                        !_song.path) &&
+                        "opacity-40 pointer-events-none ") +
+                    // If the song is playing and is from this playlist, change color, if the song has been added to the queue clicking the album, it won't show the color
+                    ($queue.find((song) => song.index == $queueIndex)?.list
+                        ?.id == $currentList?.id &&
+                    $queue.find((song) => song.index == $queueIndex)?.list
+                        ?.type == $currentList?.type &&
+                    $queue.find((song) => song.index == $queueIndex)?.song.id ==
+                        _song.id
+                        ? " text-[#ec5588]"
+                        : "")
+                }
+                onClick={() => songHandleClick(song, $currentListSongs)}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            >
+                {/* Imagen */}
+                <div className="h-10 w-auto aspect-square rounded relative">
+                    <Image
+                        alt={_song.name}
+                        width={40}
+                        height={40}
+                        src={getImageUrl({
+                            imageId: _song.image,
+                            width: 40,
+                            height: 40,
+                            fallback: _song?.images[0]?.url,
+                            placeHolder: "/song-placeholder.png",
+                        })}
+                        className="rounded absolute top-0 bottom-0 left-0 right-0"
+                    />
+                </div>
+
+                {/* Contenedor principal */}
+                <div className="grid grid-cols-[1fr_min-content] md:grid-cols-[1fr_2fr] w-full items-center">
+                    {/* Título (alineado a la izquierda) */}
+                    <div className="max-w-full min-w-0 w-full">
+                        <span
+                            className="text-base font-semibold block w-fit max-w-full truncate pr-1 cursor-pointer"
+                            // onClick={(event) => {
+                            //     navigate(`/song/${_song.id}`);
+                            //     event.stopPropagation();
+                            // }}
+                        >
+                            {_song.name}
+                        </span>
+                    </div>
+                    <div className="w-full h-full max-w-full min-w-0 flex flex-row items-center">
+                        <div className="hidden flex-1 md:flex flex-row gap-2 truncate pr-2">
+                            <label className="text-md truncate max-w-[50%]">
+                                {_song.artists.map((artist, index) => (
+                                    <span
+                                        className="md:hover:underline cursor-pointer"
+                                        key={index}
+                                        onClick={(event) => {
+                                            router.push(`/artist/${artist.id}`);
+                                            event.stopPropagation();
+                                        }}
+                                    >
+                                        {artist.name}
+                                        {index < _song.artists.length - 1
+                                            ? ", "
+                                            : ""}
+                                    </span>
+                                ))}
+                            </label>
+                            <span className="mx-1">•</span>
+
+                            <span
+                                className="md:hover:underline text-md truncate cursor-pointer"
+                                onClick={(event) => {
+                                    router.push(`/album/${_song.albumId}`);
+                                    event.stopPropagation();
+                                }}
+                            >
+                                {_song.albumName || "Ablum desconocido"}
+                            </span>
+                        </div>
+
+                        {/* Botones y tiempo (alineados a la derecha) */}
+                        <div className="flex items-center gap-x-2 md:gap-4 ml-auto w-fit">
+                            {$songsInIndexedDB?.includes(_song.id) && (
+                                <div className="min-h-6 min-w-6">
+                                    <CheckCircle2 className="flex h-full w-full text-[#ec5588]" />
+                                </div>
+                            )}
+                            <LikeButton song={_song} />
+                            {/* <EllipsisVertical className="text-gray-400 flex md:hidden md:hover:text-white md:hover:scale-105" /> */}
+
+                            <label className="text-sm text-white/80 select-none min-w-7 flex justify-center items-center">
+                                {hovered && window.innerWidth > 768 ? (
+                                    <PopupMenu>
+                                        <PopupMenuTrigger>
+                                            <EllipsisVertical className="text-gray-400 md:hover:text-white md:hover:scale-105" />
+                                        </PopupMenuTrigger>
+                                        <PopupMenuContent></PopupMenuContent>
+                                    </PopupMenu>
+                                ) : (
+                                    getTime(_song.duration)
+                                )}
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </SongContextMenu>
+    );
+}
---
+++ src/components/ListSongs/SongContextMenu.tsx
@@ -0,0 +1,309 @@
+"use client";
+
+import {
+    Copy,
+    Download,
+    HardDriveDownload,
+    Link,
+    ListEnd,
+    ListPlusIcon,
+    ListStart,
+    ListX,
+    PlayCircle,
+    Share2,
+} from "lucide-react";
+import ContextMenuContent from "../ContextMenu/Content";
+import ContextMenu from "../ContextMenu/ContextMenu";
+import ContextMenuOption from "../ContextMenu/Option";
+import ContextMenuTrigger from "../ContextMenu/Trigger";
+import { likedSongs } from "@/stores/likedList";
+import type { SongDB } from "@/db/song";
+import { type ReactNode } from "react";
+import { useStore } from "@nanostores/react";
+import { queue, queueIndex, saveSongToIndexedDB } from "@/stores/audio";
+import { currentList, currentListSongs } from "@/stores/currentList";
+import ContextMenuSplitter from "../ContextMenu/Splitter";
+import { songHandleClick } from "./HandleClick";
+import { langData } from "@/stores/lang";
+import { getImageUrl } from "@/lib/getImageUrl";
+import SubContextMenu from "../ContextMenu/SubContextMenu/ContextMenu";
+import SubContextMenuTrigger from "../ContextMenu/SubContextMenu/Trigger";
+import SubContextMenuContent from "../ContextMenu/SubContextMenu/Content";
+import { networkStatus } from "@/stores/networkStatus";
+import { userLists } from "@/stores/userLists";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function SongContextMenu({
+    children,
+    song,
+}: {
+    children: ReactNode;
+    song: SongDB<
+        | "id"
+        | "name"
+        | "artists"
+        | "albumName"
+        | "albumId"
+        | "duration"
+        | "image"
+        | "path"
+    >;
+}) {
+    const $likedSongs = useStore(likedSongs);
+    const $currentListSongs = useStore(currentListSongs);
+    const $lang = useStore(langData);
+    const $networkStatus = useStore(networkStatus);
+
+    const offline = $networkStatus == "offline";
+
+    const $userLists = useStore(userLists);
+
+    const router = useRouter();
+
+    if (!$lang) return;
+
+    return (
+        <ContextMenu>
+            <ContextMenuTrigger>{children}</ContextMenuTrigger>
+            <ContextMenuContent
+                cover={getImageUrl({ imageId: song?.image })}
+                title={song.name}
+                description={
+                    song.albumName +
+                    " • " +
+                    song.artists.map((artist) => artist.name).join(", ")
+                }
+            >
+                <ContextMenuOption
+                    onClick={() => songHandleClick(song, $currentListSongs)}
+                >
+                    <PlayCircle className="h-5 w-5" />
+                    {$lang.play_song}
+                </ContextMenuOption>
+                <ContextMenuOption
+                    disable={offline}
+                    onClick={() => {
+                        if (likedSongs.get().includes(song.id)) {
+                            fetch(`/api/like/${song.id}`, {
+                                method: "DELETE",
+                            }).then((response) => {
+                                if (response.ok) {
+                                    // Remove song to liked songs store
+                                    likedSongs.set(
+                                        likedSongs
+                                            .get()
+                                            .filter(
+                                                (likedSong) =>
+                                                    likedSong != song.id
+                                            )
+                                    );
+                                } else {
+                                    console.log("Error");
+                                    // Tell user like request was unsuccessful
+                                }
+                            });
+                        } else {
+                            fetch(`/api/like/${song.id}`, {
+                                method: "POST",
+                            }).then((response) => {
+                                if (response.ok) {
+                                    // Add song to liked songs store
+                                    likedSongs.set([
+                                        ...likedSongs.get(),
+                                        song.id,
+                                    ]);
+                                } else {
+                                    console.log("Error");
+                                    // Tell user like request was unsuccessful
+                                }
+                            });
+                        }
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill={"transparent"}
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className={
+                            "lucide lucide-hand-metal text-white h-5 w-5"
+                        }
+                    >
+                        <rect
+                            x="6"
+                            y="10"
+                            width="11"
+                            height="7"
+                            strokeLinejoin="miter"
+                            strokeWidth="0"
+                        />
+                        <path d="M18 12.5V10a2 2 0 0 0-2-2 2 2 0 0 0-2 2v1.4"></path>
+                        <path d="M14 11V9a2 2 0 1 0-4 0v2"></path>
+                        <path d="M10 11V5a2 2 2 1 0-4 0v9"></path>
+                        <path d="m7 15-1.76-1.76a2 2 0 0 0-2.83 2.82l3.6 3.6C7.5 21.14 9.2 22 12 22h2a8 8 0 0 0 8-8V7a2 2 0 1 0-4 0v5"></path>
+                    </svg>
+                    {$likedSongs.includes(song.id)
+                        ? $lang.remove_from_liked
+                        : $lang.add_to_liked}
+                </ContextMenuOption>
+                <ContextMenuOption
+                    onClick={() => {
+                        const tempQueue = queue.get();
+                        if (!tempQueue) return;
+
+                        const index = tempQueue.findIndex(
+                            (_song) => _song.index == queueIndex.get()
+                        );
+
+                        queue.set([
+                            ...tempQueue.slice(0, index + 1),
+                            {
+                                list: currentList.get(),
+                                index:
+                                    Math.max(
+                                        ...tempQueue.map((_song) => _song.index)
+                                    ) + 1,
+                                song: song,
+                            },
+                            ...tempQueue.slice(index + 1),
+                        ]);
+                    }}
+                >
+                    <ListStart className="h-5 w-5" />
+                    {$lang.play_next}
+                </ContextMenuOption>
+
+                <ContextMenuOption
+                    onClick={() => {
+                        const tempQueue = queue.get();
+                        if (!tempQueue) return;
+                        queue.set([
+                            ...tempQueue,
+                            {
+                                song: song,
+                                index:
+                                    Math.max(
+                                        ...tempQueue.map(
+                                            (queueSong) => queueSong.index
+                                        )
+                                    ) + 1,
+                                list: currentList.get(),
+                            },
+                        ]);
+                    }}
+                >
+                    <ListEnd className="h-5 w-5" />
+                    {$lang.add_to_queue}
+                </ContextMenuOption>
+                <SubContextMenu>
+                    <SubContextMenuTrigger disable={offline}>
+                        <ListPlusIcon className="w-5 h-5" />
+                        {$lang.add_song_to_playlist}
+                    </SubContextMenuTrigger>
+                    <SubContextMenuContent>
+                        {$userLists.map((list) => (
+                            <ContextMenuOption
+                                key={list.id}
+                                onClick={() => {
+                                    fetch("/api/playlist/add-song", {
+                                        method: "POST",
+                                        body: JSON.stringify({
+                                            songId: song.id,
+                                            playlistId: list.id,
+                                        }),
+                                    });
+                                }}
+                            >
+                                <Image
+                                    width={24}
+                                    height={24}
+                                    alt={list.name}
+                                    className="h-6 w-6"
+                                    src={getImageUrl({
+                                        imageId: list?.image,
+                                        placeHolder: "/rockit-background.png",
+                                        width: 24,
+                                        height: 24,
+                                    })}
+                                />
+                                {list.name}
+                            </ContextMenuOption>
+                        ))}
+                    </SubContextMenuContent>
+                </SubContextMenu>
+                <ContextMenuSplitter />
+                <ContextMenuOption
+                    disable={typeof navigator.share == "undefined" || offline}
+                    onClick={() => {
+                        navigator.share({
+                            title: "RockIt!",
+                            text: `${song.name} ${song.albumName} ${song.artists
+                                .map((artist) => artist.name)
+                                .join(", ")}`,
+                            url: `/song/${song.id}`,
+                        });
+                    }}
+                >
+                    <Share2 className="h-5 w-5" />
+                    {$lang.share_song}
+                </ContextMenuOption>
+                <ContextMenuOption
+                    onClick={() => {
+                        navigator.clipboard.writeText(
+                            location.origin + `/song/${song.id}`
+                        );
+                    }}
+                >
+                    <Copy className="h-5 w-5" />
+                    {$lang.copy_song_url}
+                </ContextMenuOption>
+                <ContextMenuSplitter />
+                <ContextMenuOption className="hover:bg-red-700" disable>
+                    <ListX className="h-5 w-5" />
+                    {$lang.remove_from_queue}
+                </ContextMenuOption>
+                <ContextMenuOption className="hover:bg-red-700" disable>
+                    <ListX className="h-5 w-5" />
+                    {$lang.remove_from_playlist}
+                </ContextMenuOption>
+                <ContextMenuSplitter />
+                <ContextMenuOption disable>
+                    <Download className="h-5 w-5" />
+                    {$lang.download_mp3}
+                </ContextMenuOption>
+                <ContextMenuOption
+                    disable={offline}
+                    onClick={() => {
+                        saveSongToIndexedDB(song, true);
+                    }}
+                >
+                    <HardDriveDownload className="h-5 w-5" />
+                    {$lang.download_song_to_device}
+                </ContextMenuOption>
+                <ContextMenuSplitter />
+                <ContextMenuOption
+                    onClick={() => {
+                        router.push(`/artist/${song.artists[0].id}`);
+                    }}
+                >
+                    <Link className="h-5 w-5" />
+                    {$lang.go_to_artist}
+                </ContextMenuOption>
+                <ContextMenuOption
+                    onClick={() => {
+                        router.push(`/album/${song.albumId}`);
+                    }}
+                >
+                    <Link className="h-5 w-5" />
+                    {$lang.go_to_album}
+                </ContextMenuOption>
+            </ContextMenuContent>
+        </ContextMenu>
+    );
+}

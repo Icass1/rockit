9997489cb627f4ebc7534efe73eababf97130421
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-06 11:03:54 +0200

    Added lang api route

---
+++ src/app/api/lang/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db/db";
+
+import { readFile } from "fs/promises";
+import { getSession } from "@/lib/auth/getSession";
+import { UserDB } from "@/lib/db/user";
+
+export async function GET(request: Request) {
+    const session = await getSession();
+
+    const searchParams = new URL(request.url).searchParams;
+
+    const requestLang = searchParams.get("lang");
+
+    let lang: string;
+
+    if (!session.user) {
+        if (requestLang) {
+            lang = requestLang;
+        } else {
+            return new Response("Unauthenticated", { status: 401 });
+        }
+    } else {
+        lang = (
+            db
+                .prepare("SELECT lang FROM user WHERE id = ?")
+                .get(session.user.id) as UserDB<"lang">
+        ).lang;
+    }
+
+    const fileBuffer = await readFile(`src/lang/${lang}.json`, "utf-8");
+
+    return NextResponse.json({ lang: lang, langFile: JSON.parse(fileBuffer) });
+}

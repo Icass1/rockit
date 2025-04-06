a478b5f2f85e156041e7cd1183d1b089161bb441
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-06 11:19:51 +0200

    Added setResources while downloading

--- src/lib/downloadResources.ts
+++ src/lib/downloadResources.ts
@@ -1,22 +1,22 @@
-import * as cheerio from "cheerio";
+// import * as cheerio from "cheerio";
 import type { Dispatch, SetStateAction } from "react";
 import { openRockItIndexedDB } from "./indexedDB";
 
-function extractImports(jsFileContent: string) {
-    const importRegex =
-        /import\s+["']([^"']+)["'];|import(?:["'\s]*[\w*{}\s,]*from\s*)?["']([^"']+)["']/g;
-    const resources = [];
-    let match;
+// function extractImports(jsFileContent: string) {
+//     const importRegex =
+//         /import\s+["']([^"']+)["'];|import(?:["'\s]*[\w*{}\s,]*from\s*)?["']([^"']+)["']/g;
+//     const resources = [];
+//     let match;
 
-    while ((match = importRegex.exec(jsFileContent)) !== null) {
-        const resourcePath = match[1] || match[2]; // Match either type of import path
-        if (resourcePath) {
-            resources.push(resourcePath);
-        }
-    }
+//     while ((match = importRegex.exec(jsFileContent)) !== null) {
+//         const resourcePath = match[1] || match[2]; // Match either type of import path
+//         if (resourcePath) {
+//             resources.push(resourcePath);
+//         }
+//     }
 
-    return resources;
-}
+//     return resources;
+// }
 
 // function fillFilesIndexedDB(filesStore: IDBObjectStore) {
 //     console.warn("fillFilesIndexedDB");
@@ -63,82 +63,106 @@ function extractImports(jsFileContent: string) {
 //     });
 // }
 
-function fetchResource(url: string, database: IDBDatabase): Promise<string[]> {
-    return new Promise((resolve) => {
-        // If a file is some thing like this, ./index.D5cSYS0A.js in an import statement, the route to that file is /_astro/index.D5cSYS0A.js
-        if (/\.[A-Za-z0-9\-_]{8}\./.test(url)) {
-            url = url.replace("./", "/_astro/");
-        }
+// function fetchResource(url: string, database: IDBDatabase): Promise<string[]> {
+//     return new Promise((resolve) => {
+//         // If a file is some thing like this, ./index.D5cSYS0A.js in an import statement, the route to that file is /_astro/index.D5cSYS0A.js
+//         if (/\.[A-Za-z0-9\-_]{8}\./.test(url)) {
+//             url = url.replace("./", "/_astro/");
+//         }
 
-        fetch(url).then((response) => {
-            if (response.ok) {
-                response.blob().then((fileContent) => {
-                    if (
-                        response.headers
-                            .get("content-type")
-                            ?.includes("text") ||
-                        response.headers
-                            .get("content-type")
-                            ?.includes("javascript")
-                    ) {
-                        const resources: string[] = [];
+//         fetch(url).then((response) => {
+//             if (response.ok) {
+//                 response.blob().then((fileContent) => {
+//                     if (
+//                         response.headers
+//                             .get("content-type")
+//                             ?.includes("text") ||
+//                         response.headers
+//                             .get("content-type")
+//                             ?.includes("javascript")
+//                     ) {
+//                         const resources: string[] = [];
 
-                        fileContent.text().then((text) => {
-                            const loadedCheerio = cheerio.load(text);
-                            loadedCheerio("script[src]").each((_, el) => {
-                                const src = loadedCheerio(el).attr("src");
-                                if (src) {
-                                    resources.push(src);
-                                }
-                            });
+//                         fileContent.text().then((text) => {
+//                             const loadedCheerio = cheerio.load(text);
+//                             loadedCheerio("script[src]").each((_, el) => {
+//                                 const src = loadedCheerio(el).attr("src");
+//                                 if (src) {
+//                                     resources.push(src);
+//                                 }
+//                             });
 
-                            loadedCheerio("link").each((_, el) => {
-                                const src = loadedCheerio(el).attr("href");
-                                if (src) {
-                                    resources.push(src);
-                                }
-                            });
-                            loadedCheerio("astro-island").each((_, el) => {
-                                let src =
-                                    loadedCheerio(el).attr("component-url");
-                                if (src) {
-                                    resources.push(src);
-                                }
-                                src = loadedCheerio(el).attr("renderer-url");
-                                if (src) {
-                                    resources.push(src);
-                                }
-                            });
-                            loadedCheerio("img[src]").each((_, el) => {
-                                const src = loadedCheerio(el).attr("src");
-                                if (src) {
-                                    resources.push(src);
-                                }
-                            });
-                            resources.push(...extractImports(text));
+//                             loadedCheerio("link").each((_, el) => {
+//                                 const src = loadedCheerio(el).attr("href");
+//                                 if (src) {
+//                                     resources.push(src);
+//                                 }
+//                             });
+//                             loadedCheerio("astro-island").each((_, el) => {
+//                                 let src =
+//                                     loadedCheerio(el).attr("component-url");
+//                                 if (src) {
+//                                     resources.push(src);
+//                                 }
+//                                 src = loadedCheerio(el).attr("renderer-url");
+//                                 if (src) {
+//                                     resources.push(src);
+//                                 }
+//                             });
+//                             loadedCheerio("img[src]").each((_, el) => {
+//                                 const src = loadedCheerio(el).attr("src");
+//                                 if (src) {
+//                                     resources.push(src);
+//                                 }
+//                             });
+//                             resources.push(...extractImports(text));
 
-                            resolve(resources);
-                        });
-                    } else {
-                        resolve([]);
-                    }
+//                             resolve(resources);
+//                         });
+//                     } else {
+//                         resolve([]);
+//                     }
 
-                    const transaction = database?.transaction(
-                        "files",
-                        "readwrite"
-                    );
-                    const filesStore = transaction?.objectStore("files");
-                    filesStore?.put({
-                        url: url,
-                        fileContent,
-                        contentType: response.headers.get("content-type"),
-                    });
-                });
-            } else {
-                resolve([]);
-            }
-        });
+//                     const transaction = database?.transaction(
+//                         "files",
+//                         "readwrite"
+//                     );
+//                     const filesStore = transaction?.objectStore("files");
+//                     filesStore?.put({
+//                         url: url,
+//                         fileContent,
+//                         contentType: response.headers.get("content-type"),
+//                     });
+//                 });
+//             } else {
+//                 resolve([]);
+//             }
+//         });
+//     });
+// }
+
+async function downloadFile(
+    url: string,
+    database: IDBDatabase,
+    setResources?: Dispatch<SetStateAction<string[]>> | undefined
+) {
+    console.log(`Downloading ${url}`);
+
+    if (setResources) setResources((value) => [...value, url]);
+
+    const response = await fetch(url);
+    const fileContent = await response.text();
+
+    const transaction = database?.transaction("file", "readwrite");
+    const filesStore = transaction?.objectStore("file");
+    filesStore?.put({
+        url: "/",
+        fileContent,
+        contentType: response.headers.get("content-type"),
     });
+
+    if (setResources)
+        setResources((value) => value.filter((value) => value != url));
 }
 
 export async function downloadResources({
@@ -158,23 +182,31 @@ export async function downloadResources({
         database = await openRockItIndexedDB();
     }
 
-    const responseRoot = await fetch("/");
-    const fileContent = await responseRoot.text();
+    downloadFile("/", database, setResources);
 
-    const transaction = database?.transaction("file", "readwrite");
-    const filesStore = transaction?.objectStore("file");
-    filesStore?.put({
-        url: "/",
-        fileContent,
-        contentType: responseRoot.headers.get("content-type"),
-    });
+    // const responseRoot = await fetch("/");
+    // const fileContent = await responseRoot.text();
+
+    // const transaction = database?.transaction("file", "readwrite");
+    // const filesStore = transaction?.objectStore("file");
+    // filesStore?.put({
+    //     url: "/",
+    //     fileContent,
+    //     contentType: responseRoot.headers.get("content-type"),
+    // });
 
     const responseStaticTree = await fetch("/api/get-static-tree");
     const staticTree: string[] = await responseStaticTree.json();
 
     await Promise.all(
         staticTree.map(async (path) => {
-            console.log(`Downloading /_next/static/${path}`);
+
+            return downloadFile(
+                `/_next/static/${path}`,
+                database,
+                setResources
+            );
+
             fetch(`/_next/static/${path}`).then((response) => {
                 if (response.ok)
                     response.text().then((data) => {
@@ -192,25 +224,4 @@ export async function downloadResources({
             });
         })
     );
-
-    return;
-
-    const tempResources: string[][] = await Promise.all(
-        resources.map(async (url) => {
-            if (!database) {
-                database = await openRockItIndexedDB();
-            }
-
-            if (setResources) setResources((value) => [...value, url]);
-
-            const resources = await fetchResource(url, database);
-
-            if (setResources)
-                setResources((value) => value.filter((value) => value != url));
-
-            return resources;
-        })
-    );
-    const newResources = [...new Set(tempResources.flat(1))];
-    downloadResources({ resources: newResources, database, setResources });
 }

44f628ca7b9ad03f765780af8b1aae63b98b0616
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-07 15:51:54 +0200

    Added downloads api route

---
+++ src/app/api/downloads/route.ts
@@ -0,0 +1,29 @@
+import { getSession } from "@/lib/auth/getSession";
+import { ENV } from "@/rockitEnv";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const session = await getSession();
+
+    if (!session?.user) {
+        return NextResponse.json(
+            { error: "Not authenticated" },
+            { status: 401 }
+        );
+    }
+
+    let response;
+
+    try {
+        response = await fetch(
+            `${ENV.BACKEND_URL}/downloads?user=${session.user.id}`,
+            {
+                signal: AbortSignal.timeout(2000),
+            }
+        );
+    } catch {
+        return new Response("Error connecting to backend", { status: 500 });
+    }
+
+    return response;
+}

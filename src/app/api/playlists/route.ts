d954d765dbdd88b93202727a78a093f8b71538af
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-07 15:53:27 +0200

    Added playlists api route

---
+++ src/app/api/playlists/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db/db";
+import { parsePlaylist, RawPlaylistDB } from "@/lib/db/playlist";
+
+export async function GET(request: NextRequest) {
+    const searchParams = new URL(request.url).searchParams;
+
+    let playlists;
+    try {
+        playlists = searchParams
+            .get("playlists")
+            ?.split(",")
+            .map((playlistID) =>
+                parsePlaylist(
+                    db
+                        .prepare(
+                            `SELECT ${
+                                searchParams.get("q") || "*"
+                            } FROM playlist WHERE id = ?`
+                        )
+                        .get(playlistID) as RawPlaylistDB
+                )
+            );
+    } catch (err) {
+        return new Response(err?.toString(), { status: 404 });
+    }
+
+    if (!playlists) {
+        return new Response("playlist not found", { status: 404 });
+    }
+
+    return NextResponse.json(playlists);
+}

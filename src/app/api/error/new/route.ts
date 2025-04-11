ff5633885bafb9a3524fd90ebeba06c60ffac8a4
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-11 13:57:43 +0200

    Added error log api route

---
+++ src/app/api/error/new/route.ts
@@ -0,0 +1,50 @@
+import { getSession } from "@/lib/auth/getSession";
+import { db } from "@/lib/db/db";
+import { generateId } from "@/lib/generateId";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const data = await request.json();
+
+    const session = await getSession();
+
+    console.warn("New error to check");
+
+    const {
+        msg,
+        source,
+        lineNo,
+        columnNo,
+        errorMessage,
+        errorCause,
+        errorName,
+        errorStack,
+    }: {
+        msg: string;
+        source: string;
+        lineNo: number;
+        columnNo: number;
+        errorMessage: string;
+        errorCause: string;
+        errorName: string;
+        errorStack: string;
+    } = data;
+
+    db.prepare(
+        "INSERT INTO error (id, msg, source, lineNo, columnNo, errorMessage, errorCause, errorName, errorStack, dateAdded, userId) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    ).run(
+        generateId(16),
+        msg,
+        source,
+        lineNo,
+        columnNo,
+        errorMessage,
+        errorCause,
+        errorName,
+        errorStack,
+        new Date().toISOString(),
+        session.user.id
+    );
+
+    return new NextResponse("OK");
+}

61d3a0b63cfed449522f7bf578dff05584f0a968
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-17 12:58:07 +0200

    Added skelton to image while loading

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,11 @@
-import { DetailedHTMLProps, ImgHTMLAttributes } from "react";
+"use client";
+
+import {
+    DetailedHTMLProps,
+    ImgHTMLAttributes,
+    useEffect,
+    useState,
+} from "react";
 
 export default function Image(
     props: DetailedHTMLProps<
@@ -6,9 +13,45 @@ export default function Image(
         HTMLImageElement
     >
 ) {
+    const { className = "", src, alt, ...rest } = props;
+    const [loaded, setLoaded] = useState(false);
+    const [objectUrl, setObjectUrl] = useState<string | null>(null);
+
+    useEffect(() => {
+        let isMounted = true;
+        let objectUrlRef: string;
+
+        if (src) {
+            setLoaded(false);
+
+            fetch(src)
+                .then((res) => res.blob())
+                .then((blob) => {
+                    if (!isMounted) return;
+                    objectUrlRef = URL.createObjectURL(blob);
+                    setObjectUrl(objectUrlRef);
+                    setLoaded(true);
+                });
+        }
+
+        return () => {
+            isMounted = false;
+            if (objectUrlRef) URL.revokeObjectURL(objectUrlRef);
+        };
+    }, [src]);
+
+    const combinedClassName = `${className} overflow-hidden`;
+
     return (
-        // <picture {...props}>
-        <img {...props} alt={props.alt} src={props.src} />
-        // </picture>
+        <div className={combinedClassName} {...rest}>
+            {!loaded && <div className="skeleton relative h-full w-full" />}
+            {objectUrl && (
+                <img
+                    src={objectUrl}
+                    alt={alt}
+                    className="relative h-full w-full"
+                />
+            )}
+        </div>
     );
 }

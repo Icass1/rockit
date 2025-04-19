b1411a9fd899758e45f37dfd39cdf8dde021fa87
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-19 11:51:47 +0200

    Added validate session api route for websocket

---
+++ src/app/api/validate-session/route.ts
@@ -0,0 +1,8 @@
+import { getSession } from "@/lib/auth/getSession";
+import { NextResponse } from "next/server";
+
+export async function GET(): Promise<NextResponse> {
+    const session = await getSession();
+
+    return NextResponse.json(session.user);
+}

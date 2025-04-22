8996cf372390da95c7b10ca91e3010a729cdf510
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-22 17:50:03 +0200

    Added hook to get if is dev environment

---
+++ src/hooks/useDev.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export default function useDev() {
+    const [dev, setDev] = useState(false);
+
+    useEffect(() => {
+        setDev(
+            window.location.hostname == "localhost" ||
+                window.location.hostname == ""
+        );
+    }, []);
+    return dev;
+}

f42bf2dbec5a4074421df1314bca43808f16fc0a
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-30 18:44:56 +0200

    [FIX] Added session check

--- src/app/api/pin/[type]/[id]/route.ts
+++ src/app/api/pin/[type]/[id]/route.ts
@@ -77,7 +77,7 @@ export async function DELETE(
     const { id, type } = await params;
     const session = await getSession();
 
-    if (!session.user.id) {
+    if (!session?.user.id) {
         return new NextResponse("Unauthenticated", { status: 401 });
     }
 

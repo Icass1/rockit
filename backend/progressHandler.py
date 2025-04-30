2c5cb6da770b0037c377ffcf0685aabb33009cbd
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-04-30 18:20:54 +0200

    [NEW] Added progress handler

---
+++ backend/progressHandler.py
@@ -0,0 +1,51 @@
+from logging import Logger
+from typing import Any, Dict, Optional
+
+from logger import getLogger
+from messageHandler import MessageHandler
+
+
+class ProgressHandler:
+    def __init__(self) -> None:
+        self.downloads_ids_dict: Dict[str, str] = {}
+        self.downloads_dict: Dict[str, MessageHandler] = {}
+        self.logger: Logger = getLogger(
+            name=__name__, class_name="ProgressHandler")
+
+    def add_task(
+        self,
+        description: str,
+        start: bool = True,
+        total: Optional[float] = 100.0,
+        completed: int = 0,
+        visible: bool = True,
+        **fields: Any,
+    ):
+
+        self.downloads_dict[self.downloads_ids_dict[description]].add(
+            message={'id': self.downloads_ids_dict[description], 'completed': completed, 'total': total, 'message': fields['message']})
+
+        return self.downloads_ids_dict[description]
+
+    def update(
+        self,
+        task_id,
+        *,
+        total: Optional[float] = None,
+        completed: Optional[float] = None,
+        advance: Optional[float] = None,
+        description: Optional[str] = None,
+        visible: Optional[bool] = None,
+        refresh: bool = False,
+        **fields: Any,
+    ):
+        if completed:
+            self.downloads_dict[task_id].add(
+                message={'id': task_id, 'completed': completed, 'message': fields['message']})
+
+    def start_task(self, task_id):
+        pass
+        # self.logger.info(task_id)
+
+    def remove_task(self, task_id):
+        self.logger.info(task_id)

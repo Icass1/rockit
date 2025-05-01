493c2c89db6311c7a3dfae60ec17dabf77e0b3b7
Nicorebo18 <nicorebo18@gmail.com>
2025-05-01 20:26:21 +0200

    [UPDATE] Improve online user indicator simulation

--- src/components/Header/HeaderOnlineUsers.tsx
+++ src/components/Header/HeaderOnlineUsers.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { User } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function OnlineUserIndicator() {
     const [onlineCount, setOnlineCount] = useState(0);
+    const currentCount = useRef(5);
 
+    // Simulación de la actualización del número de usuarios en línea
     useEffect(() => {
-        // Simulación random de usuarios online
-        setOnlineCount(Math.floor(Math.random() * 15));
+        const interval = setInterval(() => {
+            const change = Math.random() < 0.55 ? -1 : 1;
+            let newCount = currentCount.current + change;
+            newCount = Math.max(0, Math.min(10, newCount));
+            currentCount.current = newCount;
+            setOnlineCount(newCount);
+        }, 300*1000);
+
+        return () => clearInterval(interval);
     }, []);
 
     return (

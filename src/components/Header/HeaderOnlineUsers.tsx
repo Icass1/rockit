6936e3a76017b12c58f0973dfd87ed20230bf07d
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-03 11:27:36 +0200

    [UPDATE] Updated online users component with new users count store

--- src/components/Header/HeaderOnlineUsers.tsx
+++ src/components/Header/HeaderOnlineUsers.tsx
@@ -1,31 +1,17 @@
 "use client";
 
+import { users } from "@/stores/users";
+import { useStore } from "@nanostores/react";
 import { User } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
 
 export default function OnlineUserIndicator() {
-    const [onlineCount, setOnlineCount] = useState(0);
-    const currentCount = useRef(5);
-
-    // Simulación de la actualización del número de usuarios en línea
-    useEffect(() => {
-        setOnlineCount(currentCount.current);
-        const interval = setInterval(() => {
-            const change = Math.random() < 0.51 ? -1 : 1;
-            let newCount = currentCount.current + change;
-            newCount = Math.max(0, Math.min(10, newCount));
-            currentCount.current = newCount;
-            setOnlineCount(newCount);
-        }, 300*1000);
-
-        return () => clearInterval(interval);
-    }, []);
+    const $users = useStore(users);
 
     return (
         <>
-            {onlineCount > 0 && (
+            {$users > 0 && (
                 <div className="flex items-center gap-1 text-green-500">
-                    <span className="text-md font-semibold">{onlineCount}</span>
+                    <span className="text-md font-semibold">{$users}</span>
                     <User className="h-6 w-6" />
                 </div>
             )}

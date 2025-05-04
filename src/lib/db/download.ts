4aeb5eed437b2cd622ec30ffe9b4661fae30ece7
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-05 00:17:17 +0200

    [UPDATE] Added seen and success columns to download table

--- src/lib/db/download.ts
+++ src/lib/db/download.ts
@@ -2,13 +2,30 @@
 // ************** Download log stuff **************
 // ************************************************
 
-export interface DownloadDB {
+export interface RawDownloadDB {
     id: string;
     userId: string;
     dateStarted: string;
-    dateEnded: string | undefined;
+    dateEnded?: string;
     downloadURL: string;
     status: string;
+    seen: string;
+    success?: string;
+}
+
+export type DownloadDB<
+    Keys extends keyof DownloadDBFull = keyof DownloadDBFull,
+> = Pick<DownloadDBFull, Keys>;
+
+export interface DownloadDBFull {
+    id: string;
+    userId: string;
+    dateStarted: string;
+    dateEnded?: string;
+    downloadURL: string;
+    status: string;
+    seen: boolean;
+    success?: boolean;
 }
 
 export const downloadQuery = `CREATE TABLE IF NOT EXISTS download (
@@ -17,5 +34,26 @@ export const downloadQuery = `CREATE TABLE IF NOT EXISTS download (
     dateStarted TEXT NOT NULL,
     dateEnded TEXT,
     downloadURL TEXT NOT NULL,
-    status TEXT NOT NULL
+    status TEXT NOT NULL,
+    seen BOOLEAN DEFAULT "1" NOT NULL,
+    success BOOLEAN
 )`;
+
+export function parseDownload(
+    download: RawDownloadDB | undefined
+): DownloadDB | undefined {
+    if (!download) {
+        return undefined;
+    }
+
+    return {
+        id: download.id,
+        userId: download.userId,
+        dateStarted: download.dateStarted,
+        dateEnded: download.dateEnded,
+        downloadURL: download.downloadURL,
+        status: download.status,
+        seen: download.seen == "1" ? true : false,
+        success: download.success == "1" ? true : false,
+    };
+}

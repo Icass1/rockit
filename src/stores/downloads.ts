71fdf6ac92a76992edc71d42e38debe6a3862df2
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-07 23:19:21 +0200

    [UPDATE] Added request to /api/downloads when init

--- src/stores/downloads.ts
+++ src/stores/downloads.ts
@@ -1,3 +1,4 @@
+import { DownloadDB } from "@/lib/db/download";
 import { atom } from "nanostores";
 
 export const downloads = atom<string[]>([]);
@@ -6,16 +7,17 @@ export const downloadedLists = atom<string[]>([]);
 let eventSources: string[] = [];
 
 export const downloadInfo = atom<{
-    [key: string]: { completed: number; message: string };
+    [key: string]: { completed: number; message: string; downloadId: string };
 }>({});
 
-function onMessage(event: MessageEvent<string>) {
+function onMessage(event: MessageEvent<string>, downloadId: string) {
     const message: { id: string; completed: number; message: string } =
         JSON.parse(event.data);
 
     downloadInfo.get()[message.id] = {
         message: message.message,
         completed: message.completed,
+        downloadId,
     };
 
     downloadInfo.set({ ...downloadInfo.get() });
@@ -35,21 +37,31 @@ export async function startDownload(url: string) {
 downloads.subscribe((value) => {
     for (const downloadId of value) {
         if (eventSources.includes(downloadId)) {
-            console.log("Skipping id", downloadId);
             continue;
         }
-        eventSources?.push(downloadId);
+        eventSources.push(downloadId);
         const eventSource = new EventSource(
             `/api/downloads/status?id=${downloadId}`
         );
         eventSource.onmessage = (event) => {
-            onMessage(event);
+            onMessage(event, downloadId);
         };
         eventSource.onerror = (error) => {
             eventSources = eventSources.filter((id) => id != downloadId);
             downloads.set(downloads.get().filter((id) => id != downloadId));
+            console.log(eventSource.readyState, eventSource.CLOSED);
             console.error("EventSource failed:", error);
             eventSource.close();
         };
     }
 });
+
+fetch("/api/downloads")
+    .then((response) => response.json())
+    .then((data: DownloadDB[]) => {
+        data.forEach((download) =>
+            downloads.set([...downloads.get(), download.id])
+        );
+
+        console.log(data);
+    });

636304a10a532e6a7f73c15a6b3f829e539b1ddd
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-12 11:02:30 +0200

    [UPDATE] Spotify artist api types

--- backend/spotifyApiTypes/RawSpotifyApiArtist.py
+++ backend/spotifyApiTypes/RawSpotifyApiArtist.py
@@ -1,27 +1,27 @@
-from typing import List, Any
+from typing import List, Any, Optional
 from dataclasses import dataclass
 
 @dataclass
-class SpotifyArtistExternalUrls:
-    spotify: str
+class ArtistExternalUrls:
+    spotify: Optional[str]
     _json: dict
-    def from_dict(obj: Any) -> 'SpotifyArtistExternalUrls':
+    def from_dict(obj: Any) -> 'ArtistExternalUrls':
         _spotify = obj.get('spotify') if obj and 'spotify' in obj else None
-        return SpotifyArtistExternalUrls(_spotify, obj)
+        return ArtistExternalUrls(_spotify, obj)
     def __getitem__(self, item):
         if item == 'spotify':
             return self.spotify
         return None
 
 @dataclass
-class SpotifyArtistFollowers:
-    href: str
-    total: int
+class ArtistFollowers:
+    href: Optional[Any]
+    total: Optional[int]
     _json: dict
-    def from_dict(obj: Any) -> 'SpotifyArtistFollowers':
+    def from_dict(obj: Any) -> 'ArtistFollowers':
         _href = obj.get('href') if obj and 'href' in obj else None
         _total = obj.get('total') if obj and 'total' in obj else None
-        return SpotifyArtistFollowers(_href, _total, obj)
+        return ArtistFollowers(_href, _total, obj)
     def __getitem__(self, item):
         if item == 'href':
             return self.href
@@ -30,16 +30,16 @@ class SpotifyArtistFollowers:
         return None
 
 @dataclass
-class SpotifyArtistImages:
-    url: str
-    height: int
-    width: int
+class ArtistImages:
+    url: Optional[str]
+    height: Optional[int]
+    width: Optional[int]
     _json: dict
-    def from_dict(obj: Any) -> 'SpotifyArtistImages':
+    def from_dict(obj: Any) -> 'ArtistImages':
         _url = obj.get('url') if obj and 'url' in obj else None
         _height = obj.get('height') if obj and 'height' in obj else None
         _width = obj.get('width') if obj and 'width' in obj else None
-        return SpotifyArtistImages(_url, _height, _width, obj)
+        return ArtistImages(_url, _height, _width, obj)
     def __getitem__(self, item):
         if item == 'url':
             return self.url
@@ -51,24 +51,24 @@ class SpotifyArtistImages:
 
 @dataclass
 class RawSpotifyApiArtist:
-    external_urls: SpotifyArtistExternalUrls
-    followers: SpotifyArtistFollowers
-    genres: List[str]
-    href: str
-    id: str
-    images: List[SpotifyArtistImages]
-    name: str
-    popularity: int
-    type: str
-    uri: str
+    external_urls: Optional[ArtistExternalUrls]
+    followers: Optional[ArtistFollowers]
+    genres: Optional[List[str]]
+    href: Optional[str]
+    id: Optional[str]
+    images: Optional[List[ArtistImages]]
+    name: Optional[str]
+    popularity: Optional[int]
+    type: Optional[str]
+    uri: Optional[str]
     _json: dict
     def from_dict(obj: Any) -> 'RawSpotifyApiArtist':
-        _external_urls = SpotifyArtistExternalUrls.from_dict(obj.get('external_urls')) if obj and 'external_urls' in obj else None
-        _followers = SpotifyArtistFollowers.from_dict(obj.get('followers')) if obj and 'followers' in obj else None
+        _external_urls = ArtistExternalUrls.from_dict(obj.get('external_urls')) if obj and 'external_urls' in obj else None
+        _followers = ArtistFollowers.from_dict(obj.get('followers')) if obj and 'followers' in obj else None
         _genres = obj.get('genres') if obj and 'genres' in obj else None
         _href = obj.get('href') if obj and 'href' in obj else None
         _id = obj.get('id') if obj and 'id' in obj else None
-        _images = [SpotifyArtistImages.from_dict(k) for k in obj.get('images')] if obj and 'images' in obj else None
+        _images = [ArtistImages.from_dict(k) for k in obj.get('images')] if obj and 'images' in obj else None
         _name = obj.get('name') if obj and 'name' in obj else None
         _popularity = obj.get('popularity') if obj and 'popularity' in obj else None
         _type = obj.get('type') if obj and 'type' in obj else None

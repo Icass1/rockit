1905ae32867030424dfcb6db77407db4fcea2b0f
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-12 11:28:03 +0200

    [UPDATE] Refactor Downloads component layout and integrate SongsStatus component

--- src/app/downloader/page.tsx
+++ src/app/downloader/page.tsx
@@ -1,6 +1,7 @@
 import ClearDownloads from "@/components/Downloader/ClearDownloads";
 import DownloadElement from "@/components/Downloader/DownloadElement";
 import InputBar from "@/components/Downloader/InputBar";
+import SongsStatus from "@/components/Downloader/SongsStatus";
 import Image from "@/components/Image";
 import { getSession } from "@/lib/auth/getSession";
 import { db } from "@/lib/db/db";
@@ -25,53 +26,63 @@ export default async function Downloads() {
         .map((entry) => parseDownload(entry as RawDownloadDB));
 
     return (
-        <div className="relative mx-auto h-full w-[500px] overflow-y-auto bg-neutral-900 md:pt-24 md:pb-24">
-            <div className="mt-5 flex justify-center gap-6">
-                <Image
-                    width={30}
-                    height={30}
-                    src="/youtube-music-logo.svg"
-                    alt="YouTube Music Logo"
-                    className="h-6 object-contain"
-                />
-                <Image
-                    width={30}
-                    height={30}
-                    src="/spotify-logo.png"
-                    alt="Spotify Logo"
-                    className="h-7 object-contain"
-                />
-            </div>
-            <label className="ml-3 block w-fit py-3 text-3xl font-bold">
-                Music Downloader
-            </label>
-
-            <InputBar></InputBar>
-            <div className="mx-2 mb-4 flex items-center justify-between">
-                <div className="flex flex-row items-center gap-2">
-                    <label className="text-lg font-bold text-white">
-                        {lang.latest_downloads}
-                    </label>
-                    <Link href={"#"}>
-                        <RotateCw className="h-4 w-4"></RotateCw>
-                    </Link>
+        <div className="grid h-full grid-cols-2 gap-x-10 overflow-y-auto px-10 md:pt-24 md:pb-24">
+            <div className="sticky top-0 h-fit w-full max-w-full min-w-0 rounded bg-neutral-900 p-2">
+                <div className="mt-5 flex justify-center gap-6">
+                    <Image
+                        width={30}
+                        height={30}
+                        src="/youtube-music-logo.svg"
+                        alt="YouTube Music Logo"
+                        className="h-6 object-contain"
+                    />
+                    <Image
+                        width={30}
+                        height={30}
+                        src="/spotify-logo.png"
+                        alt="Spotify Logo"
+                        className="h-7 object-contain"
+                    />
                 </div>
+                <label className="ml-3 block w-fit py-3 text-3xl font-bold">
+                    Music Downloader
+                </label>
 
-                <ClearDownloads></ClearDownloads>
-            </div>
-            <div className="flex flex-col gap-2">
-                {downloads.map((download) => {
-                    if (!download) return false;
+                <InputBar></InputBar>
+                <div className="mx-2 mb-4 flex items-center justify-between">
+                    <div className="flex flex-row items-center gap-2">
+                        <label className="text-lg font-bold text-white">
+                            {lang.latest_downloads}
+                        </label>
+                        <Link href={"#"}>
+                            <RotateCw className="h-4 w-4"></RotateCw>
+                        </Link>
+                    </div>
 
-                    return (
-                        <DownloadElement
-                            key={download.id}
-                            download={download}
-                        />
-                    );
-                })}
+                    <ClearDownloads></ClearDownloads>
+                </div>
+                <div className="flex flex-col gap-2">
+                    {downloads.map((download) => {
+                        if (!download) return false;
+
+                        return (
+                            <DownloadElement
+                                key={download.id}
+                                download={download}
+                            />
+                        );
+                    })}
+                </div>
+                {downloads.length == 0 && (
+                    <label className="mx-auto block w-fit text-xl font-semibold text-neutral-300">
+                        There is nothing to show here
+                    </label>
+                )}
+                <div className="min-h-10" />
             </div>
-            <div className="min-h-10" />
+            <div className="sticky top-0 h-fit w-full max-w-full min-w-0 rounded bg-neutral-900 p-2">
+                <SongsStatus />
+            </div>
         </div>
     );
 }

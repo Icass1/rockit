8e4ba4ff6baecc5fcf5afa080be62d610a55fbf5
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-19 12:38:22 +0200

    [FIX] Fixed artist db images

--- backend/db/artist.py
+++ backend/db/artist.py
@@ -2,10 +2,13 @@ from typing import TypedDict, List, Optional
 import json
 from dataclasses import dataclass
 
+from backend.db.commonTypes import OldImageDB
+
 
 class RawArtistDB(TypedDict):
     id: str
     images: str
+    image: str
     name: str
     genres: str
     followers: int
@@ -17,7 +20,8 @@ class RawArtistDB(TypedDict):
 @dataclass
 class ArtistDBFull:
     id: str
-    images: str
+    images: List[OldImageDB]
+    image: str
     name: str
     genres: List[str]
     followers: int
@@ -33,6 +37,7 @@ def parse_artist(raw_artist: Optional[RawArtistDB]) -> Optional[ArtistDBFull]:
     return ArtistDBFull(
         id=raw_artist.get("id"),
         images=json.loads(raw_artist.get("images", "[]")),
+        image=raw_artist.get("image"),
         name=raw_artist.get("name"),
         genres=json.loads(raw_artist.get("genres", "[]")),
         followers=raw_artist.get("followers"),
@@ -46,6 +51,7 @@ artist_query = """
 CREATE TABLE IF NOT EXISTS artist (
     id TEXT NOT NULL PRIMARY KEY UNIQUE,
     images TEXT NOT NULL,
+    image TEXT,
     name TEXT NOT NULL,
     genres TEXT NOT NULL,
     followers INTEGER NOT NULL,

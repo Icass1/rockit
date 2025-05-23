a634bbb57fac976af978fdfacaa03550d3170884
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-23 10:16:48 +0200

    [UPDATE] Added context menu to quick selections songs

--- src/components/Home/QuickSelectionsSong.tsx
+++ src/components/Home/QuickSelectionsSong.tsx
@@ -3,6 +3,7 @@ import { getImageUrl } from "@/lib/getImageUrl";
 import { currentList } from "@/stores/currentList";
 import { songHandleClick } from "@/components/ListSongs/HandleClick";
 import Image from "@/components/Image";
+import SongContextMenu from "../ListSongs/SongContextMenu";
 
 export default function QuickSelectionsSong({
     song,
@@ -39,6 +40,47 @@ export default function QuickSelectionsSong({
             })
         );
     };
+
+    return (
+        <SongContextMenu
+            onPlay={handleClick}
+            song={{
+                ...song,
+                path: "this path is not needed but cannot be empty",
+            }}
+        >
+            <div
+                className="flex h-fit cursor-pointer items-center gap-2 rounded-lg p-2 transition hover:bg-zinc-800"
+                onClick={handleClick}
+            >
+                {/* Imagen de la canción */}
+                <Image
+                    width={100}
+                    height={100}
+                    className="aspect-square h-12 min-h-12 w-12 min-w-12 rounded-sm object-cover"
+                    src={getImageUrl({
+                        imageId: song.image,
+                        height: 100,
+                        width: 100,
+                        placeHolder: "/api/image/song-placeholder.png",
+                    })}
+                    alt={`Song Cover for ${song.name}`}
+                />
+                {/* Información de la canción */}
+                <div className="flex w-full max-w-full min-w-0 flex-col justify-center">
+                    {/* Nombre de la canción */}
+                    <span className="text-md w-full max-w-full min-w-0 truncate font-semibold text-white">
+                        {song.name}
+                    </span>
+                    {/* Artista y álbum */}
+                    <span className="w-full max-w-full min-w-0 truncate text-sm text-gray-400">
+                        {song.artists[0].name}
+                        {" • "} {song.albumName}
+                    </span>
+                </div>
+            </div>
+        </SongContextMenu>
+    );
 
     return (
         <div

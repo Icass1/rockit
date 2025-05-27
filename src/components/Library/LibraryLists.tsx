e1053174f925c9674ff265bb75935d4cfc9ba0b4
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-05-27 15:42:45 +0200

    [UPDATE] Added context menu to library lists

--- src/components/Library/LibraryLists.tsx
+++ src/components/Library/LibraryLists.tsx
@@ -3,7 +3,7 @@
 import { getImageUrl } from "@/lib/getImageUrl";
 import Link from "next/link";
 import Image from "@/components/Image";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { PlaylistDB } from "@/lib/db/playlist";
 import { AlbumDB } from "@/lib/db/album";
 import { useStore } from "@nanostores/react";
@@ -11,6 +11,263 @@ import { langData } from "@/stores/lang";
 import NewPlaylistButton from "@/components/Library/NewPlaylistButton";
 import useWindowSize from "@/hooks/useWindowSize";
 import useFetch from "@/hooks/useFetch";
+import ContextMenu from "../ContextMenu/ContextMenu";
+import ContextMenuTrigger from "../ContextMenu/Trigger";
+import ContextMenuContent from "../ContextMenu/Content";
+import ContextMenuOption from "../ContextMenu/Option";
+import {
+    HardDriveDownload,
+    Library,
+    ListEnd,
+    ListStart,
+    PlayCircle,
+    Shuffle,
+} from "lucide-react";
+import { libraryLists } from "@/stores/libraryLists";
+import { playListHandleClick } from "../PlayList";
+import { currentListSongs } from "@/stores/currentList";
+import { SongDB } from "@/lib/db/song";
+import {
+    database,
+    queue,
+    queueIndex,
+    saveSongToIndexedDB,
+} from "@/stores/audio";
+import { downloadFile, downloadRsc } from "@/lib/downloadResources";
+import ContextMenuSplitter from "../ContextMenu/Splitter";
+
+async function getListSongs(list: PlaylistDB | AlbumDB) {
+    if (list.type == "playlist") {
+        const response = await fetch(
+            `/api/songs1?songs=${list.songs
+                .map((song) => song.id)
+                .join()}&p=id,image,name,artists,path,duration,albumName,albumId`
+        );
+        const songs = (await response.json()) as SongDB<
+            | "id"
+            | "image"
+            | "name"
+            | "artists"
+            | "path"
+            | "duration"
+            | "albumName"
+            | "albumId"
+        >[];
+        return songs;
+    } else if (list.type == "album") {
+        const response = await fetch(
+            `/api/songs1?songs=${list.songs.join()}&p=id,image,name,artists,path,duration,albumName,albumId`
+        );
+        const songs = (await response.json()) as SongDB<
+            | "id"
+            | "image"
+            | "name"
+            | "artists"
+            | "path"
+            | "duration"
+            | "albumName"
+            | "albumId"
+        >[];
+        return songs;
+    } else {
+        console.warn("Unknown list type:", list);
+    }
+}
+
+function AddListContextMenu({
+    children,
+    list,
+}: {
+    children: React.ReactNode;
+    list: PlaylistDB | AlbumDB;
+}) {
+    const addListToTopQueue = async () => {
+        const songs = await getListSongs(list);
+
+        if (!songs) {
+            console.warn("addListToTopQueue Songs is undefined");
+            return;
+        }
+
+        const tempQueue = queue.get();
+        if (!tempQueue) return;
+
+        const songsToAdd = songs
+            .map((song, index) => {
+                return {
+                    song: song,
+                    list: { type: list.type, id: list.id },
+                    index:
+                        Math.max(...tempQueue.map((_song) => _song.index)) +
+                        index +
+                        1,
+                };
+            })
+            .filter((queueSong) => queueSong?.song?.id);
+        const index = tempQueue.findIndex(
+            (_song) => _song.index == queueIndex.get()
+        );
+
+        queue.set([
+            ...tempQueue.slice(0, index + 1),
+            ...songsToAdd,
+            ...tempQueue.slice(index + 1),
+        ]);
+    };
+
+    const addListToQueueRandomly = async () => {
+        const songs = await getListSongs(list);
+        if (!songs) {
+            console.warn("addListToBottomQueue Songs is undefined");
+            return;
+        }
+
+        const tempQueue = queue.get();
+        if (!tempQueue) return;
+
+        const songsToAdd = songs
+            .map((song, index) => {
+                return {
+                    song: song,
+                    list: { type: list.type, id: list.id },
+                    index:
+                        Math.max(...tempQueue.map((_song) => _song.index)) +
+                        index +
+                        1,
+                };
+            })
+            .filter((queueSong) => queueSong?.song?.id);
+        const index = tempQueue.findIndex(
+            (_song) => _song.index == queueIndex.get()
+        );
+
+        const queueAfterCurrentIndex = [
+            // ...songsToAdd,
+
+            ...tempQueue.slice(index + 1),
+        ];
+
+        songsToAdd.forEach((song, i) => {
+            // Assign a random index to each song to be added
+            const index = Math.floor(
+                Math.random() * (queueAfterCurrentIndex.length + i)
+            );
+            // Insert the song into the queueAfterCurrentIndex at the random index
+            queueAfterCurrentIndex.splice(index, 0, song);
+        });
+
+        queue.set([
+            ...tempQueue.slice(0, index + 1),
+            ...queueAfterCurrentIndex,
+        ]);
+    };
+
+    const addListToBottomQueue = async () => {
+        const songs = await getListSongs(list);
+        if (!songs) {
+            console.warn("addListToBottomQueue Songs is undefined");
+            return;
+        }
+        const tempQueue = queue.get();
+        if (!tempQueue) return;
+
+        const songsToAdd = songs
+            .map((song, index) => {
+                return {
+                    song: song,
+                    list: { type: list.type, id: list.id },
+                    index:
+                        Math.max(...tempQueue.map((_song) => _song.index)) +
+                        index +
+                        1,
+                };
+            })
+            .filter((queueSong) => queueSong?.song?.id);
+        queue.set([...tempQueue, ...songsToAdd]);
+    };
+
+    const handlePlay = async () => {
+        const songs = await getListSongs(list);
+        if (!songs) {
+            console.warn("Songs is undefined");
+            return;
+        }
+
+        currentListSongs.set(songs);
+
+        playListHandleClick({ type: list.type, id: list.id });
+    };
+
+    const downloadListToDevice = async () => {
+        currentListSongs.get().map((song) => {
+            saveSongToIndexedDB(song);
+        });
+
+        if (!database) return;
+
+        const imageBlob = await fetch(`/api/image/${list.image}`).then(
+            (response) => response.blob()
+        );
+
+        const imageToSave = {
+            id: list.image,
+            blob: imageBlob,
+        };
+
+        await downloadFile(`/${list.type}/${list.id}`, database);
+        await downloadRsc(`/${list.type}/${list.id}`, database);
+
+        const imagesTx = database.transaction("images", "readwrite");
+        const imagesStore = imagesTx.objectStore("images");
+        imagesStore.put(imageToSave);
+
+        console.log("List downloaded!");
+    };
+
+    const handleRemoveFromLibrary = () => {
+        fetch(`/api/remove-list/${list.type}/${list.id}`)
+            .then((response) => response.json())
+            .then(() => {
+                libraryLists.set(
+                    libraryLists.get().filter((list) => list.id !== list.id)
+                );
+            });
+    };
+
+    return (
+        <ContextMenu>
+            <ContextMenuTrigger>{children}</ContextMenuTrigger>
+            <ContextMenuContent>
+                <ContextMenuOption onClick={handlePlay}>
+                    <PlayCircle className="h-5 w-5" />
+                    Play {list.type}
+                </ContextMenuOption>
+                <ContextMenuSplitter />
+                <ContextMenuOption onClick={addListToTopQueue}>
+                    <ListStart className="h-5 w-5" />
+                    Add list to top of queue
+                </ContextMenuOption>
+                <ContextMenuOption onClick={addListToQueueRandomly}>
+                    <Shuffle className="h-5 w-5" />
+                    Add list to queue randomly
+                </ContextMenuOption>
+                <ContextMenuOption onClick={addListToBottomQueue}>
+                    <ListEnd className="h-5 w-5" />
+                    Add list to bottom of queue
+                </ContextMenuOption>
+                <ContextMenuSplitter />
+                <ContextMenuOption onClick={downloadListToDevice}>
+                    <HardDriveDownload className="h-5 w-5" />
+                    Download list to device
+                </ContextMenuOption>
+                <ContextMenuOption onClick={handleRemoveFromLibrary}>
+                    <Library className="h-5 w-5" />
+                    Remove from library
+                </ContextMenuOption>
+            </ContextMenuContent>
+        </ContextMenu>
+    );
+}
 
 export function LibraryLists({
     filterMode,
@@ -22,10 +279,16 @@ export function LibraryLists({
     const { width } = useWindowSize();
     const $lang = useStore(langData);
 
-    const data = useFetch<{
+    const [data, updateLists] = useFetch<{
         playlists: PlaylistDB[];
         albums: AlbumDB[];
     }>("/api/library/lists");
+
+    useEffect(() => {
+        return libraryLists.listen(() => {
+            updateLists();
+        });
+    }, [updateLists]);
 
     const playlists = data?.playlists;
     const albums = data?.albums;
@@ -87,63 +350,73 @@ export function LibraryLists({
                 <NewPlaylistButton />
 
                 {filteredPlaylists.map((playlist, index) => (
-                    <Link
+                    <AddListContextMenu
                         key={"playlist" + index}
-                        href={`/playlist/${playlist.id}`}
-                        className="library-item flex h-auto w-full max-w-full min-w-0 flex-col transition-transform md:hover:scale-110"
+                        list={playlist}
                     >
-                        <Image
-                            alt={playlist.name}
-                            className="cover aspect-square h-auto w-full rounded-md"
-                            src={getImageUrl({
-                                imageId: playlist.image,
-                                height: 300,
-                                width: 300,
-                                fallback: playlist.images?.[0]?.url,
-                                placeHolder: "/api/image/rockit-background.png",
-                            })}
-                        />
-                        <label className="min-h-6 truncate text-center font-semibold">
-                            {playlist.name}
-                        </label>
-                        <label className="min-h-5 truncate text-center text-sm text-gray-400">
-                            {playlist.owner}
-                        </label>
-                    </Link>
+                        <Link
+                            href={`/playlist/${playlist.id}`}
+                            className="library-item flex h-auto w-full max-w-full min-w-0 flex-col transition-transform md:hover:scale-110"
+                        >
+                            <Image
+                                alt={playlist.name}
+                                className="cover aspect-square h-auto w-full rounded-md"
+                                src={getImageUrl({
+                                    imageId: playlist.image,
+                                    height: 300,
+                                    width: 300,
+                                    fallback: playlist.images?.[0]?.url,
+                                    placeHolder:
+                                        "/api/image/rockit-background.png",
+                                })}
+                            />
+                            <label className="min-h-6 truncate text-center font-semibold">
+                                {playlist.name}
+                            </label>
+                            <label className="min-h-5 truncate text-center text-sm text-gray-400">
+                                {playlist.owner}
+                            </label>
+                        </Link>
+                    </AddListContextMenu>
                 ))}
 
                 {filteredAlbums.map((album, index) => (
-                    <Link
-                        key={"album" + index}
-                        href={`/album/${album.id}`}
-                        className="library-item flex h-auto w-full max-w-full min-w-0 flex-col transition-transform md:hover:scale-110"
-                    >
-                        <Image
-                            alt={album.name}
-                            className="cover aspect-square h-auto w-full rounded-md"
-                            src={getImageUrl({
-                                imageId: album.image,
-                                height: 300,
-                                width: 300,
-                                fallback: album.images[0]?.url,
-                                placeHolder: "/api/image/song-placeholder.png",
-                            })}
-                        />
-                        <label className="mt-1 truncate text-center font-semibold">
-                            {album.name}
-                        </label>
-                        <div className="mx-auto flex max-w-full flex-row truncate text-center text-sm text-gray-400">
-                            {album.artists.map((artist, i) => (
-                                <label
-                                    key={album.id + artist.id}
-                                    className="truncate md:hover:underline"
-                                >
-                                    {artist.name}
-                                    {i < album.artists.length - 1 ? ", " : ""}
-                                </label>
-                            ))}
-                        </div>
-                    </Link>
+                    <AddListContextMenu key={"album" + index} list={album}>
+                        <Link
+                            key={"album" + index}
+                            href={`/album/${album.id}`}
+                            className="library-item flex h-auto w-full max-w-full min-w-0 flex-col transition-transform md:hover:scale-110"
+                        >
+                            <Image
+                                alt={album.name}
+                                className="cover aspect-square h-auto w-full rounded-md"
+                                src={getImageUrl({
+                                    imageId: album.image,
+                                    height: 300,
+                                    width: 300,
+                                    fallback: album.images[0]?.url,
+                                    placeHolder:
+                                        "/api/image/song-placeholder.png",
+                                })}
+                            />
+                            <label className="mt-1 truncate text-center font-semibold">
+                                {album.name}
+                            </label>
+                            <div className="mx-auto flex max-w-full flex-row truncate text-center text-sm text-gray-400">
+                                {album.artists.map((artist, i) => (
+                                    <label
+                                        key={album.id + artist.id}
+                                        className="truncate md:hover:underline"
+                                    >
+                                        {artist.name}
+                                        {i < album.artists.length - 1
+                                            ? ", "
+                                            : ""}
+                                    </label>
+                                ))}
+                            </div>
+                        </Link>
+                    </AddListContextMenu>
                 ))}
             </div>
             <div className="min-h-10" />

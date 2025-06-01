e17c56129aabc33e9c2f32a1748c09b8c05406b5
IgnacioPCWSL <i.cassdolopez@gmail.com>
2025-06-01 12:50:55 +0200

    [UPDATE] Minor changes in database test

--- src/databaseTest.ts
+++ src/databaseTest.ts
@@ -1,69 +1,82 @@
 // import sqlite from "better-sqlite3";
 // export const db = sqlite("database/test-database.db");
 
-import { album_images, albums, songs, spotify_images } from "./lib/db2/db";
+import { users } from "./lib/db/db";
 import { getDatabaseDate } from "./lib/getTime";
 
 console.log("==========================");
 
 console.log(getDatabaseDate());
 
-albums.insert(
-    {
-        id: "awefasdf",
-        name: "ASDF",
-        disc_count: 2,
-        image: "imageid",
-        release_date: "1942",
-    },
+// albums.insert(
+//     {
+//         id: "awefasdf",
+//         name: "ASDF",
+//         disc_count: 2,
+//         image: "imageid",
+//         release_date: "1942",
+//     },
 
-    { ignoreIfExists: true }
-);
+//     { ignoreIfExists: true }
+// );
 
-spotify_images.insert(
-    {
-        url: "https://putopython.com",
-        width: 600,
-        height: 600,
-        id: "imageida1",
-    },
+// spotify_images.insert(
+//     {
+//         url: "https://putopython.com",
+//         width: 600,
+//         height: 600,
+//         id: "imageida1",
+//     },
 
-    { ignoreIfExists: true }
-);
+//     { ignoreIfExists: true }
+// );
 
-album_images.insert(
-    {
-        album_id: "awefasdf",
-        image_id: "imageida1",
-    },
+// album_images.insert(
+//     {
+//         album_id: "awefasdf",
+//         image_id: "imageida1",
+//     },
 
-    { ignoreIfExists: true }
-);
+//     { ignoreIfExists: true }
+// );
 
-songs.insert(
-    [
-        {
-            id: "songid",
-            name: "testname",
-            album_id: "awefasdf",
-            duration: 50,
-            track_number: 1,
-            disc_number: 1,
-            isrc: "isrctest",
-        },
-        {
-            id: "songid2",
-            name: "testname2",
-            album_id: "awefasdf",
-            duration: 36,
-            track_number: 2,
-            disc_number: 1,
-            isrc: "isrctest2",
-        },
-    ],
-    { ignoreIfExists: true }
-);
+// songs.insert(
+//     {
+//         id: "songid",
+//         name: "testname",
+//         album_id: "awefasdf",
+//         duration: 50,
+//         track_number: 1,
+//         disc_number: 1,
+//         isrc: "isrctest",
+//     },
+//     { ignoreIfExists: true }
+// );
 
-const song = songs.get("id", "songid");
+// songs.insert(
+//     [
+//         {
+//             id: "songid",
+//             name: "testname",
+//             album_id: "awefasdf",
+//             duration: 50,
+//             track_number: 1,
+//             disc_number: 1,
+//             isrc: "isrctest",
+//         },
+//         {
+//             id: "songid2",
+//             name: "testname2",
+//             album_id: "awefasdf",
+//             duration: 36,
+//             track_number: 2,
+//             disc_number: 1,
+//             isrc: "isrctest2",
+//         },
+//     ],
+//     { ignoreIfExists: true }
+// );
 
-console.log(song.album.date_added);
+const user = users.get("id", "kor0r3n05o6ihak3");
+
+console.log(user.user_lists.map((list) => [list.item_type, list.item_id]));

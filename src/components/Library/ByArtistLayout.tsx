9d62668efc44ccc7f8a1ed0187e07a92170810e8
IcassLaptopUbuntu <i.cassdolopez@gmail.com>
2025-07-08 20:52:15 +0200

    [NEW] Add ByArtistLayout component to display albums and playlists by artist

---
+++ src/components/Library/ByArtistLayout.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { getImageUrl } from "@/lib/getImageUrl";
+import Link from "next/link";
+import Image from "@/components/Image";
+import { PlaylistDB } from "@/lib/db/playlist";
+import { AlbumDB } from "@/lib/db/album";
+import NewPlaylistButton from "@/components/Library/NewPlaylistButton";
+import useWindowSize from "@/hooks/useWindowSize";
+import { useMemo } from "react";
+import { AddListContextMenu } from "./AddListContextMenu";
+
+export function ByArtistLayout({
+    filteredAlbums,
+    filteredPlaylists,
+}: {
+    filteredAlbums: AlbumDB[];
+    filteredPlaylists: PlaylistDB[];
+}) {
+    const { width } = useWindowSize();
+
+    const artists = useMemo(() => {
+        const artistMap: Record<string, (AlbumDB | PlaylistDB)[]> = {};
+
+        artistMap["Your playlists"] = [];
+
+        filteredAlbums.forEach((album) => {
+            album.artists.forEach((artist) => {
+                if (!artistMap[artist.name]) {
+                    artistMap[artist.name] = [];
+                }
+                artistMap[artist.name].push(album);
+            });
+        });
+        filteredPlaylists.forEach((playlist) => {
+            artistMap["Your playlists"].push(playlist);
+        });
+        return Object.entries(artistMap).map(([name, albums]) => ({
+            name,
+            albums,
+        }));
+    }, [filteredAlbums, filteredPlaylists]);
+
+    if (!width) return null;
+
+    return (
+        <div className="flex flex-col gap-7">
+            {artists.map((artist) => (
+                <div key={artist.name} className="flex flex-col gap-4">
+                    <h3 className="ml-6 text-3xl font-semibold">
+                        {artist.name}
+                    </h3>
+
+                    <div
+                        className="grid gap-x-5 gap-y-3 px-5"
+                        style={{
+                            gridTemplateColumns:
+                                width < 768
+                                    ? "repeat(auto-fill, minmax(40%, 1fr))"
+                                    : "repeat(auto-fill, minmax(200px, 1fr))",
+                        }}
+                    >
+                        {artist.name === "Your playlists" ? (
+                            <NewPlaylistButton />
+                        ) : null}
+                        {artist.albums.map((album, index) => {
+                            return (
+                                <AddListContextMenu
+                                    key={"album" + index}
+                                    list={album}
+                                >
+                                    <Link
+                                        key={"album" + index}
+                                        href={`/album/${album.id}`}
+                                        className="library-item flex h-auto w-full max-w-full min-w-0 flex-col transition-transform md:hover:scale-110"
+                                    >
+                                        <Image
+                                            alt={album.name}
+                                            className="cover aspect-square h-auto w-full rounded-md"
+                                            src={getImageUrl({
+                                                imageId: album.image,
+                                                height: 300,
+                                                width: 300,
+                                                fallback:
+                                                    album?.images?.[0]?.url,
+                                                placeHolder:
+                                                    "/api/image/song-placeholder.png",
+                                            })}
+                                        />
+                                        <label className="mt-1 truncate text-center font-semibold">
+                                            {album.name}
+                                        </label>
+                                    </Link>
+                                </AddListContextMenu>
+                            );
+                        })}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}

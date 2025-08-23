e47402415d8d5b5e4282bf9e08bb09cc5d1df2ef
= <=>
2025-08-23 23:53:52 +0200

    [UPDATE] Remove obsolete SQL files and update drop.sql for improved table management

--- db/drop.sql
+++ db/drop.sql
@@ -1,20 +1,30 @@
-DROP TABLE main.external_images CASCADE;
-DROP TABLE main.internal_images CASCADE;
-DROP TABLE main.albums CASCADE;
-DROP TABLE main.album_external_images CASCADE;
-DROP TABLE main.artists CASCADE;
-DROP TABLE main.album_artists CASCADE;
-DROP TABLE main.artist_external_images CASCADE;
-DROP TABLE main.songs CASCADE;
-DROP TABLE main.song_artists CASCADE;
-DROP TABLE main.users CASCADE;
-DROP TABLE main.user_lists CASCADE;
-DROP TABLE main.user_queue_songs CASCADE;
-DROP TABLE main.user_pinned_lists CASCADE;
-DROP TABLE main.user_liked_songs CASCADE;
-DROP TABLE main.user_history_songs CASCADE;
-DROP TABLE main.playlists CASCADE;
-DROP TABLE main.playlist_external_images CASCADE;
-DROP TABLE main.playlist_songs CASCADE;
-DROP TABLE main.downloads CASCADE;
-DROP TABLE main.errors CASCADE;
+
+DROP TABLE IF EXISTS main.albums CASCADE;
+DROP TABLE IF EXISTS main.album_external_images CASCADE;
+DROP TABLE IF EXISTS main.album_artists CASCADE;
+
+DROP TABLE IF EXISTS main.artists CASCADE;
+DROP TABLE IF EXISTS main.artist_external_images CASCADE;
+DROP TABLE IF EXISTS main.artist_genres CASCADE;
+
+DROP TABLE IF EXISTS main.songs CASCADE;
+DROP TABLE IF EXISTS main.song_artists CASCADE;
+DROP TABLE IF EXISTS main.song_external_images CASCADE;
+
+DROP TABLE IF EXISTS main.users CASCADE;
+DROP TABLE IF EXISTS main.user_lists CASCADE;
+DROP TABLE IF EXISTS main.user_queue_songs CASCADE;
+DROP TABLE IF EXISTS main.user_pinned_lists CASCADE;
+DROP TABLE IF EXISTS main.user_liked_songs CASCADE;
+DROP TABLE IF EXISTS main.user_history_songs CASCADE;
+
+DROP TABLE IF EXISTS main.playlists CASCADE;
+DROP TABLE IF EXISTS main.playlist_songs CASCADE;
+DROP TABLE IF EXISTS main.playlist_external_images CASCADE;
+
+DROP TABLE IF EXISTS main.lists CASCADE;
+DROP TABLE IF EXISTS main.genres CASCADE;
+DROP TABLE IF EXISTS main.errors CASCADE;
+DROP TABLE IF EXISTS main.downloads CASCADE;
+DROP TABLE IF EXISTS main.internal_images CASCADE;
+DROP TABLE IF EXISTS main.external_images CASCADE;

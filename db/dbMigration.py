9515c9989e922decf52f89e26d3e179056e89ce5
= <=>
2025-08-31 01:02:07 +0200

    [UPDATE ]Refactor dbMigration and dbTest to enhance database interactions, improve session management, and update variable naming for clarity

--- db/dbMigration.py
+++ db/dbMigration.py
@@ -192,9 +192,9 @@ def get_spotify_data(ids: List[str], data_name: Literal["album"] | Literal["arti
 
 get_token()
 
-rockit_db = RockitDB()
+rockit_db = RockitDB("admin", "admin", "12.12.12.3", 5432, "development_1")
 
-session = rockit_db.session
+session = rockit_db.get_session()
 
 conn = sqlite3.connect(
     'file:database/database-prod.db?mode=ro', check_same_thread=False)
@@ -251,12 +251,13 @@ def add_internal_images():
             url=url,
             path=path)
 
+        image_to_add = session.merge(image_to_add)
         internal_images_in_db.append(image_to_add)
-
-        session.merge(image_to_add)
 
     session.commit()
     logger.info("Added all internal images.")
+
+# 28126
 
 
 def add_albums():
@@ -272,7 +273,7 @@ def add_albums():
         public_id: str = album[0]
         type = album[1]
         external_images = json.loads(album[2])
-        internal_image_id = album[3]
+        internal_image_public_id = album[3]
         name = album[4]
         release_date = album[5]
         artists = json.loads(album[6])
@@ -295,15 +296,22 @@ def add_albums():
         if found:
             continue
 
-        if internal_image_id == "":
+        if internal_image_public_id == "":
             logger.error(
-                f"Skipping album {public_id} with empty internal_image_id")
+                f"Skipping album {public_id} with empty internal_image_public_id")
             continue
 
+        internal_image_id: int | None = None
+
         for k in internal_images_in_db:
-            if k.public_id == internal_image_id:
+            if k.public_id == internal_image_public_id:
                 internal_image_id = k.id
                 break
+
+        if not internal_image_id:
+            logger.error(
+                f"Skipping album because it has a internal_image_public_id but an InternalImage was not found")
+            continue
 
         # Get int id of list. If it does not exist, create it.
         album_id: int | None = None
@@ -321,6 +329,8 @@ def add_albums():
 
             list_to_add: ListRow = session.merge(list_to_add)
             session.flush()
+
+            lists_in_db.append(list_to_add)
 
             album_id: int | None = list_to_add.id
 
@@ -396,6 +406,11 @@ def download_and_add_to_db_albums_data(ids: List[str]):
         else:
             image_url = album["images"][0]["url"]
 
+        if not isinstance(image_url, str):
+            logger.error(
+                f"image_url of album {album} is not str, {image_url=}")
+            continue
+
         image_path_dir = os.path.join("album", sanitize_folder_name(
             album["artists"][0]["name"]), sanitize_folder_name(album["name"]))
         image_path = os.path.join(image_path_dir, "image.png")
@@ -418,10 +433,12 @@ def download_and_add_to_db_albums_data(ids: List[str]):
                 image = internal_image
                 break
 
+        image_id: int | None = None
+
         if image:
             image_id = image.id
         else:
-            image_public_id = create_id(20)
+            image_public_id = create_id()
 
             image_to_add = InternalImageRow(
                 public_id=image_public_id,
@@ -453,6 +470,8 @@ def download_and_add_to_db_albums_data(ids: List[str]):
 
             list_to_add: ListRow = session.merge(list_to_add)
             session.flush()
+
+            lists_in_db.append(list_to_add)
 
             album_id: int | None = list_to_add.id
 
@@ -536,6 +555,11 @@ def download_and_add_to_db_songs_data(ids: List[str]):
         else:
             logger.error(
                 f"Skipping song {public_id=} album not found {album_public_id=}")
+            continue
+
+        if not isinstance(album_id, int):
+            logger.error(
+                f"Skipping song {public_id=} album_id is not int, {album_id=}")
             continue
 
         song_to_add = SongRow(
@@ -648,11 +672,19 @@ def add_songs():
                 internal_image_id = k.id
                 break
 
+        if not internal_image_id:
+            logger.error(f"internal_image_id of song {id} is None")
+            continue
+
         album_id: int | None = None
         for album in albums_in_db:
             if album.public_id == album_public_id:
                 album_id = album.id
                 break
+
+        if not album_id:
+            logger.error(f"album_id of song {id} is None")
+            continue
 
         song_to_add = SongRow(
             public_id=id,
@@ -718,6 +750,7 @@ def add_playlists():
             session.flush()
 
             playlist_id: int | None = list_to_add.id
+            lists_in_db.append(list_to_add)
 
         if not playlist_id:
             logger.error(
@@ -729,6 +762,10 @@ def add_playlists():
             if k.public_id == internal_image_public_id:
                 internal_image_id = k.id
                 break
+
+        if not internal_image_id:
+            logger.error(f"internal_image_id of song {id} is None")
+            continue
 
         playlist_to_add = PlaylistRow(
             id=playlist_id,
@@ -839,11 +876,20 @@ def add_artists():
         if found:
             continue
 
+        internal_image_id: str | None = None
+
+        logger.error("Not implemented error")
+
+        if not internal_image_id:
+            logger.error(f"internal_image_id of artist {artist['id']} is None")
+            continue
+
         artist_to_add = ArtistRow(
             public_id=artist["id"],
             name=artist["name"],
             followers=artist["followers"]["total"],
-            popularity=artist["popularity"]
+            popularity=artist["popularity"],
+            internal_image_id=internal_image_id
         )
 
         artist_to_add = session.merge(artist_to_add)
@@ -916,6 +962,13 @@ def add_song_artists():
     logger.info(f"Adding {len(song_artists_list)} song artists...")
 
     for song_id, artist_public_id in tqdm(song_artists_list, desc="Adding song artists"):
+
+        if song_id == None:
+            logger.error(f"song_id is None, {artist_public_id}")
+
+        if artist_public_id == None:
+            logger.error(f"artist_public_id is None, {song_id}")
+
         for artist_in_db in artists_in_db:
             if artist_in_db.public_id == artist_public_id:
                 artist_id = artist_in_db.id
@@ -1175,7 +1228,7 @@ def add_users():
             super_admin=super_admin if super_admin is not None else False
         )
 
-        session.merge([user_to_add])
+        session.merge(user_to_add)
 
     if len(user_lists_values) > 0:
         stmt = insert(user_lists).values(
@@ -1212,13 +1265,13 @@ def add_users():
 
 if __name__ == "__main__":
     # add_internal_images()
-    add_albums()
+    # add_albums()
     # add_songs()
     # add_playlists()
     # add_artists()
     # add_song_artists()
     # add_album_artists()
-    # add_users()
+    add_users()
     # download_and_add_to_db_albums_data(['6G9PvX9f6Dq26JzsiVHIdl', '18fOLsMG8Msf1DEaW0E71K', '2nE5WmbSq3qbyDrOpSTlnq', '5MAL7e4EnKXW1hFg6NbFqP', '4LH4d3cOWNNsVw41Gqt2kv', '4LH4d3cOWNNsVw41Gqt2kv', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '5qSfqoD1O3P45CYysXlpOo', '6FMu88LoghMcmme2aDkK3S', '0bgjJ99UFbk0yBOzjJl7cq', '5cwUCXPFFfNsnk4qipc40D', '47fRf3JwriMUPPzFjdvNS6', '2OyVtIEp7O7a6o82DF4Ba5', '5FfkiNcXAvagExRCLd8nn4', '0j12QW17dkUCCI7eOAiT1r', '35KafpmKh0nDLzBLV75MpR', '6OMYQUITdN6wBaWfEtgooI', '3REUXdj5OPKhuDTrTtCBU0', '4rQV5S9FhajZdyzFfcyYw9', '6CoeDRu0SmpFtLZMcRTO2F', '3ZdkT5buYFi1WQaB0XNNtf', '3ZdkT5buYFi1WQaB0XNNtf', '5Vdzprr5cOqXQo44eHeV7t', '3srdrIrP3V7LTmRujRfLhK', '1ICKrl6sDjJD1YdR9VDfPR', '54DjkEN3wdCQgfCTZ9WjdB', '2coqGqbnSCAy740mClWesA', '1j57Q5ntVi7crpibb0h4sv', '4HDJMKkwAMVFewqfZcmf84', '65Uo74eW8L3zXUxSOlSm6H', '2cKZfaz7GiGtZEeQNj1RyR', '71cfSO0iO1fjgQLEb3Wc6C', '78hVLZZJhaXgrnfXKc6yxF', '7MD06W6wJm7J6jqkBszV22', '1O2sEdKLsSHROEyYgUQmnb', '31TRqoVBTQi0lzlPLtvINn', '3t3BbpFJiGcXl4jI5CRLLA', '243XzHQegX82bPnUVQ0SPV', '3hgAoHSmany3EiKL0Aqh3G', '1UUOBzIHw0noiRGRpbt3sz', '0R8Pl54TXSwXWtAEVaP7ew', '1JDlLoZugxdneiaTnGyaKr', '0VPad241NfcHMOXcRcLrDe', '3wCe8HjHk6QNGcf5D3jgW1', '05owfigVGpgPe7RKJG1hum', '1B4oPgG5ljWTRxsKcTHAYn', '56dfEbntfVTMCxjrjggL1e', '02CxAhdSRhzcm6XQ8m5RNp', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '7iLuHJkrb9KHPkMgddYigh', '3iMwQk5yE0UDDKbLCdcxZA', '3VNTh6evo3MyUsStAiatcY', '5jkwdY6jS1Hzi8epr6HW7h', '1Ghv7iViywM23K8BRFggQv', '08jWgM4vSkTose4blKBWov', '5WndWfzGwCkHzAbQXVkg2V', '34SBayfOPJb9ztyCTvS3no', '6FTFKwFEs3hwpnj68VKXg3', '2FsaIC8jrXvWGIfokVZ4Jg', '033cvSPAuSU5ArRfIgQSDU', '0X4ZNTZw7SYgrp5rlBQC3N', '4gouGcdQn9OvjX42xnWrF0', '2nPJlUlcyQ24e1VdayD6TT', '5Yi08NDFoVAnvHoXqpp9O5', '3g5K7IOTOSY6nIfu7lj562', '0Zf6FJVyK6qUxmg1WMNruG', '2qSwmENEVYoacaxatupAaE', '61xN74Rar4MojQJ9ssqSKB', '1kHbPoLRmiAHIDgYRraYU9', '1yc1L4kNOK9mu44coyKX9s', '3T0Rxzvmk3nQcTfqggJIsD', '5TkfP3cqWgeBvCugPeiGNl', '4l4u9e9jSbotSXNjYfOugy', '64q58AfjSrrX9Egp7Zryw8', '2JxEUrs4GzHQdkV6qQayz5', '29m6DinzdaD0OPqWKGyMdz', '6oU298pdPTCQnMx1PYwyUA', '0F77QekrNe8vVAjU2sepja', '6b5osVLAcVTBvnqwDaPV4w', '0ib2UtSmLGssyqyoY6X8cm', '3SsNd5MeJKc1IK3nazaWg2', '6iVOz2hudE6dv5Yrcsw2c9', '4VykjLwkyfKMZVLrJJVrYh', '7oIftk0P8cViwNpNEdCYl2', '3gPlX9Zs3tXZZKNCyoOkSm', '23enz9nXJhH1BR1Rm5CzDJ', '3REUXdj5OPKhuDTrTtCBU0', '3UBqHwvxUDl6jWxY2RhmrN', '43L4t2oVmuJsudEls5C6Gh', '0tJT9ZnxnlElGa34DRj59l', '6zpu2C7qPjXrhv5cyuLNZn', '2blsPE3sO5SnroFjfEAlfj', '6DlSUW5gmq6Byc3osKDJ2p', '7tDrvcJnNTSIaFmRJVhagA', '4YimfceN1R0nOS9CefIFhm', '6DlSUW5gmq6Byc3osKDJ2p', '4gouGcdQn9OvjX42xnWrF0', '3pZ6D15onAaT2YyiTbcHmh', '1w7JOjdpfTBz4rvhWQDWJz', '48MhNEYxMJvcBbqz85UTQP', '48MhNEYxMJvcBbqz85UTQP', '274tWwrhGJy0IFeMENjWi7', '1DCI0mQQdf0LYoXheONDXi', '391ScNR3xKywWSpfDwP3n0'])
     # download_and_add_to_db_songs_data(["3oUphdZVPyrsprZ8FgbmQS","1yN2z5XVtaAOYGdeEqEuqd","7HKxTNVlkHsfMLhigmhC0I","7yBl8YmvcgwwYlazJcvowE","1WgOedf4pKmgepml1CMW6o","60e5PcqGmlVFQ0HHgUagCJ","6dzrCHBBKutQezMkeWaOEY","1efuoLEUL1K0D96d63svRO","4FNm4ZJaUVLDCMXcqwS6zY","2sevvnMrqH607r5lwk3kFT","0QyBdoz2JktWEo111DBEx9","0q5giEtY4wsFTwjWqswLwx","4r9hiElqKWMPT4Z3vN2exq","6XQHlsNu6so4PdglFkJQRJ","4f6PUDRYJI51UrZy0jDAxD","70RecAVg5QudOXfJs64sM5","1Vk4yRsz0iBzDiZEoFMQyv","762K1h8yVV5IgAVuEMpqfZ","39ncDMVidHOeQgeC5anYZM","4S3DXtdTdgOIezKgu8DR0M","1plcM0XlbKdjND7Ufokuzb","42pqNrGr0TZz5bzdCXt4sw","36N7FB6SiOxmnjwjfjTcSG","4LqaFJOawdPH1JliPDdssk","5iFujkF6DJlRhOyxYl4zjE","4q7tMaar2kUMweKu6N5bZz","51Y1WwassIhqZYBwwaAjzr","06jttXe119nc0r9Brkpn8a","2tBv9tAdqEbLNDi5smSjbg","40d2EcaOOCUjDzzo2YvUWn","3gbwtdRrtD05lz08DTxfFt","5kRBzRZmZTXVg8okC7SJFZ","5BMwpS4iYKR30kq9U9beaT","7gXjNlZlfP1GXRH1WvyuE2","2bonbKENtFAQQh8U4UEAu5","3ykSdTGmYPFl8pDBXer1zG","1tDWVeCR9oWGX8d5J9rswk","0vFOzaXqZHahrZp6enQwQb","2OF5LBNqH7GC9expc1Oatd","4VhiQU99RQ1qUCELJIK44n","5AyEjwO4pC5STlD1gU1wLr","23jsI6wT9St6jfSXgEQKeq","6nczjViR1GmwR1pwZKSpZC","7hZhyg8pMHYoCxaclBkG0L","0xycFQK4tNdLoM9DFE7TSn","4tgUwqjXLDlByyZctSun3c","5SXemxLERs0imkLpe4Aq8v","4PgZTLaGAMlKD6FCATr2sz","2miIOgni3EEut8pvYcxeYn","1e7sKwOgiMF2FBgb8HmlRb","7fYKKFD0jiSlMDgG9IUs1q","62JvDbmEtwIU3m4LhiG6Ah","1lzaRptS63unC0qgT6hm7T","6yVnpVWuT3NdhAmQe05T33","0a93e7F9pOj3dYHTrYb9JO","1MVKUdzL5pA9YlRkQMsb6j","3zJgt8DyC6ypyDnlrxUV8H","3Y0iJfYWOftZxtQUF8wa4f","5Chf7pNfiKfFFhBpbTdcY4","2BMqay80iBzZTa608Y1eG1","5dpRJkvY8oWMQmQbEQTXhO","4OUmlC67FoPLvQNuE5C7kF","0eRyOunOVBChlXxIvqwOxH","0CaBBQsaAiRHhiLmzi7ZRp","2VsX1BoWSGiuVXGiFSUr6h","50qHmeex1nPTyQTwBXjSE4","3zw4FTrVNfy2teEkV9FOvh","5tztLBvTlNC15Np2tnQ5Ll","5maTh4fY9SlgR3FhRkf040","7rgUYidQh5tH0YlXCoKaYJ","7y1hMqXNa0dKQLZH7CKbUG","4ekUX4pWizXXksJe0JfS9U","3idqWaBn3mRdsIodCU6uBi","6wfK1R6FoLpmUA9lk5ll4T","0sIZcoe69iSym9AecvZ7CT","2RvKsA6Ho7VbJkVFiD4UQF","5vl0sRtrQM1BTSAxQZFqEN","0KogQrSowDnZoU8GSpaxkj","7KyUS1fkcA491aobPDCcq1","5393jrdpAwmkq1u0RbP4Wk","0oeM1wuV8lRBJQg3Emvg9c","2F8DsV74G8roccHCegEAeE","2OgEAnBYY0ZTGv1uHgR3rX","6jnmiFauoWEtg5yXLJYzzF","4jhovniliZO54AmswLDA7f","6wdWtY9TSHJkJcqrZYXszh","1GHBv69iqWr9stD2DtMFkv","0K2WjMLZYr09LKwurGRYRE","1vLPTWPfJSIrqOhNUWNfNY","6nqHzwOdGIaX57U6VU6kMO","6hXPFI5fRd4QvWngZSSelM","5XY9FK0mB0BS0u8YB2ehN9","5Oioq6hXx9prdYO5wBTHid","76TFQMhDHtTKMSCGl1wp0r","41q8ZiB3LZl4HRmmzUTIo4","6HWHpSlPPLj7VL6ia625qk","3x11uesIVLyMlgFn7QYsNv","06n4xlUyRu88SKeQW1veOa","6OH9r1lZtmLN04CNefClwg","05mkxgw4kGQnarKMEovShB","066OGJp1Kbxn1OOFY8hbS7","7IzJv4GEEQfoQ6Z5umrleJ","6JNJERZGJwDVgkmbohBw7u","2Di0qFNb7ATroCGB3q0Ka7","71SvEDmsOwIWw1IozsZoMA","2bCQHF9gdG5BNDVuEIEnNk","5dRQUolXAVX3BbCiIxmSsf","0e3yhVeNaTfKIWQRw9U9sY","5WwqdeavrQrbeAMDxGawse","5vmRQ3zELMLUQPo2FLQ76x","05RgAMGypEvqhNs5hPCbMS","5JvsxPAHsGxwNq4xto2HtM","4d6eqRtpDX7tydHJGDZUBQ","3oEkrIfXfSh9zGnE7eBzSV","0J2p4KYdr6Mg4ET6JPlbe1","2tUBqZG2AbRi7Q0BIrVrEj","6mYrhCAGWzTdF8QnKuchXM","36lkJSDyMEZoWTqtRj8Q8q","62LJFaYihsdVrrkgUOJC05","1mCsF9Tw4AkIZOjvZbZZdT","1YrnDTqvcnUKxAIeXyaEmU","3ZZq9396zv8pcn5GYVhxUi","1Qrdlkgg9I4J7r3P4kZNwr","2245x0g1ft0HC7sf79zbYN","58XWGx7KNNkKneHdprcprX","4ZhPLoMzZwewHLLjV1J15c","4wswaG5vmNINMZcVBsAyBP","1eyq8cjUQ2daFthW2PC2GM","2A6yzRGMgSQCUapR2ptm6A","2KH16WveTQWT6KOG9Rg6e2","1CtAzw53AIXKjAemxy4b1j","6pPWRBubXOBAHnjl5ZIujB","0X1sqQ652p1sceKM2nJlIJ","7DFawVSjI88xR9mjnWwURg","2aEuA8PSqLa17Y4hKPj5rr","65HA9FsYyA2xro9RzNlNmy","4ByEFOBuLXpCqvO1kw8Wdm","4uOKFydzAejjSFqYbv1XPt","19Ym5Sg0YyOCa6ao21bdoG","3nFJbZCHP4d9vduKjJLdBL","1hlveB9M6ijHZRbzZ2teyh","2ax1vei61BzRGsEn6ckEdL","5rL2E6ZlMJYMdV799HumhZ","2KbnkNVzg8ubqK5wAXSxf1","09dKenoVuQzPRQ8Anoo6we","3AySFupxPGQWjauQfFXcr0","3QcqUffnkbQGWJlfyaT3yF","31l6t3Jq09uywRTVGbzant","4AWJmxQP5DK2mpQvKecaUB","4WidRX7HuOreEGkQ6tKBBi","4PY3mBItnv6YzN66Sq3Ci4","54b8qPFqYqIndfdxiLApea","31H6au3jhblhr6MMJiXnCq","5lmGgOaixbwKHtpTzjJoqx","19kAUW6NMzgDHKT19s7782","0JuW3zVUYjyW5BbtXq2to6","3hloNY7y2r53Cm3zkR7kZV","3075GFD8j1WFyF1pUGbAuh","6EePCjbtiMeb783pduiEJK","0l49BLiXUhHD2A92ONuQ7k","5Z7Xad6ghivPVONXAJFcst","2aVmwPRiY1DQMPpudL6wuF","1OBlQXdm2yQ2AjtpDhM7yc","1PtJclc46wTk367PlsU6Uj","17OqI90oTFZ3J8PVu6j07V","2TyQF1DcQ8k2cLjiweqQyG","762B4bOcXF7I2Y8UlKTyTy","3R47KOuGuGvmoeQqbODPa3","71or1G6CbfIttRDnBnTTAL","4zoWdR02nRwK8NWqpCM151","0mb5Q9w5GJKU7HClkEzHpy","4IKF9BDHZALfCXYXdS0koQ","3vkQ5DAB1qQMYO4Mr9zJN6","2HPB3px8MJZRMfu1L65Z41","35iCSlFxyiawRBUOtQAkeT","7rWgGyRK7RAqAAXy4bLft9","0odIT9B9BvOCnXfS0e4lB5","0YMFcrMtBowDdD5bPz0cgy","2vX5WL7s6UdeQyweZEx7PP","44aTAUBF0g6sMkMNE8I5kd","1KU5EHSz04JhGg3rReGJ0N","2RWFncSWZEhSRRifqiDNVV","41UXf5Tt4FShNileQJyubw","6rEJ6ncZKq9EM3TwyMhTDo","1xOXXYh6lTW8laxlW7JP2J","1s65HRPwZtSG5dBIBZ0dix","5RgFlk1fcClZd0Y4SGYhqH","54xbcKFYv2PC6ujERHppRj","5Nm9ERjJZ5oyfXZTECKmRt","1H7x0bYvO4rwUOb8fQXfEk","62Qvzu8oG0RIJYDxr90nWR","370wvMsgFk2WFsQfOI7ilP","6vhdFKCPjLBcMvfrBEGRu4","4qUAs5fOf9rGEGq4MTSFDD","2N7GESlJ6Wf5ZhgmpbCn4c","20FnYowQOrLxU92iyA94ed","1IP1ymL87BtfZqCsJTyp4C","1wDOwrbOEQOrcSldtK2riK","5cXHQInwGNOSmOBTqnuYOs","3OtsmzTZs50ahEnHKVvozC","55RzZKWf4ICmLH4ulzkXNG","0IqGdE8K3AguQWVEZ6wEV3","7oNt2nOzTTMCSMbIKP2inQ","1E4FcSqQs7U2RTebCNaYRH","0FARfrl2IDyNURBj1reFWQ","35qzUISYKada65IDfoeOLT","3NRXwFqPsmFe2dpIrT8CzK","1U9KXf8iMIHzNnG0Zb0Mzr","15F4pkkx1np7W93qQfqr80","0h7N5rOgTT1rSwLoj9f3Bk","473McZs08uo5AIimJyAelZ","5BZshzyuOfIGxmVt6RK7vn","3iD2CCv5bsJRIaqJRYD0uJ","2hOC9qItvmSkgMnxRjgPSr","5JCbyeGTxbxMRZICQ0OkZO","7LNiAjyg9P2GKINTtN6Yt8","1kN5RXxIYrSbCgO8ke88rV","5XmetMMUFNXClbiYnGdVmP","5NQYyej46WQkgCbnzGD21W","6TjUg1cTUzWHbal6yQAi7c","2AsGApoUuN8pTM17Lq9eUd","0Bs0hUYxz7REyIHH7tRhL2","4eLIq1nQNwz2qLu8DeiWIp","71LsKf3xISiOlY1mj7FFPP","5Z3Rd1fMcaty8g5Pn7yhBQ","2X9H5BokS1u5O46YpNYNsZ","5FnpXVgDOk2sLT58qM22Of","64P3zpRsDHIk7YTpRtaKYL","4zq4rrfHZeZsTbo5vjJXSV","6j67aNAPeQ31uw4qw4rpLa","5iyCSUM7zzficwaGo8GIoc","7s25THrKz86DM225dOYwnr","3EvpaRZkKWxsgDIO2zMH7g","2vEQ9zBiwbAVXzS2SOxodY","1WeoeHh0TSzsApyJ6Q8OOK","5XcZRgJv3zMhTqCyESjQrF","4rcHWl68ai6KvpXlc8vbnE","43btz2xjMKpcmjkuRsvxyg","5tdKaKLnC4SgtDZ6RlWeal","0TzbmxnYrlMV6dEln2l9nb","5QHtEiV3MEHLwOIqMxZUTV","4My3Flw72nhMNIiwUHI5KU","57ZXcBtCZXSg9TVV5xRdnR","1GeNWyZtCHbcp3ZWp8GTOO","5zWZ9iNevP0397xB3jWV2z","4LFwNJWoj74Yd71fIr1W8x","3PzsbWSQdLCKDLxn7YZfkM","4m2GdLuOhDt6rFbgOFMHIL","2cFsO1qC65joLn2GzRViBI","43YwOmGUOS3zzGvj1Feszb","0xSPEU1jpatIiRniszPjIo","1PgNBlADQqz8NZ3SKttj9l","1roY6Gv8pwLBLqDh5q1Ann","5HcTLesmMaPr0CkwdbtK0o","02Y7UtZLh7SbhYsYG5N7Rr","4wa7EsaCIc31OoedVvkVz6","7K3rmA4f6NEElsxKEOOt8H","0kesmP5jKuavoUDwuoMG3l","0SpNlEAUqNsuij5xi7Z7cQ","1nBufEoW7UXvQWgqmNl6Kx","4Wd9pEtEnZNDjgiswGOpJb","49NDegc7RQyvq6mhAs06p6","6H3kDe7CGoWYBabAeVWGiD","3lN8PP6R2IxbLP05QpYXng","1YqJSpIxdavjQ8BxAttU0r","7MTX3vevnm41xuEoPxWT3j","52dm9op3rbfAkc1LGXgipW","6zeE5tKyr8Nu882DQhhSQI","56dtB7EzO7EneUgYwX8krC","5lQKRR3MdJLtAwNBiT8Cq0","5UwbnHhjnbinJH8TefuQfN","1cU34sZG9kF4FYHCoAD0Ib","1c9dnQbOzw01ID7X2IsYOE","1GcVa4jFySlun4jLSuMhiq","6rovOdp3HgK1DeAMYDzoA7","4SDywo3ykB2PEthjXvo1UP","3VZmChrnVW8JK6ano4gSED","5veJDT0MLsLbhYsx42GXUD","7N3PAbqfTjSEU1edb2tY8j","3L5dCw2G1sNtAaZ0cqUBnY","4ogaI0XfmAunA0LyjveMSF","1FgyYm1kcXjkdR1VZmaiVo","1CAO7hiNOxJRPW4nFv2aRO","3aAuIvgJGdp4wWd60riWfB","17QTsL4K9B9v4rI8CAIdfC","4t0Pj3iBnSCZv5pDEPNmzG","2LXISHBkx8FyoxCBkckh19","1mskmld5ZKEhRaNvYVPoqZ","3fH4KjXFYMmljxrcGrbPj9","1x1XQqhBViz4opcpwc7FVs","67eX1ovaHyVPUinMHeUtIM","49OMJ1prsRA7ZYgrAjz70c","0l5TpsCL1ObiTEsHeWA0by","4fUU9WKxEgJXyrZJsUA2iP","1IhLUUzMxDDJ9pzfT95exy","6n7zeRIrGj8hTFxNWGb9k1","6goUTcMn0V30hWtKFIj6Kh","7DtdhIJlSSOaAFNk4JdXCb","6d2UQWWWZj3k4BE6WcN4IT","4LOgi2TAAoKU9ImfzRrCPO","2DjWsDGgL1xNbhnr7f6t5F","3Klfd4rsRO53fYpxmdQmYV","0BzhS74ByIVlyz8BedHaYi"])
 
--- db/dbTest.py
+++ db/dbTest.py
@@ -1,38 +1,104 @@
-import sys
 import os
+import sys
+from typing import List, Sequence, Tuple
+
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+from sqlalchemy.engine.row import Row
 
 sys.path.append(os.getcwd())  # noqa
 sys.path.append(os.getcwd() + "/backend")  # noqa
 
+from backend.db.ormModels.playlist import PlaylistRow
+from backend.logger import getLogger
 
-from backend.db.db import RockitDB, AlbumRow, ArtistRow, SongRow
-from typing import List
-from sqlalchemy import select
-from sqlalchemy.orm import joinedload
+from backend.db.associationTables.song_artists import song_artists
+from backend.db.associationTables.user_history_songs import user_history_songs
+from backend.db.db import RockitDB, UserRow, AlbumRow, ArtistRow, SongRow, ListRow
 
-rockit_db = RockitDB()
+rockit_db = RockitDB(verbose=False)
 session = rockit_db.get_session()
 
+logger = getLogger("dbTest")
 
-album_id = 1
+# album_id = 1
 
-album: AlbumRow | None = session.execute(
-    select(AlbumRow)
-    .options(
-        joinedload(AlbumRow.artists),              # Load album → artists
-        joinedload(AlbumRow.songs)
-        .joinedload(SongRow.artists)           # Load songs → artists
-    )
-    .where(AlbumRow.id == album_id)
-).unique().scalar_one_or_none()
+# album: AlbumRow | None = session.execute(
+#     select(AlbumRow)
+#     .options(
+#         joinedload(AlbumRow.artists),              # Load album → artists
+#         joinedload(AlbumRow.songs)
+#         .joinedload(SongRow.artists)           # Load songs → artists
+#     )
+#     .where(AlbumRow.id == album_id)
+# ).unique().scalar_one_or_none()
 
 
-if album:
-    print(album.name)
-    album.name = "test album name"
-    print(type(album.name))
-    artists: List[ArtistRow] = album.artists
+# if album:
+#     print(album.name)
+#     artists: List[ArtistRow] = album.artists
+#     print(f"{artists[0].name=}")
 
-    print("album.songs", artists[0].songs)
-else:
-    print("Album not found")
+#     print(f"{artists[0].songs=}")
+
+#     for song in artists[0].songs:
+#         print(song.name, [(user.username, user.super_admin,
+#               user.volume, user.admin) for user in song.queued_by_users])
+
+# else:
+#     print("Album not found")
+
+# print("\n\n")
+# user: UserRow | None = session.execute(
+#     select(UserRow)
+#     .options(
+#         joinedload(UserRow.lists)
+#         .joinedload(ListRow.album)
+#         .joinedload(AlbumRow.songs),
+#         joinedload(UserRow.lists)
+#         .joinedload(ListRow.playlist)
+#         .joinedload(PlaylistRow.songs)
+#     )
+#     .where(UserRow.id == 1)
+# ).unique().scalar_one_or_none()
+
+# if user:
+#     logger.info(
+#         f"{user.admin=}, {user.volume=}, {user.super_admin=}, {user.username=}")
+#     for list in user.lists:
+#         if list.album:
+#             logger.info(f"{list.album.name=}, {len(list.album.songs)=}")
+#         elif list.playlist:
+#             logger.info(f"{list.playlist.name}, {len(list.playlist.songs)}")
+# else:
+#     print("User not found")
+
+
+# result = session.execute(
+#     select(user_history_songs.c.played_at, SongRow.name, ArtistRow.name)
+#     .join(SongRow, user_history_songs.c.song_id == SongRow.id)
+#     .join(song_artists, song_artists.c.song_id == SongRow.id)
+#     .join(ArtistRow, song_artists.c.artist_id == ArtistRow.id).where(user_history_songs.c.user_id == 1)
+#     .order_by(user_history_songs.c.played_at.desc())
+#     .limit(10)
+# ).all()
+
+
+# for k in result:
+#     logger.info(k)
+
+
+# songs_in_db: Sequence[Row[Tuple[int, str, str | None]]] = session.execute(select(SongRow.id, SongRow.public_id, SongRow.path).where(
+#     SongRow.id.in_([1, 2, 3, 4]))).all()
+
+# print("\n\n\n\n")
+# for k in songs_in_db:
+#     a = k._mapping[SongRow.id]
+
+
+albums_in_db: Sequence[AlbumRow] = session.execute(select(AlbumRow).where(
+    AlbumRow.id.in_([1, 2, 3, 4]))).scalars().all()
+
+print("\n\n\n\n")
+for k in albums_in_db:
+    print(k)

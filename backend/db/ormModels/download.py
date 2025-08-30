3aa80f4b999f976223f76146f2ff17343f5f6c71
= <=>
2025-08-30 22:10:02 +0200

    [UPDATE] Refactor DownloadRow model to improve type safety and streamline status management

--- backend/db/ormModels/download.py
+++ backend/db/ormModels/download.py
@@ -1,9 +1,8 @@
-from typing import TYPE_CHECKING
 from datetime import datetime
+from typing import TYPE_CHECKING, Literal, Final
 
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from sqlalchemy import ForeignKey, String, Integer, DateTime, Enum, Boolean
-
 
 from backend.db.base import Base
 from backend.db.ormModels.declarativeMixin import TableDateAdded, TableDateUpdated, TableAutoincrementId
@@ -12,22 +11,44 @@ if TYPE_CHECKING:
     from backend.db.ormModels.user import UserRow
 
 
+DOWNLOAD_STATUSES: Final[tuple[str, ...]] = (
+    "pending",
+    "in_progress",
+    "completed",
+    "failed",
+    "fetching",
+    "waiting_for_queue_setup",
+    "waiting_for_songs"
+)
+
+STATUS_TYPE = Literal[
+    "pending",
+    "in_progress",
+    "completed",
+    "failed",
+    "fetching",
+    "waiting_for_queue_setup",
+    "waiting_for_songs"
+]
+
+
 class DownloadRow(Base, TableAutoincrementId, TableDateUpdated, TableDateAdded):
     __tablename__ = 'downloads'
     __table_args__ = {'schema': 'main', 'extend_existing': True},
 
+    public_id: Mapped[str] = mapped_column(String, nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey(
         'main.users.id'), nullable=False)
     date_started: Mapped[datetime] = mapped_column(
         DateTime(timezone=False), nullable=False)
-    date_ended: Mapped[datetime] = mapped_column(
+    date_ended: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=False), nullable=True)
     download_url: Mapped[str] = mapped_column(String, nullable=False)
-    status: Mapped[str] = mapped_column(Enum("pending", "in_progress", "completed",
-                                        "failed", name="download_status_enum", schema="main"), nullable=False, default="pending")
+    status: Mapped[STATUS_TYPE] = mapped_column(Enum(
+        *DOWNLOAD_STATUSES, name="download_status_enum", schema="main"), nullable=False, default="pending")
     seen: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
-    success: Mapped[int] = mapped_column(Integer, nullable=True)
-    fail: Mapped[int] = mapped_column(Integer, nullable=True)
+    success: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    fail: Mapped[int | None] = mapped_column(Integer, nullable=True)
 
     user: Mapped["UserRow"] = relationship("UserRow", back_populates="downloads",
-                                         foreign_keys=[user_id])
+                                           foreign_keys=[user_id])

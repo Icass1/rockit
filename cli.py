3a661fd0fd260147f98c68e1d74e3e4f3ec147df
IcassLaptopUbuntu <i.cassdolopez@gmail.com>
2025-09-09 22:54:15 +0200

    [UPDATE] CLI autocompleted

--- cli.py
+++ cli.py
@@ -1,199 +1,70 @@
-import sys
-import os
-import json
+# cli_app.py
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.application import Application
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout import Layout
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.layout.controls import FormattedTextControl
 
-from backend.downloader.downloader import Downloader
-from backend.constants import IMAGES_PATH, SONGS_PATH
-from backend.utils.logger import getLogger
+# Define all commands
+commands = [
+    "api-get album", "api-get song", "api-get playlist", "api-get artist",
+    "delete album", "delete song", "delete playlist"
+]
 
-logger = getLogger(__name__)
+# Autocompleter
+command_completer = WordCompleter(commands, ignore_case=True, sentence=True)
+
+# History/output text
+output_lines = ["Welcome to CLI App!", "Type a command below."]
+
+def get_output_text():
+    return "\n".join(output_lines)
+
+# Key bindings
+kb = KeyBindings()
+
+@kb.add("c-c")
+def _(event):
+    """Exit with Ctrl+C."""
+    event.app.exit()
+
+# Main UI Layout
+body = Window(content=FormattedTextControl(get_output_text), wrap_lines=True)
+session = PromptSession(completer=command_completer)
+
+def run_command(command: str):
+    output_lines.append(f"> {command}")
 
 
-downloader = Downloader()
-
-db = downloader.spotify.db
 
 
-def delete_album(album_id: str):
-
-    album_db: AlbumDBFull = db.get(
-        "SELECT * FROM album WHERE id = ?", (album_id,))
-
-    if not album_db:
-        logger.error(f"{album_id} album not found")
-        return
-
-    logger.warning(f"Deleting album {album_db.id} - {album_db.name}")
-
-    for song_id in album_db.songs:
-        delete_song(song_id)
-
-    image_db: ImageDB = db.get(
-        "SELECT * FROM image WHERE id = ?", (album_db.image,))
-
-    if image_db:
-        if image_db.path and os.path.exists(os.path.join(IMAGES_PATH, image_db.path)):
-            os.remove(os.path.join(IMAGES_PATH, image_db.path))
-            logger.info(f"Removed {os.path.join(IMAGES_PATH, image_db.path)}")
-        else:
-            logger.warning(f"Unable to delete file of image {image_db.id}")
-
-        db.execute("DELETE FROM image WHERE id = ?", (image_db.id,))
-        logger.info(f"Deleted image {image_db.id}")
+    # Here you'd implement your logic for the commands
+    if command.startswith("api-get album"):
+        output_lines.append(f"Fetching album {command.split()[-1]}...")
+    elif command.startswith("delete album"):
+        output_lines.append(f"Deleting album {command.split()[-1]}...")
     else:
-        logger.warning(f"Album image not found in database")
+        output_lines.append("Unknown command!")
 
-    db.execute("DELETE FROM album WHERE id = ?", (album_id,))
-    logger.info(f"Deleted album {album_id}")
-
-
-def delete_song(song_id: str):
-
-    song_db: SongDBFull = db.get("SELECT * FROM song WHERE id = ?", (song_id,))
-
-    if not song_db:
-        logger.error(f"{song_id} song not found")
-        return
-
-    logger.warning(f"Deleting song {song_db.id} - {song_db.name}")
-
-    if song_db.path and os.path.exists(os.path.join(SONGS_PATH, song_db.path)):
-        os.remove(os.path.join(SONGS_PATH, song_db.path))
-        logger.info(f"Removed {os.path.join(SONGS_PATH, song_db.path)}")
-    else:
-        logger.warning(f"Unable to delete file of song {song_id}")
-
-    db.execute("DELETE FROM song WHERE id = ?", (song_id,))
-    logger.info(f"Deleted song {song_id}")
-
-
-def delete_playlist(playlist_id: str):
-
-    playlist_db: PlaylistDBFull = db.get(
-        "SELECT * FROM playlist WHERE id = ?", (playlist_id,))
-
-    if not playlist_db:
-        logger.error(f"{playlist_id} playlist not found")
-        return
-
-    logger.warning(f"Deleting playlist {playlist_id} - {playlist_db.name}")
-
-    for song in playlist_db.songs:
-        delete_song(song.id)
-
-    image_db: ImageDB = db.get(
-        "SELECT * FROM image WHERE id = ?", (playlist_db.image,))
-
-    if image_db:
-        if image_db.path and os.path.exists(os.path.join(IMAGES_PATH, image_db.path)):
-            os.remove(os.path.join(IMAGES_PATH, image_db.path))
-            logger.info(f"Removed {os.path.join(IMAGES_PATH, image_db.path)}")
-        else:
-            logger.warning(f"Unable to delete file of image {image_db.id}")
-
-        db.execute("DELETE FROM image WHERE id = ?", (image_db.id,))
-        logger.info(f"Deleted image {image_db.id}")
-    else:
-        logger.warning(f"Playlist image not found in database")
-
-    db.execute("DELETE FROM playlist WHERE id = ?", (playlist_id,))
-    logger.info(f"Deleted playlist {playlist_id} - {playlist_db.name}")
-
-
-def help_delete():
-    print("Posible commands:")
-    print("  delete album <id>")
-    print("  delete song <id>")
-    print("  delete playlist <id>")
-
-
-def delete():
-    if len(sys.argv) < 3:
-        help_delete()
-    elif sys.argv[2] == "album":
-        delete_album(sys.argv[3])
-    elif sys.argv[2] == "song":
-        delete_song(sys.argv[3])
-    elif sys.argv[2] == "playlist":
-        delete_playlist(sys.argv[3])
-    else:
-        logger.error("Unknow command")
-        help_delete()
-
-
-def help_api_get():
-    print("Posible commands:")
-    print("  api-get album <id>")
-    print("  api-get song <id>")
-    print("  api-get playlist <id>")
-    print("  api-get artist <id>")
-
-
-def api_get():
-    if len(sys.argv) < 3:
-        help_api_get()
-    elif sys.argv[2] == "album":
-        print("TODO")
-    elif sys.argv[2] == "song":
-        print("TODO")
-    elif sys.argv[2] == "playlist":
-        print("TODO")
-    elif sys.argv[2] == "artist":
-        artist = downloader.spotify.get_artist(sys.argv[3])
-        if artist:
-            print(json.dumps(artist._json, indent=4))
-        else:
-            print("Artist is None")
-    else:
-        logger.error("Unknow command")
-        help_api_get()
-
-
-def get_status_mockup():
-    if len(sys.argv) != 4:
-        help_get_status_mockup()
-
-    log_file = sys.argv[2]
-    output_file = sys.argv[3]
-
-    if not os.path.exists(log_file):
-        logger.error(f"{log_file} not found")
-
-    file = open(log_file)
-    content = file.readlines()
-    file.close()
-
-    with open(output_file, "w") as f:
-        for line in content:
-            if not "messageHandler.MessageHandler.add" in line:
-                continue
-            f.write(f"{line[0:19]} {line.split(' - ')[2]}")
-
-    print(log_file, output_file)
-
-
-def help_get_status_mockup():
-    print("Posible commands:")
-    print("  get-status-mockup <log file> <output file>")
-
-
-def help():
-    print("Posible commands:")
-    print("  delete")
-    print("  api-get")
-    print("  get-status-mockup")
-
+async def app():
+    from prompt_toolkit.patch_stdout import patch_stdout
+    app = Application(
+        layout=Layout(HSplit([body])),
+        key_bindings=kb,
+        full_screen=True
+    )
+    with patch_stdout():
+        while True:
+            try:
+                command = await session.prompt_async("> ")
+                run_command(command)
+                body.content.text = get_output_text()
+            except (EOFError, KeyboardInterrupt):
+                break
+    print("Goodbye!")
 
 if __name__ == "__main__":
-
-    if len(sys.argv) < 2:
-        help()
-    elif sys.argv[1] == "delete":
-        delete()
-    elif sys.argv[1] == "api-get":
-        api_get()
-    elif sys.argv[1] == "get-status-mockup":
-        get_status_mockup()
-    else:
-        logger.error("Unknow command")
-        help()
+    import asyncio
+    asyncio.run(app())

4f4195ce0b754f6600e031aeb29d182111cf0c70
= <=>
2025-09-18 18:46:24 +0200

    [UPDATE] Implemented general responses in stats router

--- backend/routers/stats.py
+++ backend/routers/stats.py
@@ -5,36 +5,63 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.sql.expression import func
 
-from backend.db.associationTables.user_history_songs import user_history_songs
+from backend.responses.general.externalImage import RockItExternalImageResponse
+from backend.utils.auth import get_current_user
+from backend.initDb import rockit_db
+
 from backend.db.ormModels.album import AlbumRow
 from backend.db.ormModels.artist import ArtistRow
 from backend.db.ormModels.song import SongRow
 from backend.db.ormModels.user import UserRow
-from backend.responses.stats.homeStatsResponse import AlbumHomeStatsResponse, ArtistHomeStatsResponse, HomeStatsResponse, SongHomeStatsResponse
-from backend.utils.auth import get_current_user
 
-from backend.initDb import rockit_db
+from backend.db.associationTables.user_history_songs import user_history_songs
+
+from backend.responses.general.album import RockItAlbumResponse
+from backend.responses.general.artist import RockItArtistResponse
+from backend.responses.general.copyright import RockItCopyrightResponse
+from backend.responses.general.song import RockItSongResponse
+from backend.responses.stats.homeStatsResponse import HomeStatsResponse
 
 
 router = APIRouter(prefix="/stats")
 
 
-def get_response_songs_list(songs:  Sequence[Tuple[SongRow, datetime]]) -> List[SongHomeStatsResponse]:
-    out: List[SongHomeStatsResponse] = []
+def get_response_songs_list(songs:  Sequence[Tuple[SongRow, datetime]]) -> List[RockItSongResponse]:
+    out: List[RockItSongResponse] = []
     for song, played_at in songs:
         out.append(
-            SongHomeStatsResponse(
+            RockItSongResponse(
                 name=song.name,
                 publicId=song.public_id,
-                playedAt=played_at,
-                internalImageUrl=f"https://localhost:8000/image/{song.internal_image.public_id}" if song.internal_image else None,
-                album=AlbumHomeStatsResponse(
+                internalImageUrl=f"http://localhost:8000/image/{song.internal_image.public_id}" if song.internal_image else None,
+                duration=song.duration,
+                trackNumber=song.track_number,
+                downloadUrl=song.download_url,
+                popularity=song.popularity,
+                dateAdded=song.date_added,
+                discNumber=song.disc_number,
+                isrc=song.isrc,
+                album=RockItAlbumResponse(
                     publicId=song.album.public_id,
-                    name=song.album.name
+                    name=song.album.name,
+                    copyrights=[
+                        RockItCopyrightResponse(
+                            text=a.text,
+                            type=a.type
+                        ) for a in song.album.copyrights],
+                    externalImages=[
+                        RockItExternalImageResponse(
+                            url=a.url,
+                            width=a.width,
+                            height=a.height
+                        ) for a in song.album.external_images]
                 ),
                 artists=[
-                    ArtistHomeStatsResponse(
-                        name=artist.name, publicId=artist.public_id) for artist in song.artists
+                    RockItArtistResponse(
+                        name=artist.name,
+                        publicId=artist.public_id,
+                        genres=[a.name for a in artist.genres]
+                    ) for artist in song.artists
                 ]
             )
         )
@@ -42,9 +69,9 @@ def get_response_songs_list(songs:  Sequence[Tuple[SongRow, datetime]]) -> List[
     return out
 
 
-@router.get("/")
+@router.get("")
 def stats(current_user: UserRow = Depends(get_current_user)):
-    return "OK"
+    return []
 
 
 @router.get("/home")

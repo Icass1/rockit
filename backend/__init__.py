c00572af7e0314c5f5221000564cfbc1f7ebf2d9
IgnacioPCWSL <i.cassdolopez@gmail.com>
2026-02-22 23:49:29 +0100

    fix: Update image copy path to use IMAGES_PATH constant

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,8 @@
 
+from backend.constants import IMAGES_PATH
 from backend.utils.addInit import add_init_to_orm
 import shutil
 
-shutil.copytree("backend/images", "images", dirs_exist_ok=True)
+shutil.copytree("backend/images", IMAGES_PATH, dirs_exist_ok=True)
 
 add_init_to_orm()

8d121b552a9043ce149a8a0eff6784cfa2517fff
IgnacioPCWSL <i.cassdolopez@gmail.com>
2026-02-22 23:48:23 +0100

    feat: Add user placeholder image and update image copying logic

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,7 @@
-from backend.utils.addInit import add_init_to_orm
 
+from backend.utils.addInit import add_init_to_orm
+import shutil
+
+shutil.copytree("backend/images", "images", dirs_exist_ok=True)
 
 add_init_to_orm()

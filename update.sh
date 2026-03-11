2b2e2962a449933601394f752ca59cfbeb0254b5
Nicorebo-18 <Nicorebo18@gmail.com>
2026-03-11 22:31:59 +0100

    Mini fix

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@
 cd backend
 pip install -r requirements.txt
 
+# Build the backend
+cd ..
+python3 -m backend zod
+
 # Install frontend dependencies
-cd ../frontend
+cd frontend
 pnpm self-update
 pnpm install

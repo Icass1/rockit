e1681050179df8533b9ba2c261f740e9ce0082ab
Ignacio Casado <i.cassdolopez@gmail.com>
2026-04-08 12:14:05 +0200

    Removed completed column from download

--- backend/core/access/db/ormModels/download.py
+++ backend/core/access/db/ormModels/download.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Dict, List
 
-from sqlalchemy import DOUBLE_PRECISION, ForeignKey, Integer
+from sqlalchemy import ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.core.access.db.base import CoreBase
@@ -15,6 +15,7 @@ if TYPE_CHECKING:
     from backend.core.access.db.ormModels.downloadStatus import DownloadStatusRow
 
 
+# To get the completed percetage of this download, the latest downoad status with the same download id will be used. So the percentage is determined by the latest download status of this download.
 class DownloadRow(CoreBase, TableAutoincrementId, TableDateUpdated, TableDateAdded):
     __tablename__ = "download"
     __table_args__ = ({"schema": "core", "extend_existing": True},)
@@ -28,29 +29,18 @@ class DownloadRow(CoreBase, TableAutoincrementId, TableDateUpdated, TableDateAdd
     status_key: Mapped[int] = mapped_column(
         Integer, ForeignKey("core.download_status_enum.key"), nullable=False, default=1
     )
-    completed: Mapped[float | None] = mapped_column(DOUBLE_PRECISION, nullable=True)
 
     download_group: Mapped["DownloadGroupRow"] = relationship(
         "DownloadGroupRow", back_populates="downloads"
     )
     download_status_list: Mapped[List["DownloadStatusRow"]] = relationship(
-        "DownloadStatusRow",
-        back_populates="download",
-        order_by="DownloadStatusRow.id",
-        cascade="all, delete-orphan",
+        "DownloadStatusRow", back_populates="download", order_by="DownloadStatusRow.id"
     )
 
-    def __init__(
-        self,
-        download_group_id: int,
-        media_id: int,
-        status_key: int = 1,
-        completed: float | None = None,
-    ):
-        kwargs: Dict[str, None | float | int] = {}
+    def __init__(self, download_group_id: int, media_id: int, status_key: int = 1):
+        kwargs: Dict[str, int] = {}
         kwargs["download_group_id"] = download_group_id
         kwargs["media_id"] = media_id
         kwargs["status_key"] = status_key
-        kwargs["completed"] = completed
         for k, v in kwargs.items():
             setattr(self, k, v)

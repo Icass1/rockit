403dd8f53357e8e25d04321d68a54c71821572ca
Ignacio Casado <i.cassdolopez@gmail.com>
2026-04-09 12:09:36 +0200

    feat: add RegisterPage component with background styling and modal integration

---
+++ frontend/apps/web/app/register/page.tsx
@@ -0,0 +1,17 @@
+import RegisterModal from "@/components/Auth/RegisterModal";
+
+export default function RegisterPage() {
+    return (
+        <div
+            className="relative h-full w-full"
+            style={{
+                backgroundImage: "url(/background.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPositionY: "bottom",
+            }}
+        >
+            <RegisterModal />
+        </div>
+    );
+}
